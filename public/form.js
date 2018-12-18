8494d89253f16cd4decaac1e1da736298c8d5c3b
Ollie Treend <ollie@treend.com>
2018-12-18 19:08:10 +0000

    Save form submissions to Firestore
    Submitted form data is saved as a document in the `applications` collection with an automatically assigned document ID.

--- public/form.js
+++ public/form.js
@@ -1,20 +1,25 @@
 class ApplicationForm {
-    constructor(formElement) {
+    constructor(formElement, firestore) {
         this.formElement = formElement;
+        this.firestore = firestore;
         formElement.addEventListener('submit', (e) => this.handleSubmit(e));
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        console.log('Form submitted');
-        console.log(this.getFormData());
+        const formData = this.getFormData();
+        this.saveToFirebase(formData);
     }
 
     getFormData() {
+        // Create a Date object from the DOB field value
+        const dobField = this.getFieldValue('dob');
+        const dob = dobField ? new Date(dobField) : null;
+
         return {
             forename: this.getFieldValue('forename'),
             surname: this.getFieldValue('surname'),
-            dob: this.getFieldValue('dob'),
+            dob: dob,
             email: this.getFieldValue('email'),
             phone: this.getFieldValue('phone'),
             address: this.getFieldValue('address'),
@@ -26,6 +31,30 @@ class ApplicationForm {
     getFieldValue(name) {
         return this.formElement.querySelector("[name='" + name + "']").value;
     }
+
+    saveToFirebase(data) {
+        console.log('Saving to Firestore', data);
+        this.firestore.collection('applications').add(data)
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+    }
 }
 
-new ApplicationForm(document.getElementById('application-form'));
+document.addEventListener('DOMContentLoaded', () => {
+    // Get the form element
+    const form = document.getElementById('application-form');
+
+    // Initialize Cloud Firestore through Firebase
+    const db = firebase.firestore();
+
+    // Disable deprecated features
+    db.settings({
+        timestampsInSnapshots: true
+    });
+
+    new ApplicationForm(form, db);
+});
