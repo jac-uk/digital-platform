114d0cb789f5965de05681bfaf061e0e2020a562
Ollie Treend <ollie@treend.com>
2019-01-04 11:13:29 +0000

    Replace Vue 'Hello World' with a dynamic application form
    This commit removes the default Vue.js `HelloWorld` component, and introduces a new components `ApplicationForm` and `Relationships`.
    
    The new components form a Proof of Concept to show a dynamic form with repeatable fields working in Vue and producing a JSON data dump. This can be seen in the JS console.

---
+++ src/components/ApplicationForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <h2>Personal Details</h2>
+
+    <div class="form-row">
+      <div class="form-group col-sm-6">
+        <label for="forename">Forename</label>
+        <input class="form-control" type="text" v-model="form.forename" id="forename">
+      </div>
+      <div class="form-group col-sm-6">
+        <label for="surname">Surname</label>
+        <input class="form-control" type="text" v-model="form.surname" id="surname">
+      </div>
+    </div>
+
+    <h2>Relationships</h2>
+
+    <Relationships v-model="form.relationships" />
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script>
+import Relationships from './Relationships';
+
+export default {
+  name: "ApplicationForm",
+  components: {
+    Relationships
+  },
+  data() {
+    return {
+      form: {
+        forename: '',
+        surname: '',
+        relationships: [
+          // {forename: 'John', surname: 'Smith', relationship: 'Friend'},
+          // {forename: 'Jane', surname: 'Doe', relationship: 'Mother'},
+        ],
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('Form was submitted');
+      console.log(this.form);
+      console.log(JSON.stringify(this.form));
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
