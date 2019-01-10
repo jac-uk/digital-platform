2abd3fd24eaa524f68bdb007f6fea213a3efbb7b
Ollie Treend <ollie@treend.com>
2019-01-10 17:00:51 +0000

    Configure Jest code coverage reporting
    There is a known caveat to the way `.vue` Single File Components are tested: **Jest will not measure code coverage on components which don't have any methods**. This is a known issue which is documented *somewhere* online, although I'm having trouble finding exactly where.
    
    More information on code coverage can be found here: https://vue-test-utils.vuejs.org/guides/testing-single-file-components-with-jest.html#coverage

--- jest.config.js
+++ jest.config.js
@@ -19,5 +19,13 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/'
+  testURL: 'http://localhost/',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}'
+  ],
+  coverageReporters: [
+    'html',
+    'text-summary'
+  ]
 }
