f8e03c8fa86fc85bfecdf04719ee52e153b1fa69
Ollie Treend <ollie@treend.com>
2019-01-10 15:47:47 +0000

    Add Jest for unit tests
    This commit introduces Jest, alongside packages which add support for testing Vue applications. This was achieved by running the following command in the `vue` CLI tool: `vue add @vue/unit-jest`
    An example `HelloWorld` spec was supplied by the installer, for which I have created a supporting `HelloWorld` component. This results in a running and passing test suite which can be run with: `npm run test:unit`

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+  export default {
+    name: "HelloWorld",
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
