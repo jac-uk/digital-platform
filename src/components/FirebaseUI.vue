5e1180abca70a4b8333bc75c92ad02d9d71c2a8a
Ollie Treend <ollie@treend.com>
2019-01-14 22:01:43 +0000

    Add unit tests for `FirebaseUI` authentication component
    This tests that the `FirebaseUI` component correctly creates and destroys the FirebaseUI library widget.

--- src/components/FirebaseUI.vue
+++ src/components/FirebaseUI.vue
@@ -3,33 +3,39 @@
 </template>
 
 <script>
-  import {auth} from '../firebase';
+  import {auth} from '@/firebase';
   import firebaseui from 'firebaseui';
 
   export default {
     name: "FirebaseUI",
     mounted() {
-      const uiConfig = {
-        signInOptions: [
-          {
-            provider: auth.EmailAuthProvider.PROVIDER_ID,
-            requireDisplayName: false
-          }
-        ],
-        credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-        callbacks: {
-          signInSuccessWithAuthResult: () => {
-            // Do nothing here because post-login triggers listen for onAuthStateChanged event
-            return false;
-          }
-        }
-      };
-
       this.ui = new firebaseui.auth.AuthUI(auth());
-      this.ui.start('#firebaseui-auth-container', uiConfig);
+      this.ui.start('#firebaseui-auth-container', this.uiConfig);
     },
     destroyed() {
       this.ui.delete();
+    },
+    data() {
+      return {
+        uiConfig: {
+          signInOptions: [
+            {
+              provider: auth.EmailAuthProvider.PROVIDER_ID,
+              requireDisplayName: false
+            }
+          ],
+          credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+          callbacks: {
+            signInSuccessWithAuthResult: this.returnFalse
+          }
+        }
+      }
+    },
+    methods: {
+      returnFalse() {
+        // Do nothing here because post-login triggers listen for onAuthStateChanged event
+        return false;
+      }
     }
   }
 </script>
