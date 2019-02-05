726c48aff80e47c6e9810a7734fcef2a679bbf93
Ollie Treend <ollie@treend.com>
2019-02-05 09:08:02 +0000

    Mock firestore service in `vacancy` vuex module tests
    Previously these tests were using a real instance of Firebase & Firestore, and where necessary was being manually mocked with `jest.fn()` mocks.
    This commit replaces that with `mock-firebase`, which is used in other unit tests in this codebase, to mock the Firestore interface. This improves readability of the tests, since it's easier to understand how data is stored and being accessed in Firestore.

--- tests/unit/store/vacancy.spec.js
+++ tests/unit/store/vacancy.spec.js
@@ -1,8 +1,14 @@
 import vacancy from '@/store/vacancy';
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-import sanitizeFirestore from '@/utils/sanitizeFirestore';
+import sanitizeFirestore from "@/utils/sanitizeFirestore";
+import {firestore} from '@/firebase';
+
 jest.mock('@/utils/sanitizeFirestore');
+jest.mock('@/firebase', () => {
+  const firebase = require('firebase-mock');
+  const firestore = firebase.MockFirebaseSdk().firestore();
+  firestore.autoFlush();
+  return {firestore};
+});
 
 describe('store/vacancy', () => {
   const mutations = vacancy.mutations;
@@ -51,33 +57,48 @@ describe('store/vacancy', () => {
     });
 
     describe('loadVacancy', () => {
-      it('throws an error if vacancy ID has not been set', async () => {
-        await expect(actions.loadVacancy(context)).rejects.toThrowError('set the Vacancy ID first');
+      describe('vacancy ID has not been set', () => {
+        it('throws an error', async () => {
+          await expect(actions.loadVacancy(context)).rejects.toThrowError('set the Vacancy ID first');
+        });
       });
 
-      it('gets the vacancy data, sanitizes it, and commits it to the state', async () => {
+      describe('happy path', () => {
+        const docId = 'hsQqdvAfZpSw94X2B8nA';
         const data = {title: 'Example Vacancy Title'};
         const sanitizedData = {title: 'Example Vacancy Title', sanitized: true};
-        const snapshot = {
-          data: jest.fn().mockReturnValue(data)
-        };
-        getters.vacancyDoc = {
-          get: jest.fn().mockResolvedValue(snapshot)
-        };
 
+        beforeEach(async () => {
+          state.id = docId;
+          getters.vacancyDoc = firestore.collection('vacancies').doc(docId);
+          await getters.vacancyDoc.set(data);
+          await actions.loadVacancy(context);
+        });
+
+        afterEach(() => {
+          getters.vacancyDoc.delete();
+        });
+
+        it('finds the document data in Firestore and sanitizes it', () => {
+          expect(sanitizeFirestore).toHaveBeenCalledWith(data);
+        });
+
+        it('commits the sanitized data', () => {
+          expect(context.commit).toHaveBeenCalledWith('setVacancy', sanitizedData);
+        });
+      });
+
+      it('returns a Promise', () => {
         state.id = 'hsQqdvAfZpSw94X2B8nA';
-        await actions.loadVacancy(context);
-
-        expect(snapshot.data).toHaveBeenCalled();
-        expect(sanitizeFirestore).toHaveBeenCalledWith(data);
-        expect(context.commit).toHaveBeenCalledWith('setVacancy', sanitizedData);
+        getters.vacancyDoc = firestore.collection('vacancies').doc();
+        expect(actions.loadVacancy(context)).toBeInstanceOf(Promise);
       });
     });
   });
 
   describe('getters', () => {
     describe('vacancy', () => {
-      it('returns the vacancy data object from state', () => {
+      it('returns the vacancy data object from state (passed by reference)', () => {
         const data = {title: 'Example Vacancy Title'};
         state.data = data;
         expect(getters.vacancy(state)).toBe(data);
@@ -85,19 +106,19 @@ describe('store/vacancy', () => {
     });
 
     describe('vacancyDoc', () => {
-      it('returns null when vacancy ID is not set', () => {
-        expect(getters.vacancyDoc(state)).toBe(null);
+      describe('when ID is set in the state', () => {
+        it('returns the document matching path vacancies/{vacancyId}', () => {
+          state.id = 'hsQqdvAfZpSw94X2B8nA';
+          const doc = getters.vacancyDoc(state);
+          expect(doc.id).toBe('hsQqdvAfZpSw94X2B8nA');
+          expect(doc.path).toBe('vacancies/hsQqdvAfZpSw94X2B8nA');
+        });
       });
-      it('returns a Firestore DocumentReference object', () => {
-        state.id = 'hsQqdvAfZpSw94X2B8nA';
-        const doc = getters.vacancyDoc(state);
-        expect(doc).toBeInstanceOf(firebase.firestore.DocumentReference);
-      });
-      it('the document path matches vacancies/{vacancyId}', () => {
-        state.id = 'hsQqdvAfZpSw94X2B8nA';
-        const doc = getters.vacancyDoc(state);
-        expect(doc.id).toBe('hsQqdvAfZpSw94X2B8nA');
-        expect(doc.path).toBe('vacancies/hsQqdvAfZpSw94X2B8nA');
+
+      describe('when ID in state is null', () => {
+        it('returns null', () => {
+          expect(getters.vacancyDoc(state)).toBe(null);
+        });
       });
     });
   });
