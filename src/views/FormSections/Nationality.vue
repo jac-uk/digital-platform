0acd8a4581845ceebc152047310995dedb3bfec4
Ollie Treend <ollie@treend.com>
2019-02-14 16:24:31 +0000

    Move Nationality question to its own page
    The 'Your Nationality' fields have been moved to their own page, which is displayed before the 'Personal Details' page.

--- src/views/FormSections/Nationality.vue
+++ src/views/FormSections/Nationality.vue
@@ -2,26 +2,56 @@
   <section>
     <form @submit.prevent="save">
       <h2>Your Nationality</h2>
-      <h3>Are you eligible to apply?</h3>
-      <p>You need to be a citizen of:</p>
-      <ul>
-        <li>the United Kingdom, or</li>
-        <li>another Commonwealth country, or</li>
-        <li>the Republic of Ireland.</li>
-      </ul>
 
-      <div class="form-check">
-        <input class="form-check-input" type="radio" id="eligibility_no" :value="false" v-model="applicant.eligible">
-        <label class="form-check-label" for="eligibility_no">
-          No, I'm not eligible
-        </label>
-      </div>
-      <div class="form-check">
-        <input class="form-check-input" type="radio" id="eligibility_yes" :value="true" v-model="applicant.eligible">
-        <label class="form-check-label" for="eligibility_yes">
-          Yes, I'm eligible
-        </label>
-      </div>
+      <fieldset>
+        <legend>Your Nationality</legend>
+        <div class="fieldset-text">
+          We need to know your nationality to determine your eligibility for this role.
+        </div>
+        <details class="fieldset-text">
+          <summary>Which nationalities are eligible for this role?</summary>
+          <div>
+            <p>To be eligible for this role, you need to be a citizen of:</p>
+            <ul>
+              <li>the United Kingdom, or</li>
+              <li>another Commonwealth country, or</li>
+              <li>the Republic of Ireland.</li>
+            </ul>
+            <p>Unfortunately other nationalities will not be considered for this role.</p>
+          </div>
+        </details>
+        <p>I am a citizen of:</p>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" type="radio" id="nationality_uk" value="United Kingdom" v-model="applicant.nationality">
+          <label class="custom-control-label" for="nationality_uk">
+            the United Kingdom
+          </label>
+        </div>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" type="radio" id="nationality_commonwealth" value="Commonwealth" v-model="applicant.nationality">
+          <label class="custom-control-label" for="nationality_commonwealth">
+            another Commonwealth country
+          </label>
+          <select class="form-control" v-model="applicant.commonwealth_country" v-if="applicant.nationality === 'Commonwealth'">
+            <option :value="undefined">Please specify...</option>
+            <option v-for="country in commonwealthCountries" :value="country" :key="country">
+              {{country}}
+            </option>
+          </select>
+        </div>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" type="radio" id="nationality_ireland" value="Republic of Ireland" v-model="applicant.nationality">
+          <label class="custom-control-label" for="nationality_ireland">
+            the Republic of Ireland
+          </label>
+        </div>
+        <div class="custom-control custom-radio">
+          <input class="custom-control-input" type="radio" id="nationality_other" value="Other" v-model="applicant.nationality">
+          <label class="custom-control-label" for="nationality_other">
+            none of the above
+          </label>
+        </div>
+      </fieldset>
 
       <div class="form-actions">
         <button class="btn btn-primary mr-2" type="button" @click.prevent="saveAndContinue">Save and Continue</button>
@@ -35,12 +65,15 @@
 </template>
 
 <script>
+  import commonwealthCountries from '@/datalists/commonwealth-countries';
+
   export default {
     name: "Nationality",
     data() {
       return {
         applicant: this.$store.getters.applicant(),
         isSaving: false,
+        commonwealthCountries,
       };
     },
     methods: {
