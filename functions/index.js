fd450976578fd6f4b64dfc433634946248ba83e4
Todd Tyree <tatyree@gmail.com>
2019-02-19 14:37:58 +0000

    Set up failing version of most basic test
    Also, not the comment regarding when you have to mock
    `functions.config()`.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,6 @@
-use "strict"
-
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const NotifyClient = require('notifications-node-client').NotifyClient
-const notifyClient = new NotifyClient(functions.config().notify.api_key)
+const notifyClient = new NotifyClient(functions.config().notify.key)
 
-admin.initializeApp();
+admin.initializeApp()
