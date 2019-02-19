3abc1fcbcd928819ffea63a7a4939516fa8adf40
Todd Tyree <tatyree@gmail.com>
2019-02-19 11:25:34 +0000

    Remove sendgrid packages
    Our GOVUK Notify account is now active, so we no longer require Sendgrid
    for this.

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,5 @@ use "strict"
 
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
-const sgMail = require("@sendgrid/mail");
 
 admin.initializeApp();
--- tests/unit/functions/index.spec.js
+++ tests/unit/functions/index.spec.js
@@ -1,5 +1,4 @@
 const test = require("../../../functions/node_modules/firebase-functions-test")();
-const sendgrid = require("../../../functions/node_modules/@sendgrid/mail");
 
 it("Dummy test (remove when there are real tests", () => {
   expect(true).toEqual(true)
