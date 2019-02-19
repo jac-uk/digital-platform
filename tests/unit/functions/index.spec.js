92892356ffabdd542f5ada818acfe9abd1bda906
Todd Tyree <tatyree@gmail.com>
2019-02-19 14:37:58 +0000

    Set up failing version of most basic test
    Also, not the comment regarding when you have to mock
    `functions.config()`.

--- tests/unit/functions/index.spec.js
+++ tests/unit/functions/index.spec.js
@@ -1,12 +1,35 @@
-const test = require("../../../functions/node_modules/firebase-functions-test")();
-const NotifyClient = require("../../../functions/node_modules/notifications-node-client").NotifyClient;
-jest.mock("../../../functions/node_modules/notifications-node-client");
+const NotifyClient = require("../../../functions/node_modules/notifications-node-client").NotifyClient
+const test = require("../../../functions/node_modules/firebase-functions-test")()
+
+// mockConfig has to happen BEFORE requiring index.js
+test.mockConfig({ notify: { key: "deadbeef" } });
+
+const jacFunctions = require("../../../functions/index")
+jest.mock("../../../functions/node_modules/notifications-node-client")
 
 beforeEach(() => {
-  NotifyClient.mockClear();
-});
+  NotifyClient.mockClear()
+})
 
 it('Check that the consumer called the class constructor', () => {
-  const notifyClient = new NotifyClient();
-  expect(NotifyClient).toHaveBeenCalledTimes(1);
-});
+  const notifyClient = new NotifyClient()
+  expect(NotifyClient).toHaveBeenCalledTimes(1)
+})
+
+describe("Validation email", () => {
+  const mockEvent = {
+    data: {
+      email: "test@test.com",
+      displayName: "Test User"
+    }
+  };
+
+  it("is sent when a user registers", () => {
+    const notifyClient = new NotifyClient()
+    jacFunctions.sendValidationEmail(mockEvent).then(() => {
+      expect(NotifyClient).toHaveBeenCalledTimes(1)
+    })
+  })
+})
+
+
