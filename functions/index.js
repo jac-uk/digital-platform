cfdfb89107b2a8c2ba632155d9230c775632556c
Todd Tyree <tatyree@gmail.com>
2019-02-19 12:32:48 +0000

    Add most basic Notify configuration and docs
    I am going to mock these in the tests in a separate commit to make it
    easier to follow.

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,7 @@ use "strict"
 
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const NotifyClient = require('notifications-node-client').NotifyClient
+const notifyClient = new NotifyClient(functions.config().notify.api_key)
 
 admin.initializeApp();
