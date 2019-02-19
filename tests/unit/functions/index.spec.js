217070b45c1754bb8a1375b1df7aee296c9fc588
Todd Tyree <tatyree@gmail.com>
2019-02-19 13:53:03 +0000

    Ensure jest can mock the notify client
    I'm new to jest and the notifiy node client, so I'm changing out the
    `true===true` for a test that actually serves a purpose.

--- tests/unit/functions/index.spec.js
+++ tests/unit/functions/index.spec.js
@@ -1,5 +1,12 @@
 const test = require("../../../functions/node_modules/firebase-functions-test")();
+const NotifyClient = require("../../../functions/node_modules/notifications-node-client").NotifyClient;
+jest.mock("../../../functions/node_modules/notifications-node-client");
 
-it("Dummy test (remove when there are real tests", () => {
-  expect(true).toEqual(true)
+beforeEach(() => {
+  NotifyClient.mockClear();
+});
+
+it('Check that the consumer called the class constructor', () => {
+  const notifyClient = new NotifyClient();
+  expect(NotifyClient).toHaveBeenCalledTimes(1);
 });
