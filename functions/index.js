2c560d9bcc59b87451b496ef88212a460e0c0bc9
Todd Tyree <tatyree@gmail.com>
2019-02-21 14:54:33 +0000

    Ensure tests pass with ES6 class instantiation
    It is probably also safter to make a new instance every time, if a bit
    less efficient.

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const NotifyClient = require('notifications-node-client').NotifyClient
-const notifyClient = new NotifyClient(functions.config().notify.key)
 
 admin.initializeApp()
+
+exports.sendValidationEmail = functions.auth.user().onCreate((user) => {
+  const notifyClient = new NotifyClient(functions.config().notify.key)
+  const email = user.email
+  const displayName = user.displayName
+
+  return true
+});
