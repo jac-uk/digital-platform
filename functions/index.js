505c673e83443cc235c474c34c331d1181ef6bb8
Todd Tyree <tatyree@gmail.com>
2019-02-24 12:22:31 +0000

    Mock calls for .getEmailVerificaitonLink

--- functions/index.js
+++ functions/index.js
@@ -5,15 +5,22 @@ const NotifyClient = require('notifications-node-client').NotifyClient
 admin.initializeApp()
 
 exports.sendValidationEmail = functions.auth.user().onCreate((user) => {
-  const notifyClient = new NotifyClient(functions.config().notify.key)
-  const email = user.email
-  const displayName = user.displayName
+	const notifyClient = new NotifyClient(functions.config().notify.key)
+	const email = user.email
+	const displayName = user.displayName
 
-  notifyClient
-    .sendEmail(
-      functions.config().notify.templates.validation,
-      email,
-      { personalisation: { } }
-    )
-  return true
+	admin.auth().generateEmailVerificationLink(email)
+		.then((link) => {
+			// Construct email verification template, embed the link and send
+			// using custom SMTP server.
+			return true
+		})
+
+	notifyClient
+		.sendEmail(
+			functions.config().notify.templates.validation,
+			email,
+			{ personalisation: { } }
+		)
+	return true
 });
