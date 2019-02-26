63546fa90049255c8cfb013f837cfb2196df1970
Todd Tyree <tatyree@gmail.com>
2019-02-26 16:02:27 +0000

    Call notify client to send validation email
    Sort out the bad tabs as well.

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,26 @@
 const admin = require("firebase-admin");
-const functions = require("firebase-functions");
 const NotifyClient = require('notifications-node-client').NotifyClient
 
 admin.initializeApp()
+const functions = require("firebase-functions");
 
 exports.sendVerificationEmail = functions.auth.user().onCreate((user) => {
-	const notifyClient = new NotifyClient(functions.config().notify.key)
-	const email = user.email
+  const notifyClient = new NotifyClient(functions.config().notify.key)
+  const email = user.email
   const displayName = user.displayName
 
-	const callNotifyClient = (email, displayName, link) => {
-		notifyClient
-			.sendEmail(
-				functions.config().notify.templates.verification,
-				email,
-				{ personalisation: { } }
-			)
-	}
+  const sendEmail = (link) => {
+    notifyClient
+      .sendEmail(
+        functions.config().notify.templates.verification,
+        email,
+        { personalisation: { verificationLink: link } }
+      )
+      .then(() => console.info({ type: "verification Link", email: email }))
+      .catch(error => console.error(error))
+  }
 
-	return admin.auth().generateEmailVerificationLink(email)
-    .then((link) => {
-      return callNotifyClient(email, displayName, link)
-    })
+  return admin.auth().generateEmailVerificationLink(email)
+    .then(link => sendEmail(link))
+    .catch(error => console.error(error))
 });
