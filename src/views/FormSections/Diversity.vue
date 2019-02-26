77ea05c320fab91c96f4f4fcf9dde995bd8fa440
Ollie Treend <ollie@treend.com>
2019-02-26 14:02:12 +0000

    Add more questions to the Diversity page
    This commit adds the questions 'Do you hold, or have you held in the past, a fee-paid judicial role?' and 'What is your ethnic group?'

--- src/views/FormSections/Diversity.vue
+++ src/views/FormSections/Diversity.vue
@@ -131,6 +131,178 @@
             <input v-if="applicant.current_role_has_other" type="text" class="form-control" v-model="applicant.current_role_other">
           </div>
         </fieldset>
+
+        <fieldset>
+          <legend>Do you hold, or have you held in the past, a fee-paid judicial role?</legend>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="fee_paid_judicial_court" value="Fee-paid court post" v-model="applicant.fee_paid_judicial">
+            <label class="custom-control-label" for="fee_paid_judicial_court">
+              Fee-paid court post
+            </label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="fee_paid_judicial_tribunal" value="Fee-paid tribunal post" v-model="applicant.fee_paid_judicial">
+            <label class="custom-control-label" for="fee_paid_judicial_tribunal">
+              Fee-paid tribunal post
+            </label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="fee_paid_judicial_jo" value="Other fee-paid judicial office holder" v-model="applicant.fee_paid_judicial">
+            <label class="custom-control-label" for="fee_paid_judicial_jo">
+              Other fee-paid judicial office holder
+            </label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="fee_paid_judicial_no" value="I have not previously held a fee-paid role" v-model="applicant.fee_paid_judicial">
+            <label class="custom-control-label" for="fee_paid_judicial_no">
+              I have not previously held a fee-paid role
+            </label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input class="custom-control-input" type="checkbox" id="fee_paid_judicial_no_answer" value="I prefer not to answer this question" v-model="applicant.fee_paid_judicial">
+            <label class="custom-control-label" for="fee_paid_judicial_no_answer">
+              I prefer not to answer this question
+            </label>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>What is your ethnic group?</legend>
+
+          <section class="mb-3">
+            <h6>White</h6>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_white_british" value="English, Welsh, Scottish, Northern Ireland, British" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_white_british">
+                English, Welsh, Scottish, Northern Ireland, British
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_white_irish" value="Irish" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_white_irish">
+                Irish
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_white_gypsy" value="Gypsy or Irish Traveller" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_white_gypsy">
+                Gypsy or Irish Traveller
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_white_other" value="Any other White background" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_white_other">
+                Any other White background
+              </label>
+            </div>
+          </section>
+
+          <section class="mb-3">
+            <h6>Mixed/multiple ethnic backgrounds</h6>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_mixed_caribbean" value="White and Black Caribbean" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_mixed_caribbean">
+                White and Black Caribbean
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_mixed_african" value="White and Black African" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_mixed_african">
+                White and Black African
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_mixed_asian" value="White and Asian" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_mixed_asian">
+                White and Asian
+              </label>
+            </div>
+          </section>
+
+          <section class="mb-3">
+            <h6>Any other mixed/multiple ethinic background</h6>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other_mixed_asian" value="Asian/Asian British" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other_mixed_asian">
+                Asian/Asian British
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other_mixed_indian" value="Indian" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other_mixed_indian">
+                Indian
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other_mixed_pakistani" value="Pakistani" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other_mixed_pakistani">
+                Pakistani
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other_mixed_bangladeshi" value="Bangladeshi" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other_mixed_bangladeshi">
+                Bangladeshi
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other_mixed_chinese" value="Chinese" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other_mixed_chinese">
+                Chinese
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other_mixed_other" value="Any other Asian background" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other_mixed_other">
+                Any other Asian background
+              </label>
+            </div>
+          </section>
+
+          <section class="mb-3">
+            <h6>Black/African/Caribbean/Black British</h6>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_african_african" value="African" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_african_african">
+                African
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_african_caribbean" value="Caribbean" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_african_caribbean">
+                Caribbean
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_african_other" value="Any other Black/African/Caribbean background" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_african_other">
+                Any other Black/African/Caribbean background
+              </label>
+            </div>
+          </section>
+
+          <section class="mb-3">
+            <h6>Other ethnic group</h6>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other_arab" value="Arab" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other_arab">
+                Arab
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_other" value="Other" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_other">
+                Other
+              </label>
+            </div>
+            <div class="custom-control custom-radio">
+              <input class="custom-control-input" type="radio" id="ethnicity_no_answer" value="I prefer not to answer this question" v-model="applicant.ethnicity">
+              <label class="custom-control-label" for="ethnicity_no_answer">
+                I prefer not to answer this question
+              </label>
+            </div>
+          </section>
+        </fieldset>
       </div>
 
       <div class="form-actions">
@@ -156,6 +328,9 @@
       }
       if (!applicant.current_role) {
         applicant.current_role = [];
+      }
+      if (!applicant.fee_paid_judicial) {
+        applicant.fee_paid_judicial = [];
       }
 
       return {
