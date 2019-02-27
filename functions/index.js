d47870a0f8883aaff26574c3dedd407167717227
Todd Tyree <tatyree@gmail.com>
2019-02-27 14:40:01 +0000

    Abstract sendEmail logic & notify client setup
    Still need to abstract the personalisation object.  Will do that when I
    build the confirmation email function.

--- functions/index.js
+++ functions/index.js
@@ -3,23 +3,21 @@ const NotifyClient = require("notifications-node-client").NotifyClient
 
 admin.initializeApp()
 const functions = require("firebase-functions");
+const notifyClient = new NotifyClient(functions.config().notify.key)
+
+const sendEmail = (email, link, template) => {
+  notifyClient
+    .sendEmail(
+      template,
+      email,
+      { personalisation: { verificationLink: link } }
+    )
+    .then(() => console.info({ type: "verification link", email: email }))
+    .catch(error => console.error(error))
+}
 
 exports.sendVerificationEmail = functions.auth.user().onCreate((user) => {
-  const notifyClient = new NotifyClient(functions.config().notify.key)
-  const email = user.email
-
-  const sendEmail = (link) => {
-    notifyClient
-      .sendEmail(
-        functions.config().notify.templates.verification,
-        email,
-        { personalisation: { verificationLink: link } }
-      )
-      .then(() => console.info({ type: "verification link", email: email }))
-      .catch(error => console.error(error))
-  }
-
-  return admin.auth().generateEmailVerificationLink(email)
-    .then(link => sendEmail(link))
+  return admin.auth().generateEmailVerificationLink(user.email)
+    .then(link => sendEmail(user.email, link, functions.config().notify.templates.verification))
     .catch(error => console.error(error))
 });
