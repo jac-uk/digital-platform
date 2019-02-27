11b736190e2ca334db2803a1f4a4345a0408a217
Todd Tyree <tatyree@gmail.com>
2019-02-27 15:50:20 +0000

    Abstract personalisation object into function
    This is necessairly going to change depending on the template, so it
    makes sense to make sure it's handled at the level of the initial email
    setup.

--- functions/index.js
+++ functions/index.js
@@ -5,19 +5,22 @@ admin.initializeApp()
 const functions = require("firebase-functions");
 const notifyClient = new NotifyClient(functions.config().notify.key)
 
-const sendEmail = (email, link, template) => {
+const sendEmail = (email, link, template, personalisation) => {
   notifyClient
-    .sendEmail(
-      template,
-      email,
-      { personalisation: { verificationLink: link } }
-    )
+    .sendEmail(template, email, personalisation)
     .then(() => console.info({ type: "verification link", email: email }))
     .catch(error => console.error(error))
 }
 
 exports.sendVerificationEmail = functions.auth.user().onCreate((user) => {
   return admin.auth().generateEmailVerificationLink(user.email)
-    .then(link => sendEmail(user.email, link, functions.config().notify.templates.verification))
+    .then((link) => {
+      return sendEmail(
+        user.email,
+        link,
+        functions.config().notify.templates.verification,
+        { personalisation: { verificationLink: link } }
+      )
+    })
     .catch(error => console.error(error))
 });
