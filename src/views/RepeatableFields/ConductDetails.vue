5655eee6fc825a6aa6217e3b141061316a6b24b8
Ollie Treend <ollie@treend.com>
2019-03-08 12:31:31 +0000

    Add questions to Character page
    This commit adds questions to the Character page. Currently, after answering yes, the relevant repeatable component is displayed but the questions aren't correct in these yet.

---
+++ src/views/RepeatableFields/ConductDetails.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="form-group">
+      <label :for="inputIds.name">Conduct Details</label>
+      <input type="text" class="form-control" :id="inputIds.name" v-model="row.name">
+    </div>
+    <slot name="removeButton"></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ConductDetails',
+    props: [
+      'row',
+      'index',
+    ],
+    data() {
+      return {
+        inputIds: {
+          name: `legal_association_${this.index}_name`,
+        }
+      };
+    },
+  }
+</script>
