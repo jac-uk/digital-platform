51a0a3f66bcc941e63b2b899369813c581a5b58f
Ollie Treend <ollie@treend.com>
2019-03-11 15:59:35 +0000

    Make `ApplicationNav` menu dynamic
    This commit refactors the `ApplicationNav` component to use the Vuex `navigation` module as the source of truth for the application form's navigation menu. This is in favour of the previously hardcoded list of links in the component.

--- src/components/ApplicationNav.vue
+++ src/components/ApplicationNav.vue
@@ -1,46 +1,19 @@
 <template>
   <ol class="nav nav-pills flex-column">
-    <li class="nav-item">
-      <RouterLink to="/apply" class="nav-link" active-class="active" exact>Introduction</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/nationality" class="nav-link" active-class="active">Nationality</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/personal" class="nav-link" active-class="active">Personal Details</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/qualifications" class="nav-link" active-class="active">Qualifications</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/experience" class="nav-link" active-class="active">Experience</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/self-assessment" class="nav-link" active-class="active">Self Assessment</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/assessors" class="nav-link" active-class="active">Assessors</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/character" class="nav-link" active-class="active">Character</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/preferences" class="nav-link" active-class="active">Preferences</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/declarations" class="nav-link" active-class="active">Declarations</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/diversity" class="nav-link" active-class="active">Diversity</RouterLink>
-    </li>
-    <li class="nav-item">
-      <RouterLink to="/apply/outreach" class="nav-link" active-class="active">Outreach</RouterLink>
+    <li v-for="page in applyPages" :key="page.path" class="nav-item">
+      <RouterLink :to="page.path" class="nav-link" active-class="active" exact>
+        {{page.title}}
+      </RouterLink>
     </li>
   </ol>
 </template>
 
 <script>
   export default {
-    name: 'ApplicationNav'
+    data() {
+      return {
+        applyPages: this.$store.getters.applyPages
+      };
+    }
   }
 </script>
