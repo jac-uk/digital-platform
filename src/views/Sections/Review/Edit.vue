464c0951c8b691c986110f15809ddd3ae1701b37
Todd Tyree <tatyree@gmail.com>
2019-03-13 15:17:05 +0000

    Add Assessors to Review Page

--- src/views/Sections/Review/Edit.vue
+++ src/views/Sections/Review/Edit.vue
@@ -5,6 +5,7 @@
     <Qualifications />
     <Experience />
     <SelfAssessment />
+    <Assessors />
   </section>
 </template>
 
@@ -13,12 +14,14 @@ import Experience from '@/views/Sections/Experience/Show';
 import Personal from '@/views/Sections/Personal/Show';
 import Qualifications from '@/views/Sections/Qualifications/Show';
 import SelfAssessment from '@/views/Sections/SelfAssessment/Show';
+import Assessors from '@/views/Sections/Assessors/Show';
 export default {
   components: {
     Experience,
     Personal,
     Qualifications,
     SelfAssessment,
+    Assessors,
   },
   data() {
     return {
