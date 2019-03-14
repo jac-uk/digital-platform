57c4a37629387b579deb84fd04637b69fc415b61
Ollie Treend <ollie@treend.com>
2019-03-14 13:24:39 +0000

    Add styling to 'verify your email' page
    Put the page in a container to centrally align it, and use the new `<details>` styles.

--- src/views/VerifyEmail.vue
+++ src/views/VerifyEmail.vue
@@ -1,20 +1,22 @@
 <template>
-  <div>
-    <h3>Verify your email address</h3>
+  <main class="container">
+    <h1>Verify your email address</h1>
     <p>The JAC needs to make sure we have the right email address for you.</p>
     <p>We've sent an email to <strong>{{email}}</strong> containing a link to verify your email address.</p>
     <p>Follow the link to complete your registration and start your application.</p>
     <details>
       <summary>Help! I haven't received the email yet</summary>
-      <p>It could take up to 10 minutes for the email to arrive.</p>
-      <p>Don't forget to check your 'spam' or 'junk mail' folder in case it didn't go into your inbox.</p>
-      <p>If it still hasn't arrived after 10 minutes, you can resend the email:</p>
-      <button class="btn btn-primary" type="button" :disabled="sendInProgress" @click.prevent="sendVerificationEmail">
-        Resend email
-      </button>
-      <span class="spinner-border spinner-border-sm text-secondary ml-2" v-if="sendInProgress"></span>
+      <section>
+        <p>It could take up to 10 minutes for the email to arrive.</p>
+        <p>Don't forget to check your 'spam' or 'junk mail' folder in case it didn't go into your inbox.</p>
+        <p>If it still hasn't arrived after 10 minutes, you can resend the email:</p>
+        <button class="btn btn-primary" type="button" :disabled="sendInProgress" @click.prevent="sendVerificationEmail">
+          Resend email
+        </button>
+        <span class="spinner-border spinner-border-sm text-secondary ml-2" v-if="sendInProgress"></span>
+      </section>
     </details>
-  </div>
+  </main>
 </template>
 
 <script>
