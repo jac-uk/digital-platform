f5a9fdfc9d47b7f7d7e57f79ee9211d116e88c45
Ollie Treend <ollie@treend.com>
2019-03-14 10:09:26 +0000

    Allow re-sending verification emails via `onCall` Cloud Function
    This commit adds an `onCall` handler which allows users to re-send their verification email, plus refactors the existing Cloud Function to be more abstracted and reusable.
    
    - Create a generic `sendEmail` method which will send an email via the GOV.UK Notify service. This has been abstracted because other Cloud Functions in the future will also need to send emails.
    - Create a local `sendVerificationEmail` method which will accept a user's email address, generate a verification link, and email it to them.
    - Refactor the `auth.user().onCreate()` handler to use the new `sendVerificationEmail` method
    - Register a new `onCall()` handler to use the `sendVerificationEmail` method. This will be used to allow users to re-send a verification email (typically this will be because the first email didn't come through, or it went to spam, etc.)
    
    Additionally, I've run `npm update` to keep locked dependencies up to date.

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,48 @@
 const admin = require("firebase-admin");
-const NotifyClient = require("notifications-node-client").NotifyClient
-
-admin.initializeApp()
 const functions = require("firebase-functions");
+const NotifyClient = require("notifications-node-client").NotifyClient;
 
-exports.sendVerificationEmail = functions.auth.user().onCreate((user) => {
-  const notifyClient = new NotifyClient(functions.config().notify.key)
-  const email = user.email
+admin.initializeApp();
 
-  const sendEmail = (link) => {
-    notifyClient
-      .sendEmail(
-        functions.config().notify.templates.verification,
-        email,
-        { personalisation: { verificationLink: link } }
-      )
-      .then(() => console.info({ type: "verification link", email: email }))
-      .catch(error => console.error(error))
-  }
+const sendEmail = (email, templateId, personalisation) => {
+  const client = new NotifyClient(functions.config().notify.key);
 
+  console.info({
+    action: 'Sending email',
+    email,
+    templateId,
+    personalisation,
+  });
+
+  return client
+    .sendEmail(
+      templateId,
+      email,
+      {personalisation}
+    )
+    .then(notifyResponse => {
+      console.info(JSON.parse(JSON.stringify(notifyResponse)));
+      return true;
+    });
+};
+
+const sendVerificationEmail = (email) => {
   return admin.auth().generateEmailVerificationLink(email)
-    .then(link => sendEmail(link))
-    .catch(error => console.error(error))
+    .then(link => {
+      const templateId = functions.config().notify.templates.verification;
+      const personalisation = {
+        verificationLink: link,
+      };
+      return sendEmail(email, templateId, personalisation);
+    });
+};
+
+exports.sendVerificationEmailOnNewUser = functions.auth.user().onCreate((user) => {
+  const email = user.email;
+  return sendVerificationEmail(email);
+});
+
+exports.sendVerificationEmail = functions.https.onCall((data, context) => {
+  const email = context.auth.token.email;
+  return sendVerificationEmail(email);
 });
