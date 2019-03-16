1541005feae33af0a72357c68497368efe70f092
Ollie Treend <ollie@treend.com>
2019-03-16 12:19:37 +0000

    Adjust 'Self Assessment' questions page
    To match content & questions defined in JudicialAppointmentsCommission/digital-platform#144

--- src/views/Sections/SelfAssessment/Edit.vue
+++ src/views/Sections/SelfAssessment/Edit.vue
@@ -16,10 +16,57 @@
       </div>
 
       <fieldset :disabled="application.state === 'submitted'">
-        <legend>Additional Selection Criteria</legend>
+        <legend>Self Assessment</legend>
+        <div class="fieldset-text">
+          <p>Please refer to the
+          <a href="https://www.judicialappointments.gov.uk/sites/default/files/sync/128_final_competency_framework.pdf" target="_blank">guidance and competency framework</a>
+            on the JAC website before completing this part of your application.</p>
+          <p>You have a maximum of 250 words for each competency.</p>
+        </div>
+        <div class="form-group">
+          <label for="leadership">Leadership</label>
+          <textarea class="form-control" id="leadership" rows="9" v-model="application.competency_leadership"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="exercising_judgement">Exercising Judgement</label>
+          <textarea class="form-control" id="exercising_judgement" rows="9" v-model="application.competency_exercising_judgement"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="possessing_and_building_knowledge">Possessing and Building Knowledge</label>
+          <textarea class="form-control" id="possessing_and_building_knowledge" rows="9" v-model="application.competency_possessing_and_building_knowledge"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="assimilating_and_clarifying_information">Assimilating and Clarifying Information</label>
+          <textarea class="form-control" id="assimilating_and_clarifying_information" rows="9" v-model="application.competency_assimilating_and_clarifying_information"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="working_and_communicating_with_others">Working and Communicating with Others</label>
+          <textarea class="form-control" id="working_and_communicating_with_others" rows="9" v-model="application.competency_working_and_communicating_with_others"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="managing_work_effectively">Managing Work Effectively</label>
+          <textarea class="form-control" id="managing_work_effectively" rows="9" v-model="application.competency_managing_work_effectively"></textarea>
+        </div>
+      </fieldset>
+
+      <fieldset :disabled="application.state === 'submitted'">
+        <legend>Additional Selection Criterion</legend>
         <div class="form-group">
           <label for="additional_selection_criteria">How do you meet this requirement?</label>
-          <textarea class="form-control" id="additional_selection_criteria" rows="3" v-model="application.additional_selection_criteria"></textarea>
+          <textarea class="form-control" id="additional_selection_criteria" rows="9" v-model="application.additional_selection_criteria"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="additional_selection_criteria_details">Please provide details</label>
+          <textarea class="form-control" id="additional_selection_criteria_details" rows="9" v-model="application.additional_selection_criteria_details"></textarea>
+        </div>
+      </fieldset>
+
+      <fieldset :disabled="application.state === 'submitted'">
+        <legend>Reasonable Length of Service</legend>
+        <div class="fieldset-text">Refer to the Information Page</div>
+        <div class="form-group">
+          <label for="length_of_service_mitigation">If you are unable to provide 3 years’ service, please provide any mitigation you would like the Commission to consider</label>
+          <textarea class="form-control" id="length_of_service_mitigation" rows="3" v-model="application.length_of_service_mitigation"></textarea>
         </div>
       </fieldset>
 
@@ -27,33 +74,6 @@
         <legend>Authorisations</legend>
         <p>Do you currently have a Section 9(1) authorisation?</p>
         <BooleanInput v-model="application.has_section_9_1_authorisation" />
-      </fieldset>
-
-      <fieldset :disabled="application.state === 'submitted'">
-        <legend>Self Assessment</legend>
-        <div class="fieldset-text">
-          You may wish to refer to our guidance and the competency framework for this exercise before you complete this part of your application.
-        </div>
-        <div class="form-group">
-          <label for="exercising_judgement">Exercising Judgement</label>
-          <textarea class="form-control" id="exercising_judgement" rows="3" v-model="application.competency_exercising_judgement"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="possessing_and_building_knowledge">Possessing and Building Knowledge</label>
-          <textarea class="form-control" id="possessing_and_building_knowledge" rows="3" v-model="application.competency_possessing_and_building_knowledge"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="assimilating_and_clarifying_information">Assimilating and Clarifying Information</label>
-          <textarea class="form-control" id="assimilating_and_clarifying_information" rows="3" v-model="application.competency_assimilating_and_clarifying_information"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="working_and_communicating_with_others">Working and Communicating with others</label>
-          <textarea class="form-control" id="working_and_communicating_with_others" rows="3" v-model="application.competency_working_and_communicating_with_others"></textarea>
-        </div>
-        <div class="form-group">
-          <label for="managing_work_effectively">Managing Work Effectively</label>
-          <textarea class="form-control" id="managing_work_effectively" rows="3" v-model="application.competency_managing_work_effectively"></textarea>
-        </div>
       </fieldset>
 
       <SaveAndContinueButtons v-if="application.state !== 'submitted'" :isSaving="isSaving" @saveAndContinue="saveAndContinue" />
