22bc5ab93b9e97561173453c7e2bfac0c26f969b
Todd Tyree <tatyree@gmail.com>
2019-03-18 16:39:51 +0000

    Add user research question to review and submit
    Closes #68

--- src/views/Sections/Review/Edit.vue
+++ src/views/Sections/Review/Edit.vue
@@ -15,6 +15,14 @@
       You are applying for <strong>{{vacancy.jac_ref}}: {{vacancy.title}}</strong>
     </p>
     <form v-if="application.state != 'submitted'"  @submit.prevent="save">
+      <fieldset>
+        <legend>
+          We’re looking for people to take part in user research activities
+          such as testing a future version of this form. Tell us if you’re
+          happy for us to contact you.
+        </legend>
+        <BooleanInput v-model="application.willing_to_participate_in_user_research" />
+      </fieldset>
       <div class="form-actions">
         <button class="btn btn-primary mr-2" type="button" @click.prevent="saveAndSubmit">
           Submit application
@@ -26,6 +34,7 @@
 </template>
 
 <script>
+import BooleanInput from '@/components/BooleanInput';
 import Citizenship from '@/views/Sections/Citizenship/Show';
 import Experience from '@/views/Sections/Experience/Show';
 import Personal from '@/views/Sections/Personal/Show';
@@ -38,6 +47,7 @@ import Declarations from '@/views/Sections/Declarations/Show';
 import Diversity from '@/views/Sections/Diversity/Show';
 export default {
   components: {
+    BooleanInput,
     Citizenship,
     Experience,
     Personal,
