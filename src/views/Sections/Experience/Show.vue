bea5db80ffca4a2e4a89c2860d015d9abbddb8a6
Ollie Treend <ollie@treend.com>
2019-03-18 14:41:07 +0000

    Add form logic to Experience Review page
    This commit adds logic to display relevant fields based on the responses given in the Experience review section.
    Different fields are shown depending on whether this is a judicial appointment or not.

--- src/views/Sections/Experience/Show.vue
+++ src/views/Sections/Experience/Show.vue
@@ -5,46 +5,83 @@
     </div>
 
     <div class="card-body">
-      <table class="table mb-3" v-for="exp in applicant.experience" :key="exp.id">
+      <table class="table mb-3" v-for="exp in applicant.experience" :key="exp.title">
         <tbody>
           <tr>
-            <th>Title:</th>
+            <th scope="row">Title</th>
             <td>{{ exp.title }}</td>
           </tr>
           <tr>
-            <th>Current Role:</th>
-            <td>{{ exp.currently_in_job ? "Yes" : "No"}}</td>
+            <th scope="row">Date from</th>
+            <td>{{ exp.date_from.toLocaleDateString() }}</td>
           </tr>
-          <tr v-if="exp.appointment_type">
-            <th>Appointment Type:</th>
+          <tr>
+            <th scope="row">Date to</th>
+            <td v-if="exp.currently_in_job">I currently work in this job</td>
+            <td v-else>{{ exp.date_to.toLocaleDateString() }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Judicial appointment?</th>
+            <td>{{ exp.is_judicial_appointment ? 'Yes' : 'No' }}</td>
+          </tr>
+        </tbody>
+        <tbody v-if="exp.is_judicial_appointment">
+          <tr>
+            <th scope="row">Salaried or fee-paid?</th>
             <td>{{ exp.appointment_type }}</td>
           </tr>
-          <tr v-if="exp.date_from">
-            <th>Date From:</th>
-            <td>{{ exp.date_from.toLocaleDateString() }}</td>
-            <tr v-if="exp.date_to">
-              <th>Date To:</th>
-              <td>{{ exp.date_to.toLocaleDateString() }}</td>
-            </tr>
-            <tr v-if="exp.circuit">
-              <th>Circuit:</th>
-              <td>{{ exp.circuit }}</td>
-            </tr>
-            <tr v-if="exp.jurisdiction">
-              <th>Jurisdiction:</th>
-              <td>{{ exp.jurisdiction }}</td>
-            </tr>
-            <tr v-if="exp.region">
-              <th>Region:</th>
-              <td>{{ exp.region }}</td>
-            </tr>
-            <tr>
-              <td colspan="10">
-                <RouterLink to="/apply/experience" class="float-right">Change</RouterLink>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+          <tr>
+            <th scope="row">Jurisdiction</th>
+            <td>{{ exp.jurisdiction }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Circuit</th>
+            <td>{{ exp.circuit }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Days sat in appointment</th>
+            <td>{{ exp.days_sat }}</td>
+          </tr>
+        </tbody>
+        <tbody v-if="!exp.is_judicial_appointment">
+          <tr>
+            <th scope="row">In Government Legal Service or Crown Prosecution Service?</th>
+            <td>{{ exp.is_public_employee ? 'Yes' : 'No' }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Company or organisation</th>
+            <td>{{ exp.company }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Law-related activities</th>
+            <td>
+              <ul>
+                <li v-for="activity in exp.activities" :key="activity">
+                  {{ activity }}
+                </li>
+                <li v-if="exp.activities_has_other">
+                  Other: {{exp.activities_other}}
+                </li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Main duties</th>
+            <td>{{ exp.main_duties }}</td>
+          </tr>
+          <tr>
+            <th scope="row">Specialism/s</th>
+            <td>{{ exp.specialisms }}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="2">
+              <RouterLink to="/apply/experience" class="float-right">Change</RouterLink>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
 
         <table class="table">
           <tbody>
@@ -80,3 +117,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  th {
+    width: 40%;
+  }
+  tbody + tbody {
+    border-top: 0;
+  }
+  ul {
+    padding-left: 1.2rem;
+  }
+</style>
