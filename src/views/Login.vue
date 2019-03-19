cc0a201dd41254e8416ea56bb90d1cf6a68c6ec6
Ollie Treend <ollie@treend.com>
2019-03-19 12:11:12 +0000

    Add feedback banner to the login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,30 @@
 <template>
-  <main class="container">
-    <div class="text-center mb-3 mt-5">
-      <img src="@/assets/jac-logo.svg" alt="Judicial Appointments Commission" width="197" height="66">
+  <main class="mt-3">
+    <Banner class="text-center" />
+    <div class="login-container">
+      <div class="text-center mb-3 mt-5">
+        <img src="@/assets/jac-logo.svg" alt="Judicial Appointments Commission" width="197" height="66">
+      </div>
+      <p>
+        To apply for this role, you’ll need to create an account with your email address and a password. We’ll send you
+        a link to verify your email address.
+      </p>
+      <p>
+        If you’ve already registered, sign in to return to your application.
+      </p>
+      <FirebaseUI @signInSuccess="loginRedirect" />
     </div>
-    <p>
-      To apply for this role, you’ll need to create an account with your email address and a password. We’ll send you
-      a link to verify your email address.
-    </p>
-    <p>
-      If you’ve already registered, sign in to return to your application.
-    </p>
-    <FirebaseUI @signInSuccess="loginRedirect" />
   </main>
 </template>
 
 <script>
   import FirebaseUI from '@/components/FirebaseUI';
+  import Banner from '@/components/Banner';
 
   export default {
     components: {
-      FirebaseUI
+      FirebaseUI,
+      Banner,
     },
     methods: {
       loginRedirect(authResult) {
@@ -31,7 +36,8 @@
 </script>
 
 <style scoped>
-  .container {
+  .login-container {
     max-width: 360px;
+    margin: 0 auto;
   }
 </style>
