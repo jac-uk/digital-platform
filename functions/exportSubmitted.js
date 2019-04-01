b019a179b477effd1d4096f1cc9c3db6706c7cdd
Ollie Treend <ollie@treend.com>
2019-04-01 17:58:44 +0100

    Complete export functionality in `functions/exportSubmitted.js`
    I've refactored this method to use `async/await` rather than chained Promises – mainly to help avoid Promise hell and aid code readability. This is especially helpful when considering the nested Promises which are used to find all associated `applicant` records for `application` documents.
    
    The result is a script which will export all data associated with submitted applications. It saves data to `functions/submitted.json`.

--- functions/exportSubmitted.js
+++ functions/exportSubmitted.js
@@ -8,53 +8,71 @@
  * node exportSubmitted.js
  * ```
  *
- * Counts number of applications at each available status, nothing more.
+ * Exports applicant and application data for submitted applications.
  *
  */
+const fs = require('fs');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
 const firestore = admin.firestore();
-// This will be deprecate soon and throws warnings.  For now, though, it is
+// This is deprecated and throws warnings.  For now, though, it is
 // easier than trying to recursively parse the data structure looking for
 // firestore Timestamps.
 firestore.settings({timestampsInSnapshots: false});
 
-const related = (collection, rid) => {
-  return firestore.collection(collection)
-    .get(rid)
-    .then(snap => {
-      records = {}
-      const data = snap.data();
-      Object.getOwnPropertyNames(data).sort().forEach((key) => {
-       records[key] = snap.get(key);
-      });
-      return records;
-    })
-    .catch(e => console.warn(e));
-}
+const sortObject = (object) => {
+  const newObject = {};
+  Object.keys(object).sort().forEach((key) => {
+    newObject[key] = object[key];
+  });
+  return newObject;
+};
 
-firestore.collection('applications')
-  .get()
-  .then(snapshot => {
-    const records = {};
-    snapshot.forEach((snap) => {
-      if (snap.get('state') === 'submitted') {
-        const data = snap.data();
-        const aid = snap.get('applicant').id;
-        records[aid] = { application: {} };
+const getRecords = async () => {
+  const applications = await firestore
+    .collection('applications')
+    .where('state', '==', 'submitted')
+    .get();
 
-        Object.getOwnPropertyNames(data).sort().forEach((key) => {
-          if (key !== 'applicant' && key !== 'vacancy') {
-            records[aid]['application'][key] = snap.get(key);
-          }
-        });
-      }
+  const getApplicants = [];
+  applications.forEach((application) => {
+    const docRef = application.get('applicant');
+    getApplicants.push(docRef.get());
+  });
+  const applicants = await Promise.all(getApplicants);
+
+  const records = [];
+  applications.forEach((application) => {
+    const applicant = applicants.find((applicant) => {
+      return applicant.id === application.get('applicant').id;
     });
-    return records;
-  })
-  .then((records) => {
-    return console.log(JSON.stringify(records, null, 2));
-  })
-  .catch(e => console.warn(e));
 
+    const applicationData = application.data();
+    delete applicationData.applicant;
+    delete applicationData.vacancy;
+
+    records.push({
+      applicationId: application.id,
+      application: sortObject(applicationData),
+      applicant: sortObject(applicant.data()),
+    });
+  });
+
+  return records;
+};
+
+const main = async () => {
+  const records = await getRecords();
+  const output = JSON.stringify(records, null, 2);
+  const outputFile = './submitted.json';
+  fs.writeFile(outputFile, output, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    };
+    console.log(`Submitted applications have been exported to ${outputFile}`);
+  });
+};
+
+main();
