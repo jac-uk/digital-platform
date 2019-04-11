82ea1d11a84be5317f7cb8b6bdb526dae0cc6b9f
Ollie Treend <ollie@treend.com>
2019-04-11 17:18:57 +0100

    Add unit tests to QT
    This commit adds unit tests which were already implemented on the Application Form.
    
    As already discussed, reused components should at some point be split out into a shared npm package to avoid duplication.

--- hosting/qt/jest.config.js
+++ hosting/qt/jest.config.js
@@ -10,9 +10,6 @@ module.exports = {
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
   },
-  transformIgnorePatterns: [
-    '/node_modules/'
-  ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
@@ -22,5 +19,13 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/'
+  testURL: 'http://localhost/',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}'
+  ],
+  coverageReporters: [
+    'html',
+    'text-summary'
+  ]
 }
