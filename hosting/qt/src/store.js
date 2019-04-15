8c8e226782464ea0c9d526ca2b2032cfe21051ba
Ollie Treend <ollie@treend.com>
2019-04-15 13:27:20 +0100

    Add test for user login journey
    This commit adds a test for the user's login journey. The user must be authentictaed to see the 'Take Test' page. Unauthenticated users should be redirected to the login page.
    
    I'm parking this test for now, but it could be extended in the future to include other parts of the journey. As it stands, this can be used to test the router.

--- hosting/qt/src/store.js
+++ hosting/qt/src/store.js
@@ -1,8 +1,5 @@
-import Vue from 'vue';
 import Vuex from 'vuex';
 import auth from '@/store/auth';
-
-Vue.use(Vuex);
 
 const store = new Vuex.Store({
   // Don't use strict mode in production for performance reasons (https://vuex.vuejs.org/guide/strict.html)
---
+++ hosting/qt/tests/unit/journeys/login.spec.js
@@ -0,0 +1,65 @@
+import {shallowMount, createLocalVue} from '@vue/test-utils';
+import App from '@/App';
+import Router from 'vue-router';
+import Vuex from 'vuex';
+
+jest.mock('@/firebase', () => ({
+  auth: jest.fn(() => ({
+    signOut: jest.fn()
+  }))
+}));
+
+describe('Login journey', () => {
+  let subject;
+  let router;
+  let store;
+  beforeEach(() => {
+    const localVue = createLocalVue();
+    localVue.use(Router);
+    localVue.use(Vuex);
+
+    router = require('@/router').default;
+    store = require('@/store').default;
+
+    /**
+     * Disable custom scrollBehaviour function: window.scrollTo isn't implemented in jsdom, which is what jest uses to
+     * emulate the browser, so we won't test this behaviour.
+     * Without removing it, jsdom throws errors due to use of unimplemented functionality.
+     */
+    delete router.options.scrollBehavior;
+
+    subject = shallowMount(App, {
+      localVue,
+      router,
+      store,
+    });
+  });
+
+  describe('unauthenticated user', () => {
+    describe('when I visit /', () => {
+      it('redirects to /login', () => {
+        router.push('/');
+        expect(subject.vm.$route.path).toBe('/login');
+      });
+    });
+  });
+
+  describe('authenticated user', () => {
+    const user = {
+      uid: 'abc123',
+      email: 'user@example.com',
+      emailVerified: true,
+    };
+
+    beforeEach(() => {
+      store.dispatch('setCurrentUser', user);
+    });
+
+    describe('when I visit /', () => {
+      it('redirects to /take-test', () => {
+        router.push('/');
+        expect(subject.vm.$route.path).toBe('/take-test');
+      });
+    });
+  });
+});
