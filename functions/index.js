92639ddb47c5d8f8f6645bb9c2ebdbc137c8807d
Todd Tyree <tatyree@gmail.com>
2019-04-16 11:49:26 +0100

    Add function to add finisedAt to summaries

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const NotifyClient = require("notifications-node-client").NotifyClient;
 const qtSubmission = require("./qt/submission");
+exports.writeQtSummary = require("./qt/summary");
 
 admin.initializeApp();
 
