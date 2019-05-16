477f5e306bd4947d7b9baa1a7e15ad66e47f0b0a
Ollie Treend <ollie@treend.com>
2019-05-16 13:01:09 +0100

    Content changes to 'verify your email' page

--- hosting/qt/src/views/VerifyEmail.vue
+++ hosting/qt/src/views/VerifyEmail.vue
@@ -1,15 +1,14 @@
 <template>
   <main class="container">
     <h1>Verify your email address</h1>
-    <p>The JAC needs to make sure we have the right email address for you.</p>
-    <p>We've sent an email to <strong>{{email}}</strong> containing a link to verify your email address.</p>
-    <p>Follow the link to complete your registration and start your qualifying test.</p>
+    <p>To complete your registration we need to make sure we have the right email address for you.</p>
+    <p>We’ve sent an email to <strong>{{email}}</strong> containing a link to verify your email address and complete your registration before you start your qualifying test.</p>
 
     <section class="mt-5">
-      <h4>If you haven't received the email yet:</h4>
+      <h4>If you haven’t received the email yet</h4>
       <p>It could take up to 10 minutes for the email to arrive.</p>
-      <p>Don't forget to check your 'spam' or 'junk mail' folder in case it didn't go into your inbox.</p>
-      <p>If it still hasn't arrived after 10 minutes, you can resend the email:</p>
+      <p>Check your ‘spam’ or ‘junk mail’ folder in case it didn’t go into your inbox.</p>
+      <p>If it still hasn’t arrived after 10 minutes, you can ask us to resend the email:</p>
       <button class="btn btn-primary" type="button" :disabled="sendInProgress" @click.prevent="sendVerificationEmail">
         Resend email
       </button>
