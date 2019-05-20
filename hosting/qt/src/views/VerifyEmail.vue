5b0d3a06eaea6b80f7f7abc74edf9f2e1fd6201c
Ollie Treend <ollie@treend.com>
2019-05-20 11:42:46 +0100

    Change content on 'verify your email' page
    Including addition of `<details>` element styling. We've used a `<details>` element to hide the 'resend email' help text and button. Most users won't need this feature, and therefore the additional content and call-to-action button act as a distraction to the majority of users.

--- hosting/qt/src/views/VerifyEmail.vue
+++ hosting/qt/src/views/VerifyEmail.vue
@@ -1,19 +1,21 @@
 <template>
   <main class="container">
     <h1>Verify your email address</h1>
-    <p>To complete your registration we need to make sure we have the right email address for you.</p>
-    <p>We’ve sent an email to <strong>{{email}}</strong> containing a link to verify your email address and complete your registration before you start your qualifying test.</p>
+    <p>We need to make sure we have the right email address for you before you can start your qualifying test.</p>
+    <p>We’ve sent an email to <strong>{{email}}</strong> containing a link. Click the link to verify your email address.</p>
 
-    <section class="mt-5">
-      <h4>If you haven’t received the email yet</h4>
-      <p>It could take up to 10 minutes for the email to arrive.</p>
-      <p>Check your ‘spam’ or ‘junk mail’ folder in case it didn’t go into your inbox.</p>
-      <p>If it still hasn’t arrived after 10 minutes, you can ask us to resend the email:</p>
-      <button class="btn btn-primary" type="button" :disabled="sendInProgress" @click.prevent="sendVerificationEmail">
-        Resend email
-      </button>
-      <span class="spinner-border spinner-border-sm text-secondary ml-2" v-if="sendInProgress"></span>
-    </section>
+    <details>
+      <summary>I haven’t got the email yet</summary>
+      <section>
+        <p>It could take up to 10 minutes for the email to arrive.</p>
+        <p>Check your ‘spam’ or ‘junk mail’ folder in case it didn’t go into your inbox.</p>
+        <p>If it still hasn’t arrived after 10 minutes, you can ask us to resend the email:</p>
+        <button class="btn btn-primary" type="button" :disabled="sendInProgress" @click.prevent="sendVerificationEmail">
+          Resend email
+        </button>
+        <span class="spinner-border spinner-border-sm text-secondary ml-2" v-if="sendInProgress"></span>
+      </section>
+    </details>
   </main>
 </template>
 
