6db8c4a523c46d2c7e5ac33b8974a33a3d369a97
Ollie Treend <ollie@treend.com>
2019-05-31 17:29:58 +0100

    Add user info to Sentry error tracking
    Sentry allows us to capture the current user's information (if they're logged in) as part of reported errors. This is most useful in allowing us to understand how many unique users have been affected by an error.
    
    More information on how user tracking works, and how to implement it, can be found in the [Sentry JavaScript client documentation][1].
    
    User state might change throughout the life of the application since this is a Single Page Application. Therefore, I've used a watcher on the Vuex store – this will react to changes in the currently authenticated user ID, and will update the Sentry user state accordingly.
    
    [1]: https://docs.sentry.io/platforms/javascript/?platform=vue#capturing-the-user

--- hosting/qt/src/sentry.js
+++ hosting/qt/src/sentry.js
@@ -1,12 +1,26 @@
 import Vue from 'vue';
 import * as Sentry from '@sentry/browser';
 import * as Integrations from '@sentry/integrations';
+import store from '@/store';
 
 const initSentry = () => {
   Sentry.init({
     dsn: 'https://2ae111f2a62d4f7bbe84aaf868f0f0d5@sentry.io/1472344',
     integrations: [new Integrations.Vue({Vue, attachProps: true})],
   });
+  setUserScope();
 };
+
+const setUserScope = () => {
+  Sentry.configureScope((scope) => {
+    scope.setUser({
+      id: store.getters.currentUserId,
+      email: store.getters.currentUserEmail,
+    });
+  });
+};
+
+// Update the Sentry user scope when the current user changes in the Vuex store
+store.watch((state, getters) => (getters.currentUserId), setUserScope);
 
 export {initSentry};
