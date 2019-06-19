1f9788138d4ea1836b9e9f1cc824b2aec79bf86a
Ollie Treend <ollie@treend.com>
2019-06-19 17:53:40 +0100

    Show a warning to Internet Explorer users
    This adds a warning message to the 'sign in' and 'take test' pages for users browsing in Internet Explorer. Whilst the Vue application works in Internet Explorer, we know that Google Forms has problems which result in a poor user experience.
    
    Users will still be able to proceed with taking a test on Internet Explorer, but we're hoping that the warning message will be a sufficient deterrent.

---
+++ hosting/qt/src/components/IEWarning.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="isIE" class="alert alert-danger mt-5 mb-5" role="alert">
+    <h4>Warning</h4>
+    <p><strong>You’re using Internet Explorer.</strong></p>
+    <p>The test will not work correctly on this browser – use Chrome, Firefox or Microsoft Edge instead.</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        // Detect whether we're in Internet Explorer by checking for the existence of `document.documentMode`
+        // `document.documentMode` is only present in IE. For every other browser it's undefined.
+        isIE: !!document.documentMode,
+      };
+    }
+  }
+</script>
