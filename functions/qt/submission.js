fdbbdd64913af9356cf29974c75a9dff4d7afaec
Todd Tyree <tatyree@gmail.com>
2019-06-24 16:10:45 +0100

    Write the timestamped record with reference
    This is the logic to write the initial record.

--- functions/qt/submission.js
+++ functions/qt/submission.js
@@ -1,39 +1,22 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
-const NotifyClient = require("notifications-node-client").NotifyClient;
 
-// This throws exceptions rather than logging because we want to know as soon as these issues occur.  By throwing, we are
-// ensuring they will appear in StackDriver Errors in an actionable way.
 const createRecord = async (record) => {
   const firestore = admin.firestore();
-  const client = new NotifyClient(functions.config().notify.key);
 
-  console.info({ createRecordCalled: record });
+  record.finishedAt = admin.firestore.FieldValue.serverTimestamp();
 
-  // This function expects the test title to be in the following format:
-  // Test Name (i.e. April 2019 RUCA): Test Phase (i.e. Situational Judgement)"
-  const titleFormat = /^(.+)\s*:\s*([^:]+)$/;
-  testDetails = record.title.match(titleFormat);
-  if (testDetails === null) {
-    throw new Error("Title is incorrectly formatted " + record.title);
-  } else {
-    record.testName = testDetails[1];
-    record.testPhase = testDetails[2];
-  }
+  const userTest = firestore
+    .collection("usersTests")
+    .doc(record["Unique reference code - for JAC use only"]);
+  record.userTest = userTest;
 
-  const user = await admin.auth().getUser(record.userId);
-
-  record.createdAt = new Date();
-  await firestore
-    .collection('qtSubmissions')
+  const uts = await firestore
+    .collection("userTestSubmissions")
     .doc()
     .set(record);
 
-  await client
-    .sendEmail(
-      functions.config().notify.templates.combined_qt_sj_submitted,
-      user.email,
-      {});
+  console.info({ createdUserTestSubmission: uts.id });
   return true;
 }
 
