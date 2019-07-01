8a2cd6e1cdb4d1008d5624a087dd1e233f1dfc4d
Todd Tyree <tatyree@gmail.com>
2019-07-01 17:33:19 +0100

    Add function to regularly back up authentication
    This is a basic timed function, set to run every 12 hours, that backs up the firebase authentication data to a Google cloud
    storage bucket.  I chose to use a cloud storage bucket over a firebase bucket because these give finer-grained access control and
    more control over the retention policy.
    
    The 12 hour interval is a starting place agreed after @ollietreend and I discussed it. This seems like a good starting point that
    balances possible data loss with ease of maintenance of the bucket (i.e. ensuring that there aren't thousands of files for
    otherwise brief periods of time).

---
+++ functions/backup/authentication.js
@@ -0,0 +1,54 @@
+const backupBucket = "jac-firebase-backups";
+const backupPath = "/authentication/";
+const batchSize = 1000;
+const frequency = "every 12 hours";
+
+const admin = require("firebase-admin");
+const fs = require("fs");
+const functions = require("firebase-functions");
+const os = require("os");
+const path = require("path");
+const { promisify } = require("util");
+const writeFileAsync = promisify(fs.writeFile);
+
+const listUsers = async (users, nextPageToken) => {
+  const results = await admin.auth().listUsers(batchSize, nextPageToken);
+  results.users.forEach((userRecord) => {
+    users.push(userRecord);
+  });
+  // Get next batch of users.
+  if (results.pageToken) {
+    listUsers(users, results.pageToken);
+  }
+  return users;
+}
+
+const uploadToBackupBucket = async (users) => {
+  const date = new Date();
+  const fileName = date.toISOString() + ".json";
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+  const metadata = {
+    contentType: "application/json",
+  };
+
+  // Because fs does not return promises.
+  await writeFileAsync(tempFilePath, users);
+
+  const bucket = admin.storage().bucket(backupBucket);
+  await bucket.upload(tempFilePath, {
+    destination: backupPath + fileName,
+    metadata: metadata,
+  });
+  return fs.unlinkSync(tempFilePath);
+}
+
+const main = async () => {
+  const users = [];
+  await listUsers(users, undefined);
+  await uploadToBackupBucket(JSON.stringify(users, null, 2));
+  return true;
+}
+
+module.exports = functions.pubsub.schedule(frequency).onRun((context) => {
+   return main();
+})
