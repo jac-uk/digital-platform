c2fc07a5adc76579bf44b8a6aa4ee1854f215f4e
Alex Bokii <vanilinka33@gmail.com>
2019-07-03 16:14:59 +0100

    Use npm serve package instead of Node.js server
    As per discussion with Ollie, we agreed to use 'serve' npm package and
    remove our node server as its only purpose was serving index.html

--- hosting/reference/staticServer.js
+++
@@ -1,7 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-const port = process.argv[2] || 8002;
-
-connect().use(serveStatic(__dirname)).listen(port, function() {
-  console.log(`Server running on ${port}...`);
-});
