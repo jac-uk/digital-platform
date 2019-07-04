526e6d2afc2019728bb4e3bfb09a4ac0b3b6241d
Alex Bokii <vanilinka33@gmail.com>
2019-07-04 12:12:06 +0100

    Update test for upload and fix listing issues
    As previous upload test would change the status of test reference in
    the database, this test is updated to only check that form is
    available, until the seed functionality works.
    Also, used ES6 arrow functions

--- hosting/reference/cypress/integration/content_spec.js
+++ hosting/reference/cypress/integration/content_spec.js
@@ -1,14 +1,14 @@
-describe('Content', function() {
+describe('Content', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:8002?ref=test')
   });
 
-  it('Does not contain undefined', function() {
+  it('Does not contain undefined', () => {
     cy.wait(1000).get('body').should('not.contain', 'undefined')
   });
 
-  it('Does not contain null', function() {
+  it('Does not contain null', () => {
     cy.wait(1000).get('body').should('not.contain', 'null')
   });
 
--- hosting/reference/cypress/integration/page-load_spec.js
+++ hosting/reference/cypress/integration/page-load_spec.js
@@ -1,20 +1,20 @@
-describe('Home Page', function() {
+describe('Home Page', () => {
 
   describe('Loads', () => {
 
-    it('successfully if the correct reference value is provided in query string', function() {
+    it('successfully if the correct reference value is provided in query string', () => {
       cy.visit('http://localhost:8002?ref=test');
 
       cy.get('#screen-upload').should('be.visible');
     })
 
-    it('unsuccessfully if the reference is not provided in query string', function() {
+    it('unsuccessfully if the reference is not provided in query string', () => {
       cy.visit('http://localhost:8002');
 
       cy.get('#screen-upload').should('not.be.visible');
     })
 
-    it('unsuccessfully if the reference is not valid', function() {
+    it('unsuccessfully if the reference is not valid', () => {
       cy.visit('http://localhost:8002?ref=notvalid');
 
       cy.get('#screen-upload').should('not.be.visible');
@@ -22,7 +22,7 @@ describe('Home Page', function() {
   })
 
   describe('Displays a message', () => {
-    it('"Invalid link" if the reference is not found or incorrect', function() {
+    it('"Invalid link" if the reference is not found or incorrect', () => {
       cy.visit('http://localhost:8002?ref=');
 
       cy.get('#screen-not-found').should('be.visible');
