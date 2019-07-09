6c003e45c79ba899c70818c0ddbf4df94f5e6bc3
Alex Bokii <vanilinka33@gmail.com>
2019-07-09 15:40:28 +0100

    Add initial Vue project for QT admin

---
+++ hosting/qt-admin/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Landing-page.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
---
+++ hosting/qt-admin/src/components/Landing-page.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Admin area home</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
