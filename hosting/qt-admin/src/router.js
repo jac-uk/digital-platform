cc0958f34d59140b88c6a0dbba43fde9c9e54155
Alex Bokii <vanilinka33@gmail.com>
2019-07-10 17:24:06 +0100

    Add client side domain verification and Invalid domain template
    As Google OAuth doesn’t allow us to lock domain name and it can be
    overwritten, added an extra check for a domain name on client-side
    (though it should be done in Firestore as well).

--- hosting/qt-admin/src/router.js
+++ hosting/qt-admin/src/router.js
@@ -4,28 +4,43 @@ import store from '@/store';
 // Views
 import SignIn from '@/views/SignIn';
 import Dashboard from '@/views/Dashboard';
+import InvalidDomain from '@/views/InvalidDomain';
 
 const router = new Router({
   mode: 'history',
   routes: [
     {
+      path: '*',
+      redirect: '/',
+    },
+    {
       path: '/',
-      redirect: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+      }
     },
     {
       path: '/sign-in',
       name: 'sign-in',
       component: SignIn,
       meta: {
-        redirectIfAuth: "dashboard",
+        redirectIfAuth: 'dashboard',
       }
     },
     {
-      path: '/dashboard',
-      name: 'dashboard',
-      component: Dashboard,
-      meta: {
-        requiresAuth: true,
+      path: '/invalid-domain',
+      name: 'invalid-domain',
+      component: InvalidDomain,
+      beforeEnter: (to, from, next) => {
+        const isSignedIn = store.getters.isSignedIn;
+        const emailDomainIsValid = store.getters.emailDomainIsValid;
+        if(isSignedIn && emailDomainIsValid) {
+          return next({name: 'dashboard'});
+        }
+
+        return next();
       },
     },
   ]
@@ -41,12 +56,14 @@ router.beforeEach((to, from, next) => {
   const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
   const redirectIfAuth = to.matched.some(x => x.meta.redirectIfAuth);
   const isSignedIn = store.getters.isSignedIn;
+  const emailDomainIsValid = store.getters.emailDomainIsValid;
 
   if (requiresAuth && !isSignedIn) {
-    // User must be logged in
     return next({name: 'sign-in'});
-  } else if (redirectIfAuth && isSignedIn) {
-    return next({name: to.meta.redirectIfAuth});
+  } 
+
+  if (requiresAuth && !emailDomainIsValid) {
+    return next({name: 'invalid-domain'});
   }
 
   return next();
