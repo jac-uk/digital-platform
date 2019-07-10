5c30761e770020af1bb4bb2303a011dc35370783
Alex Bokii <vanilinka33@gmail.com>
2019-07-10 11:33:49 +0100

    Added initial styles and scss file

--- hosting/qt-admin/src/views/Dashboard.vue
+++ hosting/qt-admin/src/views/Dashboard.vue
@@ -1,6 +1,25 @@
 <template>
   <main>
-    <h1>This is Dashboard</h1>
+    <div class="container-fluid">
+      <ul class="nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard 1</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Dashboard 2</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+
+      <h1 class="text-center">Hello, admin</h1>
+
+      <p>This is admin dashboard page </p>
+    </div>
   </main>
 </template>
 
