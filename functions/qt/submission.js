9fb988ca5dff472f274eb446b82d3c4b381916e1
alexbokii <vanilink33@gmail.com>
2019-07-15 10:43:36 +0100

    Refactor tests using firebase-mock library
    This library can help us to save time on writing mocks for firebase.

--- functions/qt/submission.js
+++ functions/qt/submission.js
@@ -8,15 +8,15 @@ const sendConfirmationEmail = async (userTestDocData, confirmationTemplate) => {
   const client = new NotifyClient(functions.config().notify.key);
   const user = await admin.auth().getUser(userTestDocData.userUid);
   const personalisation = {firstname: user.displayName};
+  
   await client.sendEmail(confirmationTemplate, user.email, {personalisation});
+  
   console.info({sentConfirmationEmail: user.email});
-  return true;
 }
 
 async function getUserTest(firestore, record) {
   const userTest = await firestore.collection('usersTests').doc(record[REFERENCE_FIELD_NAME]);
   const userTestDoc = await userTest.get();
-
   return { userTest, userTestDoc };
 }
 
@@ -25,18 +25,17 @@ async function updateUserTestDoc(userTest, userTestDoc, finishedAtTimestamp) {
 
   /*
    * We only record the `finishedAt` time for the *first* submission. In the event of duplicate submissions,
-   * we log duplicates, without updating `finishedAt` timestamp.
+   * we log duplicates, without updating `finishedAt` timestamp.  
    *
    */
   if (!data.finishedAt) {
-    await firestore.collection("usersTests").set({finishedAt: finishedAtTimestamp}, {merge: true});
+    await userTest.set({finishedAt: finishedAtTimestamp}, {merge: true});
     
     console.info({updatedUsersTests: userTest.id});
+    return data;
   } else {
     console.warn({updatedUsersTests: userTest.id, duplicateSubmission: true});
   }
-
-  return data;
 }
 
 async function updateUserTestSubmission(firestore, record) {
@@ -49,19 +48,21 @@ async function updateUserTestSubmission(firestore, record) {
 const main = async (record) => {
   const firestore = admin.firestore();
   const { userTest, userTestDoc } = await getUserTest(firestore, record);
-  
+
   if (userTestDoc.exists) {
     const finishedAtTimestamp = admin.firestore.FieldValue.serverTimestamp();
     const updatedRecord = { ...record, userTest, finishedAt: finishedAtTimestamp };
 
     const userTestDocData = await updateUserTestDoc(userTest, userTestDoc, finishedAtTimestamp);
-    const userTestSubmission = await updateUserTestSubmission(firestore, updatedRecord);
-    
-    console.info({createdUserTestSubmission: userTestSubmission.id, usersTest: userTest.id});
 
-    await sendConfirmationEmail(userTestDocData, record.confirmationTemplate);
+    if (userTestDocData) {
+      const userTestSubmission = await updateUserTestSubmission(firestore, updatedRecord);
+      console.info({createdUserTestSubmission: userTestSubmission.id, usersTest: userTest.id});
+      
+      await sendConfirmationEmail(userTestDocData, record.confirmationTemplate);
+    }
   } else {
-    console.warn({updatedUsersTests: "missing", unattributableSubmission: true});
+    console.warn({updatedUsersTests: 'missing', unattributableSubmission: true});
   }
 }
 
--- functions/qt/submission.test.js
+++ functions/qt/submission.test.js
@@ -1,41 +1,155 @@
-require('notifications-node-client')
-const test = require('firebase-functions-test')();
+const firebaseFunctionsTest = require('firebase-functions-test')();
+const firebasemock = require('firebase-mock');
+const REFERENCE_CODE_FIELD_NAME = 'Unique reference code - for JAC use only';
 
-jest.mock('firebase-admin');
+let mockauth, mockfirestore, mocksdk, firebaseAdmin, emailSent;
 
-describe('submission', function() {
-  it('should update userTest with finishedAt timestamp and create userTestSubmission', async function() {
-    let req = { body: { confirmationTemplate: 'the template' } };
-    let res = {
-      status: (code) => ({
-        send: (payload) => {
-          console.log(payload);
-        }
+beforeEach(async () => {
+  mockauth = new firebasemock.MockAuthentication();
+  mockfirestore = new firebasemock.MockFirestore();
+  // we need only auth() and firestore(), so we return null
+  // for all other services
+  mocksdk = new firebasemock.MockFirebaseSdk(
+    // use null if your code does not use RTDB
+    (path) => null,
+    // use null if your code does not use AUTHENTICATION
+    () => mockauth,
+    // use null if your code does not use FIRESTORE
+    () => mockfirestore,
+    // use null if your code does not use STORAGE
+    () => null,
+    // use null if your code does not use MESSAGING
+    () => null,
+    );
+
+  // mock firebase-admin before each test
+  jest.mock('firebase-admin', () => {
+    return mocksdk;
+  });
+  
+  // set up for sendEmail test
+  firebaseFunctionsTest.mockConfig({notify: {key: 'test-key'}});
+  emailMock = setupEmailMock(() => emailSent = true);
+  emailSent = false;
+
+  // we need to set autoFlush for async oerations to prevent timeOut as by default firebaseMock won't wait for operation to complete
+  firebaseAdmin = require('firebase-admin');
+  firebaseAdmin.auth().autoFlush();
+  firebaseAdmin.firestore().collection('usersTests').autoFlush(1000);
+  firebaseAdmin.firestore().collection('userTestSubmissions').autoFlush(100);
+  await firebaseAdmin.auth().createUser({uid: 'test-user-id', email: 'alex@test.com', password: 'testpassword'});
+
+  // as firebase-mock supports `firestore.FieldValue.serverTimestamp` only for `update` operations,
+  // because we use `set` operation to save userTestSubmission, `finishedAt` field won't be populated
+  // this is why we replace `serverTimestamp` implementation with a custom object to
+  // assert on this object to make sure that the same `finishedAt`` timestamp is used for both
+  // userTest and userTestSubmission
+  firebaseAdmin.firestore.FieldValue.serverTimestamp = () => ({isEqual: () => false, type: 'submission-test'});
+
+});
+
+function setupEmailMock(f) {
+  return jest.mock('notifications-node-client', () => ({
+    NotifyClient: jest.fn().mockImplementation(() => ({
+      sendEmail: () => new Promise(resolve => {
+        f();
+        return resolve();
       })
+    }))
+  }));
+}
+
+describe('Submission', () => {
+  // set up req and res with callback function
+  const req = {
+    body: { 
+      confirmationTemplate: 'the template',
+      ['Unique reference code - for JAC use only']: 'test-reference-code',
+    }
+  };
+
+  const res = {
+    status: (code) => ({
+      send: (payload) => {
+        testHandler(payload);
+      }
+    })
+  };
+
+  it('should save a new submission and send notification to user', (done) => {
+    firebaseAdmin.firestore().collection('usersTests').doc('test-reference-code').set({
+      startedAt: 12345,
+      test: '/tests/test-id',
+      userUid: 'test-user-id',
+    });
+
+    testHandler = async () => {
+      let userTestSubmissionSnapshot = await firebaseAdmin.firestore().collection('userTestSubmissions').get();
+      let userTestSubmission = userTestSubmissionSnapshot.docs[0].data();
+
+      expect(userTestSubmissionSnapshot.docs.length).toBe(1);
+      expect(userTestSubmission.userTest.id).toBe('test-reference-code');
+      expect(userTestSubmission[REFERENCE_CODE_FIELD_NAME]).toBe('test-reference-code');
+      expect(emailSent).toBe(true);
+      return done();
     };
 
-    let userTestDoc = {};
-    let userTestSubmissionDoc = {};
+    const submission = require('./submission.js');
+    submission(req, res);
+  })
 
-    jest.mock('notifications-node-client', () => ({
-      NotifyClient: jest.fn().mockImplementation(() => ({
-        sendEmail: () => new Promise(resolve => resolve())
-      }))
-    }));
+  it('does not save submission or send email when userTest is missing', (done) => {    
+    testHandler = async () => {
+      let userTestSubmissionSnapshot = await firebaseAdmin.firestore().collection('userTestSubmissions').get();
+      expect(userTestSubmissionSnapshot.docs.length).toBe(0);
+      expect(emailSent).toBe(false);
 
-    test.mockConfig({notify: {key: 'test-key'}});
+      return done();
+    };
 
-    let mockedFirebaseAdmin = require('firebase-admin');
-    mockedFirebaseAdmin.setUserTestCallback(p => userTestDoc = p);
-    mockedFirebaseAdmin.setUserTestSubmissionCallback(p => userTestSubmissionDoc = p);
-    mockedFirebaseAdmin.setUserTestData({"Hello": "hello" });
-    
-    const submission = require('./submission.js');  
-    await submission(req, res);  
-    
-    expect(userTestDoc.finishedAt).toBe(42);
-    expect(userTestSubmissionDoc.confirmationTemplate).toBe('the template');
-    expect(userTestSubmissionDoc.finishedAt).toBe(42);
-    expect(userTestSubmissionDoc.userTest.name).toBe('userTestDocRef');
+    const submission = require('./submission.js');
+    submission(req, res);
   });
-});
+
+  it('should not save timestamp if finishedAt already exists', (done) => {
+    firebaseAdmin.firestore().collection('usersTests').doc('test-reference-code').set({
+      startedAt: 12345,
+      test: '/tests/test-id',
+      userUid: 'test-user-id',
+    });
+
+    const submission = require('./submission.js');
+
+    testHandler = async () => {
+      let userTest = await firebaseAdmin.firestore().collection('usersTests').doc('test-reference-code').get();
+      let userTestSubmissionSnapshot = await firebaseAdmin.firestore().collection('userTestSubmissions').get();
+
+      expect(userTest.data().finishedAt.type).toBe('submission-test');
+      expect(userTestSubmissionSnapshot.docs.length).toBe(1);
+
+      // setup before sending duplicate submission
+      // 1. redefine our custom timestamp object
+      firebaseAdmin.firestore.FieldValue.serverTimestamp = () => ({isEqual: () => false, type: 'duplicate-submission-test'});
+      // 2. manually reset emailSent variable
+      emailSent = false;
+
+      testHandler = async() => {
+        userTest = await firebaseAdmin.firestore().collection('usersTests').doc('test-reference-code').get();
+        userTestSubmissionSnapshot = await firebaseAdmin.firestore().collection('userTestSubmissions').get();
+
+        // finishedAt should stay the same as from the first submission
+        expect(userTest.data().finishedAt.type).toBe('submission-test');
+        // there should be only first user test submission in the db
+        expect(userTestSubmissionSnapshot.docs.length).toBe(1);
+        expect(emailSent).toBe(false);
+
+        return done();  
+      };
+
+      // sending duplicate
+      submission(req, res);
+    };
+
+    submission(req, res);
+  });
+})
