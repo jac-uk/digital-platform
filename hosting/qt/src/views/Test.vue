f68643fd4a689ac280bcb94a9d52f2dc1e225173
alexbokii <vanilinka33@gmail.com>
2019-07-24 11:21:55 +0100

    Update paths in router
    As discussed with Ollie, we want "takeTest" component to become "Test" component (hence we updated router path), and page with google from to become "TakeTest" component.
    Also, we agreed to keep test ID in the URL so we when we have multiple tests we could find the right googleFormUrl by ID which we get from URL.

--- hosting/qt/src/views/Test.vue
+++ hosting/qt/src/views/Test.vue
@@ -1,27 +1,56 @@
 <template>
-  <main>
-    <div class="container">
-      <div class="breadcrumb">
-        <button class="breadcrumb-item ative" @click="leaveThePage">Go back to My Tests page</button>
-      </div>
+  <main class="container">
+    <h1>Take a qualifying test</h1>
+
+    <IEWarning />
+
+    <div ref="loadingMessage" v-if="loaded === false && loadFailed === false">
+      <span class="spinner-border spinner-border-sm text-secondary" aria-hidden="true"></span>
+      Loading...
     </div>
-    <div class="iframe-conainer">
-      <iframe id="inlineFrameExample"
-        title="Inline Frame Example"
-        width="100%"
-        height="100%"
-        frameborder="0"
-        v-bind:src="this.testFormUrl">
-    </iframe>
+
+    <div ref="errorMessage" v-if="loaded === false && loadFailed === true">
+      <p>Could not load data. Please reload the page and try again.</p>
+    </div>
+
+    <div ref="qtView" v-if="loaded === true">
+      <h4>{{test.vacancyTitle}}</h4>
+
+      <TestWindow />
+
+      <div v-if="!testHasClosed && !userHasFinishedTest">
+        <h5>Do’s and don’ts</h5>
+        <ul>
+          <li><strong>Do</strong> make sure you’ve got a stable internet connection before you start</li>
+          <li><strong>Don’t</strong> press the back button at any point</li>
+          <li><strong>Don’t</strong> close the test window until you’ve submitted your answers</li>
+        </ul>
+
+        <h5>Timing</h5>
+        <ul>
+          <li>You have <strong>45 minutes</strong> to complete the test and must manage the time yourself</li>
+          <li>Time starts when you click ‘start test’ and stops when you click ‘submit’</li>
+          <li>Your answers won’t be marked if you submit after 45 minutes</li>
+        </ul>
+      </div>
+
+      <TestCard v-if="!testHasClosed" class="mt-4" />
     </div>
   </main>
 </template>
 
 <script>
-  import TakeTest from '@/views/TakeTest';
   import { mapGetters } from 'vuex';
+  import TestCard from '@/components/TestCard';
+  import TestWindow from '@/components/TestWindow';
+  import IEWarning from '@/components/IEWarning';
 
   export default {
+    components: {
+      TestCard,
+      TestWindow,
+      IEWarning,
+    },
     data() {
       return {
         loaded: false,
@@ -29,33 +58,43 @@
         isStarting: false,
       };
     },
+    methods: {
+      loadTestData() {
+        this.$store.commit('setTestId', this.$route.params.id);
+        return Promise.all([
+          this.$store.dispatch('loadTest'),
+          this.$store.dispatch('loadUserTest')
+        ])
+          .then(() => {
+            this.loaded = true;
+            this.$store.dispatch('subscribeUserTest');
+          })
+          .catch((e) => {
+            this.loadFailed = true;
+            throw e;
+          });
+      },
+    },
     computed: {
       ...mapGetters([
-        'testFormUrl',
+        'test',
+        'testHasClosed',
+        'userHasFinishedTest'
       ]),
     },
-    methods: {
-      leaveThePage() {
-        if (confirm('If you leave this page the progress will be lost. Do you want to leave the page?')) {
-          this.$router.push('/take-test');
-        } else {
-          return;
-        }
-        
+    mounted() {
+      this.loadTestData();
+    },
+    destroyed() {
+      this.$store.dispatch('unsubscribeUserTest');
+    },
+    watch: {
+      '$route' () {
+        this.loaded = false;
+        this.loadFailed = false;
+        this.$store.dispatch('unsubscribeUserTest');
+        this.loadTestData();
       },
     },
   };
 </script>
-
-<style scoped>
-  .iframe-conainer {
-    height: 100%;
-  }
-
-  button.breadcrumb-item {
-    border: none;
-    background: transparent;
-    text-decoration: underline;
-  }
-</style>
-
