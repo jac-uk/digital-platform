b533b6e39152e97b8473fc0f495e74c8c46094a8
Todd Tyree <tatyree@gmail.com>
2019-09-09 15:59:33 +0100

    Add shared secret and template id configuration
    Right now, this sends all the form data, but the receiving function only processes email.

--- google-apps-scripts/google-forms/writeAnswersToFirestore.js
+++ google-apps-scripts/google-forms/writeAnswersToFirestore.js
@@ -6,36 +6,38 @@
  */
 
 function onSubmit(e) {
-  const testSubmissionEndpoint = "<CHANGE TO CORRECT ENDPOINT>";
-  const data = { title: FormApp.getActiveForm().getTitle() };
+  const testSubmissionEndpoint = "ENDPOINT";
 
-  const itemResponses = e.response.getItemResponses();
-  data.email = e.response.getRespondentEmail();
+  const data = {
+    simpleKey: 'SHARED SECRET',
+    template: 'NOTIFY TEMPLATE ID',
+    title: FormApp.getActiveForm().getTitle(),
+    const itemResponses = e.response.getItemResponses();
+    data.email = e.response.getRespondentEmail();
 
-  itemResponses.forEach(function (itemResponse) {
-    const item = itemResponse.getItem();
-    // Get the question.
-    const responseTitle = item.getTitle();
-    const responseValue = itemResponse.getResponse();
+    itemResponses.forEach(function (itemResponse) {
+      const item = itemResponse.getItem();
+      // Get the question.
+      const responseTitle = item.getTitle();
+      const responseValue = itemResponse.getResponse();
 
-    if (item.getType() == "CHECKBOX_GRID") {
-      const userSelectedAnswers = {};
+      if (item.getType() == "CHECKBOX_GRID") {
+        const userSelectedAnswers = {};
 
-      /*
-       * This is a one-liner to flatten an array of arrays.  It is a less-than-clear map reduce, or at least functions as such.
-       * See:
-       *
-       * https://stackoverflow.com/questions/10865025/merge-flatten-an-array-of-arrays
-       *
-       * for more details.
-       * This is done becaue Google Apps Script does not yet support `.flat()`.
-       */
-      const answersArray = [].concat.apply([], responseValue);
+        /*
+         * This is a one-liner to flatten an array of arrays.  It is a less-than-clear map reduce, or at least functions as such.
+         * See:
+         *
+         * https://stackoverflow.com/questions/10865025/merge-flatten-an-array-of-arrays
+         *
+         * for more details.
+         * This is done becaue Google Apps Script does not yet support `.flat()`.
+         */
+        const answersArray = [].concat.apply([], responseValue);
 
-      const checkboxGrid = item.asCheckboxGridItem();
-      const allRows = checkboxGrid.getRows();
-
-      /* Build a keyed object because Firestore doesn't yet work with directly nested Arrays of Arrays *and*
+        const checkboxGrid = item.asCheckboxGridItem();
+        const allRows = checkboxGrid.getRows();
+        /* Build a keyed object because Firestore doesn't yet work with directly nested Arrays of Arrays *and*
          the format of `.getResponse()` when the question an array of checkboxes is exactly that:
 
          [['Most Appropriate'],null,null,null,['Least Appropriate'],null]
@@ -45,26 +47,25 @@ function onSubmit(e) {
          We DO NOT support this format--anything other than two columns will cause problems. That said, these two columns can be
          named any way the ops teams/policy like.
          */
-      checkboxGrid.getColumns().forEach(function (columnName) {
-        columnName = columnName.toString();
-        const chosenAnswer = allRows[answersArray.indexOf(columnName)];
-        userSelectedAnswers[columnName] = chosenAnswer;
-      });
+        checkboxGrid.getColumns().forEach(function (columnName) {
+          columnName = columnName.toString();
+          const chosenAnswer = allRows[answersArray.indexOf(columnName)];
+          userSelectedAnswers[columnName] = chosenAnswer;
+        });
 
-      data[responseTitle] = userSelectedAnswers;
-    } else {
-      data[responseTitle] = responseValue;
-    }
-  });
+        data[responseTitle] = userSelectedAnswers;
+      } else {
+        data[responseTitle] = responseValue;
+      }
+    });
 
-  const options = {
-    'method' : 'post',
-    'contentType': 'application/json',
-    'payload' : JSON.stringify(data)
-  };
+    const options = {
+      'method' : 'post',
+      'contentType': 'application/json',
+      'payload' : JSON.stringify(data)
+    };
 
-  UrlFetchApp.fetch(testSubmissionEndpoint, options);
-  console.info({ recordWritten: data.email });
-  return true;
-}
-
+    UrlFetchApp.fetch(testSubmissionEndpoint, options);
+    console.info({ recordWritten: data.email });
+    return true;
+  }
