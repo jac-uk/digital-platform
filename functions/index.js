da6e55778a0ec4a09ef0c981275080426a715a24
Martin R <martin.raymundo@judicialappointments.digital>
2019-11-24 02:00:11 +0100

    send welcom email to user when user creates an account

--- functions/index.js
+++ functions/index.js
@@ -15,6 +15,7 @@ const sendEmail = require('./sharedServices').sendEmail;
 
 exports.sendExerciseStartedEmail = require('./exercises/sendExerciseStartedEmail');
 exports.handleExerciseMailboxChange = require('./exercises/handleExerciseMailboxChange');
+exports.handleCandidateOnCreate = require('./candidates/handleCandidateOnCreate');
 
 /*
  *  TODO: All functions below this comment should be refactored into separate files.
