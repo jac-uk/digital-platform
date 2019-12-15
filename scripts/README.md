ce89cb29ec39114216ce1a249ab8e1bda63e6b0c
Martin R <martin.raymundo@judicialappointments.digital>
2019-12-15 19:42:37 +0100

    update scripts to only backup and restore certain collections

--- scripts/README.md
+++ scripts/README.md
@@ -6,7 +6,7 @@ https://console.cloud.google.com/?cloudshell=true
 ```
 
 # set your project to the UK one
-gcloud config set project platform-production-9207
+gcloud config set project platform-production-9207d
 
 # Import (restore) from the storage bucket below. 
 # In this case, the backup folder is named 2019-12-12T22%3A58%3A32.478Z
--- scripts/firestore-backup-from-US-restore-to-UK.sh
+++ scripts/firestore-backup-from-US-restore-to-UK.sh
@@ -10,7 +10,7 @@ export BACKUP_PATH="gs://application-form-e08c9-backups/$NAME"
 
 # Backup the US Firestore to the US bucket
 gcloud config set project application-form-e08c9
-gcloud firestore export $BACKUP_PATH
+gcloud firestore export $BACKUP_PATH --collection-ids=applications,candidates,exercises,meta,roles
 echo "backup name = $NAME"
 
 # Copy the US Firestore backup to the UK bucket (gs://platform-production-9207d-backups/from-us/)
@@ -18,4 +18,4 @@ gsutil -m cp -R $BACKUP_PATH gs://platform-production-9207d-backups/from-us/
 
 # Restore to UK Firestore from the UK bucket
 gcloud config set project platform-production-9207d
-gcloud firestore import gs://platform-production-9207d-backups/from-us/$NAME
+gcloud firestore import gs://platform-production-9207d-backups/from-us/$NAME --collection-ids=applications,candidates,exercises,meta,roles
