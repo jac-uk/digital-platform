a0177408e391bf709087f369af51f89a3bdd51dc
Martin R <martin.raymundo@judicialappointments.digital>
2020-01-01 16:46:54 +0100

    remind assessors 2 weeks initial email after if they haven't done their assessments

--- functions/exercises/remindAssessorsToAssess.js
+++ functions/exercises/remindAssessorsToAssess.js
@@ -4,6 +4,45 @@ const db = require('../sharedServices').db;
 const getExercisesWithDate = require('../sharedServices').getExercisesWithDate;
 
 
+const getExercisesWithReminderDate = async (dateFieldName) => {
+  // set today's date to 2 weeks before
+  const today = new Date();
+  today.setDate(today.getDate() - 14);
+
+  // set yesterday's date to 2 weeks before - 1 day
+  let yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 15);
+
+  slog(`
+    SCHEDULED JOB: Send reminder emails (2 weeks after original email)
+    Looking for exercises where ${dateFieldName} = ${today}
+  `);
+
+  let exercisesRef = db.collection('exercises');
+
+  let snapshot;
+  try {
+    snapshot = await exercisesRef.where(dateFieldName, '>', yesterday).where(dateFieldName, '<', today).get(); 
+  } catch(err) {
+    slog(`
+      ERROR: Bad query: exercisesRef.where(${dateFieldName}, '>', yesterday).where(${dateFieldName}, '<', today).get() :
+    `, err);
+    return null;
+  }
+
+  if (snapshot.empty) {
+    slog(`
+      No matching documents in Exercises
+      (2 weeks after original email) 
+      where ${dateFieldName} > ${yesterday}
+      and ${dateFieldName} < ${today}`);
+    return null;
+  }
+
+  return snapshot;
+};
+
+
 const processApplications = async (applicationIds) => {
   // for each application, set application.status = "request-assessors"
   applicationIds.forEach(doc => {
@@ -52,11 +91,19 @@ const notifyAssessors = async (exerciseIds) => {
 
 
 const remindAssessorsToAssess = async () => {
+  // send initial email asking assessors to do assessments
   const exercisesWithContactIndependentAssessorsDateTodayPromise = getExercisesWithDate('contactIndependentAssessors'); 
   const exercisesWithContactIndependentAssessorsDateToday = await exercisesWithContactIndependentAssessorsDateTodayPromise;
   if (exercisesWithContactIndependentAssessorsDateToday != null) {
     notifyAssessors(exercisesWithContactIndependentAssessorsDateToday);
   }
+
+  // remind assessors 2 weeks after if they haven't done their assessments
+  const reminderEmailsTodayPromise = getExercisesWithReminderDate('contactIndependentAssessors'); 
+  const reminderEmailsToday = await reminderEmailsTodayPromise;
+  if (reminderEmailsToday != null) {
+    notifyAssessors(reminderEmailsToday);
+  }  
   return null;
 };
 
