88b23e1caa863fa764472ee24235b6ec42c6e95d
Martin R <martin.raymundo@judicialappointments.digital>
2020-01-05 22:58:19 +0100

    handling 'uncaught exception errors in multiple functions where we try to send emails

--- functions/applications/handleApplicationOnCreate.js
+++ functions/applications/handleApplicationOnCreate.js
@@ -1,8 +1,7 @@
-/*eslint-disable no-unused-vars*/
 const functions = require('firebase-functions');
 const sendEmail = require('../sharedServices').sendEmail;
-const db = require('../sharedServices').db;
 const getData = require('../sharedServices').getData;
+const setData = require('../sharedServices').setData;
 const slog = require('../sharedServices').slog;
 
 const setApplicationOnCreateData = async (snap, context) => {
@@ -10,16 +9,29 @@ const setApplicationOnCreateData = async (snap, context) => {
   const data = snap.data();
 
   // pass in exerciseId and candidateId and save it to firestore
+  if (data.exerciseId == null) {
+    slog(`
+      ERROR: Application onCreate did not set exerciseId field.
+    `);
+    return false;
+  }
+
+  if (data.userId == null) {
+    slog(`
+      ERROR: Application onCreate did not set userId field.
+    `);
+    return false; 
+  }
+
   // and initialize a createdAt for easier sorting
-  const applicationRef = db.collection('applications').doc(context.params.applicationId);
-  const setWithMerge = applicationRef.set({
+  const newFieldsData = {
     createdAt: Date.now(),
     status: 'draft',
     firstAssessorSubmittedAssessment: false,
-    secondAssessorSubmittedAssessment: false,      
-  }, { merge: true});
+    secondAssessorSubmittedAssessment: false,    
+  };
 
-  return null;
+  return await setData('applications', context.params.applicationId, newFieldsData);
 };
 
 const sendApplicationStartedEmailToCandidate = async (snap, context) => {
@@ -44,10 +56,16 @@ const sendApplicationStartedEmailToCandidate = async (snap, context) => {
   // Set notify.templates.application_started in firebase functions like this:
   // firebase functions:config:set notify.templates.application_started="THE_GOVUK_NOTIFY_TEMPLATE_ID"  
   const templateId = functions.config().notify.templates.application_started;
-  return sendEmail(candidateEmail, templateId, personalizedData).then((sendEmailResponse) => {
-    slog(`${candidateFullName} (${candidateEmail}) has started to apply to exerciseId ${data.exerciseId}`);
-    return true;
-  });  
+  return sendEmail(candidateEmail, templateId, personalizedData)
+    .then((sendEmailResponse) => {
+      slog(`${candidateFullName} (${candidateEmail}) has started to apply to exerciseId ${data.exerciseId}`);
+      return sendEmailResponse;
+    })
+    .catch(err => {
+      console.error('Something went wrong creating Application ID: ', context.params.applicationId);
+      console.error('Error Sending Email sendApplicationStartedEmailToCandidate:', err);
+      return false;
+    });  
 };
 
 exports.handleApplicationOnCreate = functions.region('europe-west2').firestore
@@ -62,4 +80,5 @@ exports.handleApplicationOnCreate = functions.region('europe-west2').firestore
     // after setting the data, send the candidate an email 
     // so they can track their application to a judgeship vacancy
     sendApplicationStartedEmailToCandidate(snap, context);
+    return null;
   });
