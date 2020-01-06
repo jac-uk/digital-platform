533a9989c4407ac05bce7fed8c800b9fb4f1b722
Warren Searle <warren@precise-minds.co.uk>
2020-01-06 10:10:46 +0000

    Add tests for database rules

---
+++ test/rules/1.database.spec.js
@@ -0,0 +1,47 @@
+const { setup, teardown } = require('./helpers');
+const { assertFails, assertSucceeds } = require('@firebase/testing');
+
+describe("Database", () => {
+  after(async () => {
+    await teardown();
+  });
+  
+  context("Create", () => {
+    it("prevent un-authenticated user from creating a new collection", async () => {
+      const db = await setup();
+      await assertFails(db.collection("some-nonexistent-collection").add({}));
+    });
+    it("prevent authenticated user from creating a new collection", async () => {
+      const db = await setup({ uid: 'user1' });
+      await assertFails(db.collection("some-nonexistent-collection").add({}));
+    });
+    it("prevent authenticated user with verified @judicialappointments.digital email from creating a new collection", async () => {
+      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true });
+      await assertFails(db.collection("some-nonexistent-collection").add({}));
+    });
+    it("prevent authenticated user with verified @judicialappointments.gov.uk email from creating a new collection", async () => {
+      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
+      await assertFails(db.collection("some-nonexistent-collection").add({}));
+    });
+  });
+
+  context("Read", () => {
+    it("prevent un-authenticated user from reading unknown collections", async () => {
+      const db = await setup();
+      await assertFails(db.collection("some-nonexistent-collection").get());
+    });
+    it("prevent authenticated user from reading unknown collections", async () => {
+      const db = await setup({ uid: 'user1' });
+      await assertFails(db.collection("some-nonexistent-collection").get());
+    });
+    it("prevent authenticated user with verified @judicialappointments.digital email from reading unknown collections", async () => {
+      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true });
+      await assertFails(db.collection("some-nonexistent-collection").get());
+    });       
+    it("prevent authenticated user with verified @judicialappointments.gov.uk email from reading unknown collections", async () => {
+      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
+      await assertFails(db.collection("some-nonexistent-collection").get());
+    });       
+  });
+
+});
