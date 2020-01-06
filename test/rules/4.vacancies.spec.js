e3e010ba9d3b107186c4309833634f4cdf379a71
Warren Searle <warrensearle@Warrens-iMac.local>
2020-01-06 10:11:30 +0000

    Add tests for vacancies rules

---
+++ test/rules/4.vacancies.spec.js
@@ -0,0 +1,59 @@
+const { setup, teardown } = require('./helpers');
+const { assertFails, assertSucceeds } = require('@firebase/testing');
+
+describe("Vacancies", () => {
+  afterEach(async () => {
+    await teardown();
+  });
+  
+  context("Create", () => {
+    it("prevent un-authenticated user from creating a vacancy", async () => {
+      const db = await setup();
+      await assertFails(db.collection("vacancies").add({}));
+    });
+  
+    it("prevent authenticated user from creating a vacancy", async () => {
+      const db = await setup({ uid: 'user1' });
+      await assertFails(db.collection("vacancies").add({}));
+    });
+  });
+
+  context("Read", () => {
+    it("allow un-authenticated user to read a vacancy", async () => {
+      const db = await setup();
+      await assertSucceeds(db.collection("vacancies").get());
+      await assertSucceeds(db.collection("vacancies").doc('vac1').get());
+    });
+  
+    it("allow authenticated user to read a vacancy", async () => {
+      const db = await setup({ uid: 'user1' });
+      await assertSucceeds(db.collection("vacancies").get());
+      await assertSucceeds(db.collection("vacancies").doc('vac1').get());
+    });
+  });  
+
+  context("Update", () => {
+    it("prevent un-authenticated user from updating a vacancy", async () => {
+      const db = await setup();
+      await assertFails(db.collection("vacancies").doc("vac1").update({}));
+    });
+  
+    it("prevent authenticated user from updating a vacancy", async () => {
+      const db = await setup({ uid: 'user1' });
+      await assertFails(db.collection("vacancies").doc("vac1").update({}));
+    });
+  });
+
+  context("Delete", () => {
+    it("prevent un-authenticated user from deleting a vacancy", async () => {
+      const db = await setup();
+      await assertFails(db.collection("vacancies").doc("vac1").delete());
+    });
+  
+    it("prevent authenticated user from deleting a vacancy", async () => {
+      const db = await setup({ uid: 'user1' });
+      await assertFails(db.collection("vacancies").doc("vac1").delete());
+    });
+  });
+
+});
