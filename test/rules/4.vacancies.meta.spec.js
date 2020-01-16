f84453d898fc7c39ecf7d236539f1b6b600c92d6
Warren Searle <warren@precise-minds.co.uk>
2020-01-16 18:01:26 +0000

    Firestore rules. Basic tests for applicationsCount

---
+++ test/rules/4.vacancies.meta.spec.js
@@ -0,0 +1,48 @@
+const { setup, teardown } = require('./helpers');
+const { assertFails, assertSucceeds } = require('@firebase/testing');
+
+describe("Meta", () => {
+  afterEach(async () => {
+    await teardown();
+  });
+  
+  context("Create", () => {
+    // @todo
+  });
+
+  context("Read", () => {
+    it("prevent un-authenticated user from reading vacancy stats", async () => {
+      const db = await setup(
+        null,
+        { 'vacancies/vac1': { 
+            name: 'vac 1',
+            'meta/stats': { applicationsCount: 5 },
+          }
+        }
+      );
+      await assertFails(db.doc("vacancies/vac1/meta/stats").get());
+    });      
+    it("allow authenticated user to read vacancy stats", async () => {
+      const db = await setup(
+        { uid: 'bob' },
+        { 'vacancies/vac1': { 
+            name: 'vac 1',
+            'meta/stats': { applicationsCount: 5 },
+          }
+        }
+      );
+      await assertSucceeds(db.doc("vacancies/vac1/meta/stats").get());
+    });        
+  });
+
+  context("Update", () => {
+    // @todo
+  });
+
+  context("Delete", () => {
+    // @todo
+  });
+
+
+
+});
