df3ce36de3c5f5ff9f9d11cd4029215676a33948
Warren Searle <warren@precise-minds.co.uk>
2020-07-27 11:56:47 +0100

    #314 Enable deployment of malware functions
    - Update package scripts to deploy to develop
    - Update deploy script to only deploy malware functions
    - Update functions index to only define malware functions

--- functions/index.js
+++ functions/index.js
@@ -3,70 +3,13 @@
  *  create your functions in separate files
  */
 
-exports.backupFirestore = require('./backup/firestore');
-exports.backupFirebaseAuthentication = require('./backup/authentication');
-exports.onExerciseUpdate_PublishVacancy = require('./exercises/onExerciseUpdate_PublishVacancy');
-exports.onWriteVacancyStats = require('./exercises/onWriteVacancyStats');
+const functions = require('firebase-functions');
 
-// exports.sendExerciseStartedEmail = require('./exercises/sendExerciseStartedEmail');
-// exports.handleExerciseMailboxChange = require('./exercises/handleExerciseMailboxChange');
-// exports.handleCandidateOnCreate = require('./candidates/handleCandidateOnCreate');
-exports.handleApplicationOnCreate = require('./applications/handleApplicationOnCreate');
-// exports.handleApplicationOnUpdate = require('./applications/handleApplicationOnUpdate');
-// exports.handleExerciseTimelineDates = require('./exercises/handleExerciseTimelineDates');
-// exports.userTestSubmissions = require('./qt/submission');
+exports.requestMalwareScan = functions.region('europe-west2').storage
+  .object()
+  .onFinalize((object) => {
+    const handler = require('./virus-scanning/requestMalwareScan');
+    return handler(object);
+  });
 
-/*
- *  TODO: All functions below this comment should be refactored into separate files.
- */
-
-// const admin = require('firebase-admin');
-// const functions = require('firebase-functions');
-// const sendEmail = require('./sharedServices').sendEmail;
-
-// const sendVerificationEmail = async (user) => {
-//   const email = user.email;
-//   const returnUrl = functions.config().production.url;
-//   const templateId = functions.config().notify.templates.verification;
-//   const verificationLink = await admin.auth().generateEmailVerificationLink(email, {url: returnUrl});
-//   return sendEmail(email, templateId, {
-//     'applicantName': user.displayName,
-//     verificationLink,
-//   });
-// };
-
-// exports.sendVerificationEmailOnNewUser = functions.region('europe-west2').auth.user().onCreate((user) => {
-//   return sendVerificationEmail(user);
-// });
-
-// exports.processReferenceUpload = functions.storage
-//   .object()
-//   .onFinalize((object) => {
-//     const regex = /^references\/.*\/.*$/;
-//     if (regex.test(object.name)) {
-//       const handler = require('./src/references/processUpload');
-//       return handler(object);
-//     }
-//     return true;
-//   });
-
-// exports.sendReferenceRequestEmail = functions.firestore
-//   .document('references/{referenceId}')
-//   .onCreate((snapshot) => {
-//     const config = functions.config();
-//     const templateId = config.notify.templates.reference_request;
-//     const data = snapshot.data();
-
-//     const downloadUrl = `${config.references.url}/download-form/128.docx`;
-//     const uploadUrl = `${config.references.url}/?ref=${snapshot.id}`;
-
-//     const personalisation = {
-//       'applicant name': data.applicant_name,
-//       'assessor name': data.assessor.name,
-//       'download url': downloadUrl,
-//       'upload url': uploadUrl,
-//     };
-
-//     return sendEmail(data.assessor.email, templateId, personalisation);
-//   });
-  
+// @TODO enqueueMalwareScans
