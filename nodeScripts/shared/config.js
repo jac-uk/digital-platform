356c4adbfc81961bd9eca2cf9d56cd6e97fb3d4c
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:32:23 +0100

    Node script shared module for config data

---
+++ nodeScripts/shared/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ASSESSMENTS_URL: 'https://assessments-develop.judicialappointments.digital',
+  ASSESSMENT_TYPE: {
+    COMPETENCY: 'competency',
+    SKILLS: 'skills',
+    GENERAL: 'general',
+  },
+  NOTIFY_KEY: 'firebase_cloud_functions_production-0abe6c8e-0b87-4cde-9493-5da4921ccc53-b77d61c5-5c96-47de-bf9f-9850b62e854c',  // functions.config().notify.key,
+  SLACK_URL: 'https://hooks.slack.com/services/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',
+};
