a0f09ab13d0ca4c198869737ee33e0ce0110d42d
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:18:55 +0100

    Callable function to test assessment notifications

---
+++ functions/callableFunctions/testAssessmentNotification.js
@@ -0,0 +1,25 @@
+const functions = require('firebase-functions');
+const config = require('../shared/config');
+const { firebase, db } = require('../shared/admin.js');
+const { checkArguments } = require('../shared/helpers.js');
+const { testAssessmentNotification } = require('../actions/assessments')(config, firebase, db);
+
+module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  if (!checkArguments({
+    assessmentId: { required: true },
+    notificationType: { required: true },
+  }, data)) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  const result = await testAssessmentNotification({
+    assessmentId: data.assessmentId,
+    notificationType: data.notificationType,
+    email: context.auth.token.email,
+  });
+  return {
+    result: result,
+  };
+});
