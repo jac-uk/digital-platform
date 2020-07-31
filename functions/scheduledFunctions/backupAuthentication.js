75391a3bd10deb0cf4fb6b0164be965acf4eeb81
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:13:07 +0100

    Scheduled function to backup authentication

---
+++ functions/scheduledFunctions/backupAuthentication.js
@@ -0,0 +1,49 @@
+const PROJECT_ID = process.env.GCLOUD_PROJECT;
+const BACKUP_BUCKET = `${PROJECT_ID}-backups`;
+const BACKUP_PATH = '/authentication/';
+//const BACKUP_SCHEDULE = 'every 1 hours synchronized';
+const BACKUP_SCHEDULE = 'every day 23:01';
+
+const admin = require('firebase-admin');
+const firebaseTools = require('firebase-tools');
+const fs = require('fs');
+const functions = require('firebase-functions');
+const os = require('os');
+const path = require('path');
+
+// Download auth export file to the local filesystem
+const downloadAuthExport = async (filePath) => {
+  await firebaseTools.auth.export(filePath, {
+    project: PROJECT_ID,
+  });
+};
+
+// Upload local file to the backup Cloud Storage bucket
+const uploadToStorageBucket = async (localPath, fileName) => {
+  const bucket = admin.storage().bucket(BACKUP_BUCKET);
+  await bucket.upload(localPath, {
+    destination: BACKUP_PATH + fileName,
+  });
+};
+
+// Delete a file from the local filesystem
+const deleteLocalFile = (filePath) => {
+  return fs.unlinkSync(filePath);
+};
+
+// Main handler function
+const main = async () => {
+  const timestamp = (new Date()).toISOString();
+  const fileName = timestamp + '.json';
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+
+  await downloadAuthExport(tempFilePath);
+  await uploadToStorageBucket(tempFilePath, fileName);
+  deleteLocalFile(tempFilePath);
+};
+
+module.exports = functions.region('europe-west2')
+                          .pubsub
+                          .schedule(BACKUP_SCHEDULE)
+                          .timeZone('Europe/London')
+                          .onRun(main);
