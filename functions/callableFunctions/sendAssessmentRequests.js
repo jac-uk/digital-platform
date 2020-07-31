b4f9cb247dcc34a092c76bf78d4723b544698a5a
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:19:53 +0100

    Callable function to send assessment requests

---
+++ functions/callableFunctions/sendAssessmentRequests.js
@@ -0,0 +1,23 @@
+const functions = require('firebase-functions');
+const config = require('../shared/config');
+const { firebase, db } = require('../shared/admin.js');
+const { checkArguments } = require('../shared/helpers.js');
+const { sendAssessmentRequests } = require('../actions/assessments')(config, firebase, db);
+
+module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  if (!checkArguments({
+    exerciseId: { required: true },
+    assessmentId: { required: false },
+    assessmentIds: { required: false },
+    resend: { required: false },
+  }, data)) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  const result = await sendAssessmentRequests(data);
+  return {
+    result: result,
+  };
+});
