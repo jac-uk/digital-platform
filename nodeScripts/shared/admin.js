95f486ae7ff67197da386c6814b8e9ea7fcb94ec
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:31:35 +0100

    Node script shared module for admin sdk

---
+++ nodeScripts/shared/admin.js
@@ -0,0 +1,11 @@
+/**
+ * Initialises Admin SDK and exports firestore database connection
+ */
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+exports.firebase = admin;
+exports.db = admin.firestore();
+exports.app = admin.app();
+exports.auth = admin.auth();
