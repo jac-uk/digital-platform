d4d8ad96d804f176387c14e0d9474a05eb38a448
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:39:56 +0100

    Include tests for flagging application issues

---
+++ test/actions/helpers.js
@@ -0,0 +1,24 @@
+// @TODO use firestore mock library
+
+const mockSnapshot = {
+  id: 'application1',
+  ref: 'applications/application1',
+  data: () => {
+    return {
+      referenceNumber: 'applref001',
+    };
+  },
+};
+
+exports.mockDb = {
+  collection: jest.fn().mockReturnValue({
+    doc: jest.fn().mockReturnValue({
+      delete: jest.fn(),
+      get: jest.fn().mockReturnValue(mockSnapshot),
+    }),
+    where: jest.fn().mockReturnValue({
+      get: jest.fn().mockReturnValue(mockSnapshot),
+    }),
+  }),
+};
+
