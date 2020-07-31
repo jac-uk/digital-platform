8ea5ba3b7c824a555b07470970e02a066132b9aa
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:37:07 +0100

    Update tests for database rules

--- test/rules/1.database.spec.js
+++ test/rules/1.database.spec.js
@@ -1,46 +1,46 @@
 const { setup, teardown } = require('./helpers');
-const { assertFails, assertSucceeds } = require('@firebase/testing');
+const { assertFails } = require('@firebase/testing');
 
-describe("Database", () => {
+describe('Database', () => {
   after(async () => {
     await teardown();
   });
   
-  context("Create", () => {
-    it("prevent un-authenticated user from creating a new collection", async () => {
+  context('Create', () => {
+    it('prevent un-authenticated user from creating a new collection', async () => {
       const db = await setup();
-      await assertFails(db.collection("some-nonexistent-collection").add({}));
+      await assertFails(db.collection('some-nonexistent-collection').add({}));
     });
-    it("prevent authenticated user from creating a new collection", async () => {
+    it('prevent authenticated user from creating a new collection', async () => {
       const db = await setup({ uid: 'user1' });
-      await assertFails(db.collection("some-nonexistent-collection").add({}));
+      await assertFails(db.collection('some-nonexistent-collection').add({}));
     });
-    it("prevent authenticated user with verified @judicialappointments.digital email from creating a new collection", async () => {
+    it('prevent authenticated user with verified @judicialappointments.digital email from creating a new collection', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true });
-      await assertFails(db.collection("some-nonexistent-collection").add({}));
+      await assertFails(db.collection('some-nonexistent-collection').add({}));
     });
-    it("prevent authenticated user with verified @judicialappointments.gov.uk email from creating a new collection", async () => {
+    it('prevent authenticated user with verified @judicialappointments.gov.uk email from creating a new collection', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
-      await assertFails(db.collection("some-nonexistent-collection").add({}));
+      await assertFails(db.collection('some-nonexistent-collection').add({}));
     });
   });
 
-  context("Read", () => {
-    it("prevent un-authenticated user from reading unknown collections", async () => {
+  context('Read', () => {
+    it('prevent un-authenticated user from reading unknown collections', async () => {
       const db = await setup();
-      await assertFails(db.collection("some-nonexistent-collection").get());
+      await assertFails(db.collection('some-nonexistent-collection').get());
     });
-    it("prevent authenticated user from reading unknown collections", async () => {
+    it('prevent authenticated user from reading unknown collections', async () => {
       const db = await setup({ uid: 'user1' });
-      await assertFails(db.collection("some-nonexistent-collection").get());
+      await assertFails(db.collection('some-nonexistent-collection').get());
     });
-    it("prevent authenticated user with verified @judicialappointments.digital email from reading unknown collections", async () => {
+    it('prevent authenticated user with verified @judicialappointments.digital email from reading unknown collections', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true });
-      await assertFails(db.collection("some-nonexistent-collection").get());
+      await assertFails(db.collection('some-nonexistent-collection').get());
     });       
-    it("prevent authenticated user with verified @judicialappointments.gov.uk email from reading unknown collections", async () => {
+    it('prevent authenticated user with verified @judicialappointments.gov.uk email from reading unknown collections', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
-      await assertFails(db.collection("some-nonexistent-collection").get());
+      await assertFails(db.collection('some-nonexistent-collection').get());
     });       
   });
 
