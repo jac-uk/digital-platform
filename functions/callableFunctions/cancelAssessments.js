b77d93065aaa6f2ae22766b6733ca50fd53fb6e3
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:21:30 +0100

    Callable function to cancel assessments

---
+++ functions/callableFunctions/cancelAssessments.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions');
+const config = require('../shared/config');
+const { firebase, db } = require('../shared/admin.js');
+const { checkArguments } = require('../shared/helpers.js');
+const { cancelAssessments } = require('../actions/assessments')(config, firebase, db);
+
+module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  if (!checkArguments({
+    exerciseId: { required: true },
+  }, data)) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  const result = await cancelAssessments(data);
+  return {
+    result: result,
+  };
+});
