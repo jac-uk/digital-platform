4ecc647f64308a094415832fbe740b432253e1ee
Warren Searle <warren@precise-minds.co.uk>
2020-07-31 17:02:00 +0100

    Include shared module for slack integration

---
+++ functions/shared/slack.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+module.exports = (config) => {
+
+  return {
+    post
+  };
+
+  async function post(msgString) {
+    const result = await axios.post(
+      config.SLACK_URL,
+      {
+        text: msgString,
+      }
+    );
+    return result;
+  }
+
+}
