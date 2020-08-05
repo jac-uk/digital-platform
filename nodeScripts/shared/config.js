e7e552431182ec6164f1a478852e6e967a7acb04
Warren Searle <warren@precise-minds.co.uk>
2020-08-05 10:55:55 +0100

    Include nodeScripts config module

---
+++ nodeScripts/shared/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ASSESSMENTS_URL: process.env.ASSESSMENTS_URL,
+  ASSESSMENT_TYPE: {
+    COMPETENCY: 'competency',
+    SKILLS: 'skills',
+    GENERAL: 'general',
+  },
+  NOTIFY_KEY: process.env.NOTIFY_KEY,
+  SLACK_URL: process.env.SLACK_URL,
+};
