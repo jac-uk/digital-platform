9fc6d62dbd3aa96e3d4fcb59ec6c8425d9351fe6
Warren Searle <warren@precise-minds.co.uk>
2020-11-30 10:53:09 +0000

    #463 Analysis of qualifying test questions/answers

---
+++ nodeScripts/query.invitations-without-applications.js
@@ -0,0 +1,84 @@
+'use strict';
+
+// const config = require('./shared/config.js');
+const { app, db } = require('./shared/admin.js');
+const { getDocuments } = require('../functions/shared/helpers');
+
+
+const main = async () => {
+
+  const exerciseId = 'kVlymRGRhZndRaQuqDTf';
+
+  // get invitations
+  const invitations = await getDocuments(db.collection('invitations').where('vacancy.id', '==', exerciseId).select('candidate.email'));
+  console.log('invitations', invitations.length);
+
+  // get applications (which have applied)
+  const applications = await getDocuments(
+    db.collection('applications')
+    .where('exerciseId', '==', exerciseId)
+    .where('referenceNumber', '>', '')
+    .select('personalDetails.email')
+  );
+  console.log('applications', applications.length);
+
+  // get invitations without applications
+  const emailsApplied = {};
+  applications.forEach(application => {
+    if (application.personalDetails && application.personalDetails.email) {
+      emailsApplied[application.personalDetails.email] = true;
+    }
+  });
+  const incompleteInvitations = invitations.filter(invitation => !emailsApplied[invitation.candidate.email]);
+
+  console.log('incomplete', incompleteInvitations.length);
+
+  const invitationEmails = incompleteInvitations.map(item => item.candidate.email);
+  console.log(invitationEmails.join('\n'));
+
+  // // get data
+  // let ref = db.collection('assessments')
+  //   .where('exercise.id', '==', 'LYybYFxgdeCzyC1vWZdY')
+  //   .where('status', '==', 'draft')
+  //   .where('assessor.email', '==', '');
+  //   //.select('exercise.referenceNumber', 'application.referenceNumber', 'assessor.email');
+  // const documents = await getDocuments(ref);
+
+  // console.log(documents);
+  // const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  // const csvWriter = createCsvWriter({
+  //   path: 'file.csv',
+  //   header: [
+  //     { id: 'exerciseRef', title: 'Exercise Ref' },
+  //     { id: 'applicationRef', title: 'Application Ref' },
+  //     { id: 'assessorEmail', title: 'Assessor email' },
+  //   ],
+  // });
+
+  // let data = documents.filter(doc => {
+  //   if (doc.assessor.email.indexOf(' ') >= 0) {
+  //     return true;
+  //   }
+  // });
+
+  // await csvWriter.writeRecords(data.map(item => {
+  //   return {
+  //     exerciseRef: item.exercise.referenceNumber,
+  //     applicationRef: item.application.referenceNumber,
+  //     assessorEmail: `|${item.assessor.email}|`,
+  //   };
+  // }));
+
+  return true;
+};
+
+main()
+  .then((result) => {
+    console.log(result);
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit();
+  });
---
+++ nodeScripts/query.qtreport.questions-and-answers.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const config = require('./shared/config.js');
+const { app, db } = require('./shared/admin.js');
+const { getDocument } = require('../functions/shared/helpers');
+
+
+const main = async () => {
+
+  const qualifyingTestId = 'DlFf28EV5JpJ19ZbhdvZ';
+
+  // get data
+  const qualifyingTest = await getDocument(db.collection('qualifyingTests').doc(qualifyingTestId));
+
+  // question report
+  const questionData = [];
+  qualifyingTest.testQuestions.questions.forEach((question, questionIndex) => {
+    const rowData = {
+      questionNumber: questionIndex + 1,
+      questionResponses: qualifyingTest.responsesReport.questions[questionIndex].responses,
+      correct: qualifyingTest.responsesReport.questions[questionIndex].correct,
+      incorrect: qualifyingTest.responsesReport.questions[questionIndex].incorrect,
+    };
+    if (qualifyingTest.type === config.QUALIFYING_TEST.TYPE.SITUATIONAL_JUDGEMENT) {
+      rowData.partial = qualifyingTest.responsesReport.questions[questionIndex].partial;
+    }
+    questionData.push(rowData);
+  });
+
+  // answer report
+  const answerData = [];
+  qualifyingTest.testQuestions.questions.forEach((question, questionIndex) => {
+    question.options.forEach((answer, answerIndex) => {
+      const rowData = qualifyingTest.responsesReport.questions[questionIndex].answers[answerIndex];
+      rowData.questionNumber = questionIndex + 1;
+      rowData.answerNumber = answerIndex + 1;
+      rowData.questionResponses = qualifyingTest.responsesReport.questions[questionIndex].responses;
+      answerData.push(rowData);
+    });
+  });
+
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+  const questionReportHeaders = [
+    { id: 'questionNumber', title: 'Question' },
+    { id: 'questionResponses', title: 'Responses' },
+    { id: 'correct', title: 'Correct' },
+    { id: 'incorrect', title: 'Incorrect' },
+  ];
+  if (qualifyingTest.type === config.QUALIFYING_TEST.TYPE.SITUATIONAL_JUDGEMENT) {
+    answerReportHeaders.push({ id: 'partial', title: 'Partial' });
+  }
+  const questionReport = createCsvWriter({
+    path: 'questionReport.csv',
+    header: questionReportHeaders,
+  });
+  await questionReport.writeRecords(questionData);
+
+  const answerReportHeaders = [
+    { id: 'questionNumber', title: 'Question' },
+    { id: 'answerNumber', title: 'Answer' },
+    { id: 'questionResponses', title: 'Responses' },
+  ];
+  if (qualifyingTest.type === config.QUALIFYING_TEST.TYPE.SITUATIONAL_JUDGEMENT) {
+    answerReportHeaders.push({ id: 'mostAppropriate', title: 'Selected as Most Appropriate' });
+    answerReportHeaders.push({ id: 'leastAppropriate', title: 'Selected as Least Appropriate' });
+  } else {
+    answerReportHeaders.push({ id: 'selected', title: 'Selected' });
+  }
+  const answerReport = createCsvWriter({
+    path: 'answerReport.csv',
+    header: answerReportHeaders,
+  });
+  await answerReport.writeRecords(answerData);
+
+  return true;
+};
+
+main()
+  .then((result) => {
+    console.log(result);
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit();
+  });
