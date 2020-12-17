a5f9621d09a542d8d58caf1c1f49fe78e66ab38f
Lisias (Lee) Loback <lisias@loback.co.uk>
2020-12-17 11:39:15 +0000

    #475 Outreach report - generated deployable function - use a helper for the argument

--- functions/callableFunctions/generateOutreachReport.js
+++ functions/callableFunctions/generateOutreachReport.js
@@ -1,10 +1,13 @@
 const functions = require('firebase-functions');
 const { firebase, db } = require('../shared/admin.js');
+const { checkArguments } = require('../shared/helpers.js');
 const { generateOutreachReport } = require('../actions/exercises/generateOutreachReport')(firebase, db);
 
 module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
-  if (!(typeof data.exerciseId === 'string') || data.exerciseId.length === 0) {
-    throw new functions.https.HttpsError('invalid-argument', 'Please specify an "exerciseId"');
+  if (!checkArguments({
+    exerciseId: { required: true },
+  }, data)) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
   }
   if (!context.auth) {
     throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
