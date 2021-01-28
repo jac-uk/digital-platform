12df93136108e35e1a1775d056162e66006d8525
Tom Russell <TR115251@hotmail.co.uk>
2021-01-28 10:19:20 +0000

    #492/Google Sheet for collecting panel scores

--- nodeScripts/google-sheets.js
+++ nodeScripts/google-sheets.js
@@ -3,17 +3,32 @@
 const config = require('./shared/config');
 const { firebase, app, db } = require('./shared/admin.js');
 const { google } = require('googleapis');
-const { promisify } = require('util');
+// const { getDocument } = require('../functions/shared/helpers');
+// const { object } = require('firebase-functions/lib/providers/storage');
+// const { promisify } = require('util');
 
-const main = async () => {
+const main = async (spreadsheetId, range) => {
+  // const application = await getDocument(db.collection('applications').doc('testData-1'));
+  // console.log(Object.keys(application));
+  // console.table(application);
+  // console.table(app);
+  // console.table(db);
+
   return google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  }).then(auth => {
-    const api = google.sheets({ version: 'v4', auth });
-    const getSheets = promisify(api.spreadsheets.get.bind(api.spreadsheets));
-    return getSheets({ spreadsheetId: '1kOLipXEe657tKsreRbxc9uIC5x5TmOzLmpdAQONNsLA' });
   })
-  // This just prints out all Worksheet names as an example
+  .then(auth => {
+      return google.sheets({ version: 'v4', auth }).spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption: 'USER_ENTERED',
+      requestBody: {
+        values: [
+          ['Candidate Ref', 'Score'],
+        ],
+      },
+    });
+  })
   .then(({ data: { sheets } }) => {
     return sheets;
   })
@@ -22,7 +37,8 @@ const main = async () => {
   });
 };
 
-main()
+
+main('1kOLipXEe657tKsreRbxc9uIC5x5TmOzLmpdAQONNsLA', 'A1:Z1000')
   .then((result) => {
     console.log(result);
     app.delete();
