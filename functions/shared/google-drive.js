f17f0d5cdec6ebe1427ca1b26f452f872df46671
Warren Searle <warren@precise-minds.co.uk>
2021-02-18 14:25:02 +0000

    #1180 Remove old code

--- functions/shared/google-drive.js
+++ functions/shared/google-drive.js
@@ -163,3 +163,73 @@ module.exports = () => {
     return '';
   }
 };
+
+
+/* Some useful examples */
+  // // set permissions on specific folder
+  // const permission = {
+  //   'type': 'user',
+  //   'role': 'commenter',
+  //   'emailAddress': 'warren.searle@judicialappointments.digital',
+  //   supportsAllDrives: true,
+  // };
+  // await drive.addPermission(fileId, permission);
+
+
+  // // create a drive
+  // var driveMetadata = {
+  //   'name': 'WS Shared Drive',
+  // };
+  // var requestId = uuid.v4();
+  // let response = await drive.drives.create({
+  //   resource: driveMetadata,
+  //   requestId: requestId,
+  //   fields: 'id',
+  // });
+  // const driveId = response.data.id;
+  // console.log(driveId);
+
+
+  // list drives
+  // const response = await drive.drives.list();
+  // const drives = response.data.drives;
+  // console.log(drives);
+
+  // // list files on specific drive
+  // // const response = await driveService.files.list({
+  // //   corpora: 'drive',
+  // //   driveId: driveId,
+  // //   includeItemsFromAllDrives: true,
+  // //   supportsAllDrives: true,
+  // //   pageSize: 10,
+  // //   fields: 'nextPageToken, files(id, name)',
+  // // });
+  // // const files = response.data.files;
+  // // if (files.length) {
+  // //   files.map(async (file) => {
+  // //     console.log(file);
+  // //     console.log(await driveService.files.delete({
+  // //       fileId: file.id,
+  // //       supportsAllDrives: true,
+  // //     }));
+  // //     console.log(`${file.name} (${file.id})`);
+  // //   });
+  // // }
+
+  // // list permissions on a specific drive
+  // const response = await drive.permissions.list({
+  //   corpora: 'drive',
+  //   driveId: driveId,
+  //   fileId: '1wCqR71lOCtyTExFBZoyDVZKPeaP8tlk9-vLF8FuFoak',
+  //   includeItemsFromAllDrives: true,
+  //   supportsAllDrives: true,
+  //   pageSize: 10,
+  //   //fields: 'nextPageToken, files(id, name)',
+  // });
+  // const permissions = response.data.permissions;
+  // if (permissions.length) {
+  //   permissions.map((permission) => {
+  //     console.log(permission);
+  //     // console.log(`${file.name} (${file.id})`);
+  //   });
+  // }
