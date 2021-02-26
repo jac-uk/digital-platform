aabb683bc5f69a364302357fbda829f00e4e7ea9
Warren Searle <warren@precise-minds.co.uk>
2021-02-26 16:13:24 +0000

    #505 Cloud function for transferHandoverData

---
+++ functions/callableFunctions/transferHandoverData.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+const config = require('../shared/config');
+const { firebase, db } = require('../shared/admin.js');
+const { checkArguments } = require('../shared/helpers.js');
+const transferHandoverData = require('../actions/exercises/transferHandoverData')(config, firebase, db);
+
+module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  if (!checkArguments({
+    exerciseId: { required: true },
+  }, data)) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
+  }
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  return transferHandoverData(data);
+});
