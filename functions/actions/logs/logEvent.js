e54e7b6a576469364bb078197f3cd7c3203381ab
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2021-04-19 13:15:22 +0100

    logging (back-end) (#526)
    * WIP
    
    * WIP - log events
    
    * logging - attempt to get the currently logged in user, where possible
    
    * log events for all callable functions
    
    * Bug Fix - logEvent() function call fails if current logged in user cannot be obtained
    
    * Bug Fix - event log rules were wrong

---
+++ functions/actions/logs/logEvent.js
@@ -0,0 +1,42 @@
+const { auth } = require('../../shared/admin.js');
+
+module.exports = (firebase, db) => {
+  return {
+    logEvent,
+  };
+
+  /**
+   * Logs an event in the firestore database
+   *
+   * @param {string} type - The type of event (info, warning or error)
+   * @param {string} description - Description of the event
+   * @param {object} details - Details of the event
+   * @param {ojbect} user - If not specified, the current logged in user will be used
+   * @returns {string} - Document ref for the event
+   */
+  async function logEvent(type, description, details, user) {
+
+    // if a user has not been specified in the function call, attempt to get the currently logged in user
+    if (typeof(user) === 'undefined' && auth.currentUser) {
+      user = {
+        id: auth.currentUser.uid,
+        name: auth.currentUser.displayName,
+      };
+    }
+
+    // construct the event document
+    const event = {
+      timestamp: firebase.firestore.Timestamp.now(),
+      user: user || null,
+      description: description,
+      details: details,
+    };
+
+    // store the event document in the database
+    let docRef = await db.collection('/logs/' + type + '/events').add(event);
+
+    // return the unique key of the document that was created
+    return docRef.id;
+  }
+
+};
--- functions/callableFunctions/generateDiversityReport.js
+++ functions/callableFunctions/generateDiversityReport.js
@@ -1,15 +1,37 @@
 const functions = require('firebase-functions');
 const { firebase, db } = require('../shared/admin.js');
 const { generateDiversityReport } = require('../actions/exercises/generateDiversityReport')(firebase, db);
+const { getDocument } = require('../shared/helpers');
+const { logEvent } = require('../actions/logs/logEvent')(firebase, db);
 
 module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
-  if (!(typeof data.exerciseId === 'string') || data.exerciseId.length === 0) {
-    throw new functions.https.HttpsError('invalid-argument', 'Please specify an "exerciseId"');
-  }
+
+  // authenticate the request
   if (!context.auth) {
     throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
   }
+
+  // validate input parameters
+  if (!(typeof data.exerciseId === 'string') || data.exerciseId.length === 0) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please specify an "exerciseId"');
+  }
+
+  // generate the report
   const result = await generateDiversityReport(data.exerciseId);
+
+  // log an event
+  const exercise = await getDocument(db.collection('exercises').doc(data.exerciseId));
+  let details = {
+    exerciseId: exercise.id,
+    exerciseRef: exercise.referenceNumber,
+  };
+  let user = {
+    id: context.auth.token.user_id,
+    name: context.auth.token.name,
+  };
+  await logEvent('info', 'Diversity report generated', details, user);
+
+  // return the report to the caller
   return {
     result: result,
   };
