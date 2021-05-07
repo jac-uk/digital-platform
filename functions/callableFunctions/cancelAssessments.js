b7408fd5b7dfb5319eb050707c1d02549579e7e2
Tom Gillespy <halcyon@judicialappointments.digital>
2021-05-07 13:49:32 +0100

    switch callables to authenticate first before validation

--- functions/callableFunctions/cancelAssessments.js
+++ functions/callableFunctions/cancelAssessments.js
@@ -5,13 +5,13 @@ const { checkArguments } = require('../shared/helpers.js');
 const { cancelAssessments } = require('../actions/assessments')(config, firebase, db);
 
 module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
   if (!checkArguments({
     exerciseId: { required: true },
   }, data)) {
     throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
-  }
-  if (!context.auth) {
-    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
   }
   const result = await cancelAssessments(data);
   return {
--- functions/callableFunctions/initialiseAssessments.js
+++ functions/callableFunctions/initialiseAssessments.js
@@ -5,14 +5,14 @@ const { checkArguments } = require('../shared/helpers.js');
 const { initialiseAssessments } = require('../actions/assessments')(config, firebase, db);
 
 module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
   if (!checkArguments({
     exerciseId: { required: true },
     stage: { required: true },
   }, data)) {
     throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
-  }
-  if (!context.auth) {
-    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
   }
   const result = await initialiseAssessments(data);
   return {
