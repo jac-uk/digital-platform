ed684cf584e38dd55d584734e6db49a57bef48df
Adam Lewis <halcyon@judicialappointments.digital>
2021-06-30 17:43:38 +0100

    Added diversity data to new applicationRecord document

--- functions/shared/factories.js
+++ functions/shared/factories.js
@@ -148,6 +148,73 @@ module.exports = (CONSTANTS) => {
     return assessment;
   }
 
+  function newDiversityFlags(application) {
+    const applicationData = application.equalityAndDiversitySurvey ? application.equalityAndDiversitySurvey : application;
+    const data = {
+      gender: applicationData.gender,
+      ethnicity: null,
+      disability: applicationData.disability,
+      professionalBackground: {
+        barrister: null,
+        cilex: null,
+        solicitor: null,
+        other: null,
+        preferNotToSay: null,
+      },
+      socialMobility: {
+        attendedUKStateSchool: null,
+        firstGenerationUniversity: null,
+      },
+    };
+    if (applicationData.ethnicGroup) {
+      switch (applicationData.ethnicGroup) {
+        case 'uk-ethnic':
+        case 'irish':
+        case 'gypsy-irish-traveller':
+        case 'other-white':
+          data.ethnicity = 'white';
+          break;
+        case 'prefer-not-to-say':
+          data.ethnicity = applicationData.ethnicGroup;
+          break;
+        case 'other-ethnic-group':
+          data.ethnicity = applicationData.ethnicGroup;
+          break;
+        default: // @todo check catch all is appropriate for bame
+          data.ethnicity = 'bame';
+      }
+    }
+    if (applicationData.professionalBackground && applicationData.professionalBackground.length) {
+      if (applicationData.professionalBackground.indexOf('barrister') >= 0) {
+        data.professionalBackground.barrister = true;
+      }
+      if (applicationData.professionalBackground.indexOf('cilex') >= 0) {
+        data.professionalBackground.cilex = true;
+      }
+      if (applicationData.professionalBackground.indexOf('solicitor') >= 0) {
+        data.professionalBackground.solicitor = true;
+      }
+      if (applicationData.professionalBackground.indexOf('other-professional-background') >= 0) {
+        data.professionalBackground.other = true;
+      }
+      if (applicationData.professionalBackground.indexOf('prefer-not-to-say') >= 0) {
+        data.professionalBackground.preferNotToSay = true;
+      }
+    }
+
+    if (
+      application.stateOrFeeSchool === 'uk-state-selective'
+      || application.stateOrFeeSchool === 'uk-state-non-selective'
+    ) {
+      data.socialMobility.attendedUKStateSchool = true;
+    }
+    if (application.firstGenerationStudent === true) {
+      data.socialMobility.firstGenerationUniversity = true;
+    }
+
+    return data;
+  }
+
   function newApplicationRecord(exercise, application) {
     let applicationRecord = {
       exercise: {
@@ -178,6 +245,7 @@ module.exports = (CONSTANTS) => {
         characterIssues: [],
         eligibilityIssues: [],
       },
+      diversity: newDiversityFlags(application),
       history: [],
       notes: [],
     };
