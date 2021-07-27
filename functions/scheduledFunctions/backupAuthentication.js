fdb3c248b1b9d012a67b3024266c3726b63e2fb5
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2021-07-27 09:03:11 +0100

    #473 purge backups after 30 days (#626)
    * WIP - delete backups after 30 days
    
    * no message
    
    * Update firestore.js
    
    * Update backupFirestore.js
    
    * Update backupAuthentication.js
    
    * Update firestore.js

--- functions/scheduledFunctions/backupAuthentication.js
+++ functions/scheduledFunctions/backupAuthentication.js
@@ -1,7 +1,8 @@
 const config = require('../shared/config');
-const SCHEDULE = 'every day 23:01';
 const functions = require('firebase-functions');
 const { backupAuthentication } = require('../actions/backup/authentication')(config);
+
+const SCHEDULE = 'every day 23:00';
 
 module.exports = functions.region('europe-west2')
   .pubsub
