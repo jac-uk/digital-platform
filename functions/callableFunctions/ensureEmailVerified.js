690d32d8585a011d596ea0fe10cab41b48d4fc3b
Tom Gillespy <halcyon@judicialappointments.digital>
2021-08-18 10:17:37 +0100

    Adding ensureEmailVerified function to change a users email verified flag to true

---
+++ functions/callableFunctions/ensureEmailVerified.js
@@ -0,0 +1,21 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+module.exports = functions.region('europe.west-2').https.onCall(async (data, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  try {
+    const auth = admin.auth();
+    const uid = context.auth.uid;
+    const user = await auth.getUser(uid);
+    if (user.emailVerified === false) {
+      await auth.updateUser(uid, {
+        emailVerified: true,
+      });
+    }
+  } catch (e) {
+    throw new functions.https.HttpsError('unknown', e);
+  }
+  return {};
+});
