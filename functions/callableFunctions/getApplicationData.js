00f7b23f46f7419e60c32f418b8b924b0c2a3531
Adam Lewis <halcyon@judicialappointments.digital>
2021-09-22 23:15:03 +0100

    Update getApplicationData.js

--- functions/callableFunctions/getApplicationData.js
+++ functions/callableFunctions/getApplicationData.js
@@ -9,10 +9,10 @@ module.exports = functions.region('europe-west2').https.onCall(async (data, cont
   if (!context.auth) {
     throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
   }
-  if (!checkArguments({
-    exerciseId: { required: true },
-  }, data)) {
-    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
-  }
+  // if (!checkArguments({
+  //   exerciseId: { required: true },
+  // }, data)) {
+  //   throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
+  // }
   return getApplicationData(data);
 });
