5e8e72398dc613f6f96c3b61e6a97cb348334a53
HalcyonJAC <halcyon@judicialappointments.digital>
2022-01-21 17:14:32 +0000

    redo a commit that was previously rolled-back

--- test/rules/3.exercises.spec.js
+++ test/rules/3.exercises.spec.js
@@ -1,4 +1,4 @@
-const { setup, teardown, setupAdmin } = require('./helpers');
+const { setup, teardown, setupAdmin, getValidExerciseData } = require('./helpers');
 const { assertFails, assertSucceeds } = require('@firebase/rules-unit-testing');
 
 describe('Exercises', () => {
@@ -9,32 +9,37 @@ describe('Exercises', () => {
   context('Create', () => {
     it('prevent un-authenticated user from creating an exercise', async () => {
       const db = await setup();
-      await assertFails(db.collection('exercises').add({}));
+      await assertFails(db.collection('exercises').add(getValidExerciseData()));
     });
 
     it('prevent authenticated user from creating an exercise', async () => {
       const db = await setup({ uid: 'user1', email: 'user@email.com', email_verified: false });
-      await assertFails(db.collection('exercises').add({}));
+      await assertFails(db.collection('exercises').add(getValidExerciseData()));
     });
 
     it('prevent authenticated user with verified email from creating an exercise', async () => {
       const db = await setup({ uid: 'user1', email: 'user@email.com', email_verified: true });
-      await assertFails(db.collection('exercises').add({}));
+      await assertFails(db.collection('exercises').add(getValidExerciseData()));
     });
 
     it('prevent authenticated user with un-verified JAC email from creating an exercise', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.digital', email_verified: false });
+      await assertFails(db.collection('exercises').add(getValidExerciseData()));
+    });
+
+    it('prevent authenticated user with verified @judicialappointments.digital email to create an exercise with no data', async () => {
+      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true });
       await assertFails(db.collection('exercises').add({}));
     });
 
     it('allow authenticated user with verified @judicialappointments.digital email to create an exercise', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true });
-      await assertSucceeds(db.collection('exercises').add({}));
+      await assertSucceeds(db.collection('exercises').add(getValidExerciseData()));
     });
 
     it('allow authenticated user with verified @judicialappointments.gov.uk email to create an exercise', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
-      await assertSucceeds(db.collection('exercises').add({}));
+      await assertSucceeds(db.collection('exercises').add(getValidExerciseData()));
     });
   });
 
@@ -74,7 +79,7 @@ describe('Exercises', () => {
     it('prevent un-authenticated user from updating an exercise', async () => {
       const db = await setup();
       await setupAdmin(db, { 'exercises/ex1': { } });
-      await assertFails(db.collection('exercises').doc('ex1').update({}));
+      await assertFails(db.collection('exercises').doc('ex1').update(getValidExerciseData()));
     });
 
     it('prevent authenticated user from updating an exercise', async () => {
@@ -82,7 +87,7 @@ describe('Exercises', () => {
         {  uid: 'user1', email: 'user@email.com', email_verified: false },
         { 'exercises/ex1': { } }
       );
-      await assertFails(db.collection('exercises').doc('ex1').update({}));
+      await assertFails(db.collection('exercises').doc('ex1').update(getValidExerciseData()));
     });
 
     it('prevent authenticated user with verified email from updating an exercise', async () => {
@@ -90,7 +95,7 @@ describe('Exercises', () => {
         {  uid: 'user1', email: 'user@email.com', email_verified: true },
         { 'exercises/ex1': { } }
       );
-      await assertFails(db.collection('exercises').doc('ex1').update({}));
+      await assertFails(db.collection('exercises').doc('ex1').update(getValidExerciseData()));
     });
 
     it('prevent authenticated user with un-verified JAC email from updating an exercise', async () => {
@@ -98,7 +103,15 @@ describe('Exercises', () => {
         { uid: 'user1', email: 'user@judicialappointments.digital', email_verified: false },
         { 'exercises/ex1': { } }
       );
-      await assertFails(db.collection('exercises').doc('ex1').update({}));
+      await assertFails(db.collection('exercises').doc('ex1').update(getValidExerciseData()));
+    });
+
+    it('prevent authenticated user with verified @judicialappointments.digital email to update an exercise with missing data', async () => {
+      const db = await setup(
+        { uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true },
+        { 'exercises/ex1': { } }
+      );
+      await assertFails(db.collection('exercises').doc('ex1').update(getValidExerciseData()));
     });
 
     it('allow authenticated user with verified @judicialappointments.digital email to update an exercise', async () => {
@@ -106,7 +119,7 @@ describe('Exercises', () => {
         { uid: 'user1', email: 'user@judicialappointments.digital', email_verified: true },
         { 'exercises/ex1': { } }
       );
-      await assertSucceeds(db.collection('exercises').doc('ex1').update({}));
+      await assertSucceeds(db.collection('exercises').doc('ex1').update(getValidExerciseData()));
     });
 
     it('allow authenticated user with verified @judicialappointments.gov.uk email to update an exercise', async () => {
@@ -114,7 +127,7 @@ describe('Exercises', () => {
         { uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true },
         { 'exercises/ex1': { } }
       );
-      await assertSucceeds(db.collection('exercises').doc('ex1').update({}));
+      await assertSucceeds(db.collection('exercises').doc('ex1').update(getValidExerciseData()));
     });
   });
 
