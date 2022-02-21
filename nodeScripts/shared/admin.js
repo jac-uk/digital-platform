e3f0c0651c450be916258935e5dd22e763044fba
halcyonjac <halcyon@judicialappointments.digital>
2022-02-21 16:46:12 +0000

    fixed firestore bandwidth exhausted problem

--- nodeScripts/shared/admin.js
+++ nodeScripts/shared/admin.js
@@ -2,10 +2,14 @@
  * Initialises Admin SDK and exports firestore database connection
  */
 const admin = require('firebase-admin');
+const grpc = require('grpc');
 
 admin.initializeApp();
 
+const firestore = admin.firestore();
+firestore.settings({ grpc });
+
 exports.firebase = admin;
-exports.db = admin.firestore();
+exports.db = firestore;
 exports.app = admin.app();
 exports.auth = admin.auth();
