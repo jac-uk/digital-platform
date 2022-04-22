0a8750883c8b06c305dd90be3dd95861bfe8fc7e
Warren Searle <warren@precise-minds.co.uk>
2022-04-22 09:29:57 +0100

    admin#491 Merit List. Initialise a task

---
+++ functions/callableFunctions/initialiseTask.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const config = require('../shared/config');
+const { firebase, db } = require('../shared/admin.js');
+const { checkArguments } = require('../shared/helpers.js');
+const initialiseTask = require('../actions/tasks/initialiseTask')(config, firebase, db);
+const { checkFunctionEnabled } = require('../shared/serviceSettings.js')(db);
+
+module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  await checkFunctionEnabled();
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  if (!checkArguments({
+    exerciseId: { required: true },
+    type: { required: true },
+    stage: { required: true },
+    status: { required: false },
+  }, data)) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
+  }
+  return initialiseTask(data);
+});
