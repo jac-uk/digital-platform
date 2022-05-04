9422a1197fbaeb217638280bc840cf207afc3380
halcyonjac <halcyon@judicialappointments.digital>
2022-05-04 09:42:35 +0100

    fixed unit tests

--- test/rules/11.notes.spec.js
+++ test/rules/11.notes.spec.js
@@ -9,12 +9,12 @@ describe('Notes', () => {
   context('Create', () => {
     it('prevent JAC admin without permission from creating notes', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
-      await assertFails(db.collection('notes').add({}));
+      await assertFails(db.collection('notes').add({ body: '', createdBy: '' }));
     });
 
     it('allow JAC admin with permission to create notes', async () => {
       const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true, rp: ['nt2'] });
-      await assertFails(db.collection('notes').add({}));
+      await assertSucceeds(db.collection('notes').add({ body: '', createdBy: '' }));
     });
   });
 
@@ -32,24 +32,36 @@ describe('Notes', () => {
 
   context('Update', () => {
     it('prevent JAC admin without permission from updating notes', async () => {
-      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
-      await assertFails(db.collection('notes').doc('note1').update({}));
+      const db = await setup(
+        { uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true },
+        { 'notes/note1': {} }
+      );
+      await assertFails(db.collection('notes').doc('note1').update({ body: '', createdBy: '' }));
     });
 
     it('allow JAC admin with permission to update notes', async () => {
-      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true, rp: ['nt3'] });
-      await assertFails(db.collection('notes').doc('note1').update({}));
+      const db = await setup(
+        { uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true, rp: ['nt3'] },
+        { 'notes/note1': {} }
+      );
+      await assertSucceeds(db.collection('notes').doc('note1').update({ body: '', createdBy: '' }));
     });
   });
 
   context('Delete', () => {
     it('prevent JAC admin without permission from deleting notes', async () => {
-      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true });
+      const db = await setup(
+        { uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true },
+        { 'notes/note1': {} }
+      );
       await assertFails(db.collection('notes').doc('note1').delete());
     });
 
     it('allow JAC admin with permission to delete notes', async () => {
-      const db = await setup({ uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true, rp: ['nt4'] });
+      const db = await setup(
+        { uid: 'user1', email: 'user@judicialappointments.gov.uk', email_verified: true, rp: ['nt4'] },
+        { 'notes/note1': {} }
+      );
       await assertSucceeds(db.collection('notes').doc('note1').delete());
     });
   });
