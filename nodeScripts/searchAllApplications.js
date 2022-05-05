2a47a0d603ea0f7983b3d310105fa72e16b19720
warrensearle <warren@precise-minds.co.uk>
2022-05-05 11:03:15 +0100

    Service status check (#687)
    * service status check
    
    * ?
    
    * Change to use serviceSettings module
    
    * 1.14.1 (#688)
    
    * service setting start
    
    * check service status in functions
    
    * Service offline scripts
    
    * Fix storage rules
    
    * Package scripts for staging and production
    
    * Fixes
    
    Co-authored-by: Tom Russell <TR115251@hotmail.co.uk>
    
    * Update activateQualifyingTest.js
    
    * Update initialiseQualifyingTest.js
    
    * Update package-lock.json
    
    * Update package.json
    
    * package-lock.json revert
    
    * package-lock.json revert
    
    Co-authored-by: Tom Russell <TR115251@hotmail.co.uk>

--- nodeScripts/searchAllApplications.js
+++ nodeScripts/searchAllApplications.js
@@ -2,21 +2,18 @@
 
 const config = require('./shared/config');
 const { firebase, app, db } = require('./shared/admin.js');
-const initialiseQualifyingTest = require('../functions/actions/qualifyingTests/initialiseQualifyingTest')(config, firebase, db);
+const { customSearch } = require('../functions/actions/applications')(config, firebase, db);
 
 const main = async () => {
-  return initialiseQualifyingTest({
-    qualifyingTestId: 'sKqACHtcOYBFfHcVQry8',
-  });
+  return customSearch();
 };
-
 main()
-  .then((result) => {
+.then((result) => {
     console.log(result);
     app.delete();
     return process.exit();
   })
   .catch((error) => {
-    console.log(error);
+    console.error(error);
     process.exit();
   });
