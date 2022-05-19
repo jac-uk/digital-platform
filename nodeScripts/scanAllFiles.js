ff815cd948b984c93e6d42ea205739342fb3daae
HalcyonJAC <halcyon@judicialappointments.digital>
2022-05-19 09:51:20 +0100

    node script to run the 'scanAllFiles' function

---
+++ nodeScripts/scanAllFiles.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const config = require('./shared/config');
+const { firebase, app } = require('./shared/admin.js');
+const { scanAllFiles } = require('../functions/actions/malware-scanning/scanAllFiles')(config, firebase);
+
+const main = async () => {
+  return scanAllFiles(true, 5);
+};
+
+main()
+  .then((result) => {
+    console.log(result);
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit();
+  });
