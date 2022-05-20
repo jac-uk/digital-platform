1dc6d500675cfbcae3cc17ab8a6b08833d09a6cc
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2022-05-20 09:23:35 +0100

    #696 run virus scanning every night (#721)
    * change virus scanning schedule
    
    * bug fix - overnight scanning not working
    
    * run at 1pm
    
    * renamed file and set to 2pm
    
    * set time to 2am
    
    * no message
    
    * node script to run the 'scanAllFiles' function
    
    * bug fix
    
    * tidy-up
    
    * update the log file every 10 files that are scanned
    
    * run virus scanning every 1 for a few days

--- functions/scheduledFunctions/scheduleScanAllFiles.js
+++ functions/scheduledFunctions/scheduleScanAllFiles.js
@@ -1,7 +1,10 @@
 const functions = require('firebase-functions');
-const { scanAllFiles } = require('../actions/malware-scanning/scanAllFiles');
+const config = require('../shared/config');
+const { firebase } = require('../shared/admin');
+const { scanAllFiles } = require('../actions/malware-scanning/scanAllFiles')(config, firebase);
 
-const SCHEDULE = 'every day 02:00';
+// const SCHEDULE = 'every day 02:00';
+const SCHEDULE = 'every 1 hours'; // this setting is temporary
 
 module.exports = functions.region('europe-west2')
   .pubsub
