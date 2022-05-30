174e432e7a3fea3492cdb9b42fcd8b0c56a88191
halcyonjac <halcyon@judicialappointments.digital>
2022-05-30 14:19:24 +0100

    WIP

--- functions/shared/htmlWriter.js
+++ functions/shared/htmlWriter.js
@@ -9,13 +9,14 @@ const DEFAULT_STYLESHEET = `
       width: 50%;
       text-align: left;
       border-bottom: solid 1px #0B0C14;
-      padding: 8px;
+      padding: 4px 8px;
       vertical-align: top;
     }
     td {
       color: #0B0C14;
       border-bottom: solid 1px #0B0C14;
-      padding: 8px;
+      padding: 4px 8px;
+      vertical-align:top;
     }
     table {
       border-spacing: 0;
@@ -31,6 +32,29 @@ const DEFAULT_STYLESHEET = `
     h4 {
       padding-top: 30px;
     }
+    .red {
+      color:red;
+    }
+    .gray {
+      color:gray;
+    }
+    p, ul {
+      margin-bottom: 15px;
+    }
+    .vertical-text {
+      transform: rotate(-90deg);
+      /* Legacy vendor prefixes that you probably don't need... */
+      /* Safari */
+      -webkit-transform: rotate(-90deg);
+      /* Firefox */
+      -moz-transform: rotate(-90deg);
+      /* IE */
+      -ms-transform: rotate(-90deg);
+      /* Opera */
+      -o-transform: rotate(-90deg);
+      /* Internet Explorer */
+      filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
+    }
   </style>
 `;
 
@@ -39,8 +63,8 @@ class htmlWriter {
     this.html = '';
     this.stylesheet = '';
   }
-  addPageBreak() {
-    this.html += '<hr class="pb">'; /* Google Docs interprets this is a page break (don't know why) */
+  defaultStylesheet() {
+    return DEFAULT_STYLESHEET;
   }
   toString() {
     if (this.html !== '') {
@@ -64,10 +88,14 @@ class htmlWriter {
   addTitle(data, textAlign = 'left') {
     this.html += `<h2 id="title" style="text-align: ${textAlign}">${data}</h2>`;
   }
-  addHeading(data, textAlign = 'left', fontSize = 'inherit', extraStyle) {
+  addHeading(data, textAlign = 'left', fontSize = 'inherit', extraStyle = '') {
     // data = data.match(/[A-Z][a-z]+|[0-9]+/g).join(' ');
     let style = `style="text-align: ${textAlign}; font-size: ${fontSize}; ${extraStyle}"`;
     this.html += `<h4 id="${data.split(' ').join('_')}_heading" ${style}>${data}</h4>`;
+  }
+  addHeadingRaw(html, textAlign = 'left', fontSize = 'inherit', extraStyle = '') {
+    let style = `style="text-align: ${textAlign}; font-size: ${fontSize}; ${extraStyle}"`;
+    this.html += `<h4 ${style}>${html}</h4>`;
   }
   addTable(data) {
     const tableStart = '<table>';
@@ -93,11 +121,15 @@ class htmlWriter {
     rowHtml.push(tableEnd);
     this.html += rowHtml.join('');
   }
-  addParagraph(data, textAlign = 'left') {
-    this.html += `<p style="text-align: ${textAlign}">${data}</p>`;
+  addParagraph(data, textAlign = 'left', fontSize = 'inherit', extraStyle = '') {
+    let style = `style="text-align: ${textAlign}; font-size: ${fontSize}; ${extraStyle}"`;
+    this.html += `<p ${style}">${data}</p>`;
   }
   addRaw(s) {
     this.html += s;
+  }
+  addPageBreak() {
+    this.html += '<hr class="pb">'; /* Google Docs interprets this is a page break (don't know why) */
   }
 }
 
