1f7c2136f6b3faa98dc38dc9d61e3c57ac94c3b1
Lisias (Lee) Loback <lloback@users.noreply.github.com>
2022-07-22 10:42:32 +0100

    [1667] Targeted Outreach Report for digital-platform (#741)
    Co-authored-by: Lisias (Lee) Loback <lisias@loback.co.uk>

---
+++ functions/callableFunctions/targetedOutreachReport.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const { firebase, db } = require('../shared/admin.js');
+const { checkArguments } = require('../shared/helpers.js');
+const { targetedOutreachReport } = require('../actions/exercises/targetedOutreachReport')(firebase, db);
+const { checkFunctionEnabled } = require('../shared/serviceSettings.js')(db);
+
+module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  await checkFunctionEnabled();
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+  if (!checkArguments({
+    exerciseId: { required: true },
+  }, data)) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please provide valid arguments');
+  }
+  const result = await targetedOutreachReport(data);
+  return result;
+});
---
+++ nodeScripts/targetedOutreachReport.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { firebase, app, db } = require('./shared/admin.js');
+const { targetedOutreachReport } = require('../functions/actions/exercises/targetedOutreachReport')(firebase, db);
+
+const main = async () => {
+
+  //  '	 ',
+  //  ' ',                  // 30
+  //  'AA 123456       V',  // 0
+  //  'TR768534A',          // 12
+  //  'BN678987G',          // 47 
+  //  'A B  1 2 3 4 5 6 C', // 32
+  //  'GG010 203D',         // 0
+  //  'QQ 12 34 56 C',      // 44 => 9 normalized
+  //  'PP005897 C',         // 1 normalized
+  //  'PP 060953 C',        // 2 normalized
+  //  'PP024177C',          //1 normalized
+  //  'PP024174C',          // 0
+  //  'PP-012189C',         // 1 normalized
+  //  'PP024177C',          // 1
+  //  'PP 060953 C',        // 2 normalized
+
+  return targetedOutreachReport([
+    'QQ 12 34 56 C',
+  ]);
+};
+
+main()
+  .then((result) => {
+    console.log(result);
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit();
+  });
