a12e5f3f527aba5badf735248e51456009d41bbd
halcyonjac <halcyon@judicialappointments.digital>
2022-09-14 17:44:07 +0100

    tidy up

--- functions/scheduledFunctions/backupAuthentication.js
+++ functions/scheduledFunctions/backupAuthentication.js
@@ -1,7 +1,6 @@
 const config = require('../shared/config');
 const functions = require('firebase-functions');
 const { backupAuthentication } = require('../actions/backup/authentication')(config);
-const { wrapFunction } = require('../shared/sentry')(config);
 
 const SCHEDULE = 'every day 23:00';
 
@@ -9,7 +8,7 @@ module.exports = functions.region('europe-west2')
   .pubsub
   .schedule(SCHEDULE)
   .timeZone('Europe/London')
-  .onRun(wrapFunction(async () => {
+  .onRun(async () => {
     const result = await backupAuthentication();
     return result;
-  }));
+  });
--- functions/scheduledFunctions/backupFirestore.js
+++ functions/scheduledFunctions/backupFirestore.js
@@ -1,7 +1,6 @@
 const config = require('../shared/config');
 const functions = require('firebase-functions');
 const { backupFirestore } = require('../actions/backup/firestore')(config);
-const { wrapFunction } = require('../shared/sentry')(config);
 
 const SCHEDULE = 'every day 23:01';
 
@@ -9,7 +8,7 @@ module.exports = functions.region('europe-west2')
   .pubsub
   .schedule(SCHEDULE)
   .timeZone('Europe/London')
-  .onRun(wrapFunction(async () => {
+  .onRun(async () => {
     const result = await backupFirestore();
     return result;
-  }));
+  });
