f6e23bfcf4eb2faaa2dc76dc23166ea6fdfcb305
drieJac <jac.drie@digitalteam.uk>
2023-01-09 17:39:03 +0000

    Fixed bug with missing function arguments.

--- functions/actions/exercises/updateLateApplicationRequests.js
+++ functions/actions/exercises/updateLateApplicationRequests.js
@@ -11,7 +11,7 @@ module.exports = (firebase, db) => {
     await db.doc(`exercises/${exerciseId}`).update(saveData);
   }
 
-  async function removeLateApplicationRequest(params, context) {
+  async function removeLateApplicationRequest(exerciseId, candidateId) {
     const saveData = {};
     saveData['_lateApplicationRequests'] = firebase.firestore.FieldValue.arrayRemove(candidateId);
     await db.doc(`exercises/${exerciseId}`).update(saveData);
