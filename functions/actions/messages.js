e48f7c5f98ffc1f4cdc95a6587d5a7143f6476c4
drieJac <jac.drie@digitalteam.uk>
2023-01-13 16:44:04 +0000

    Fixed bug whereby the candidate id was not being removed from the _lateApplicationRequests in the exercise.

--- functions/actions/messages.js
+++ functions/actions/messages.js
@@ -9,13 +9,13 @@ module.exports = (config, firebase, db, auth) => {
    * Message created event handler
    * - Checks type of message
    * - For type = lateApplicationRequest ensure the candidate id is added to the _lateApplicationRequests field in the exercise
-   * - For type = lateApplicationApproval ensure the candidate id is removed from the _lateApplicationRequests field in the exercise
+   * - For type = lateApplicationResponse ensure the candidate id is removed from the _lateApplicationRequests field in the exercise
    */
   async function onMessageCreate(type, exerciseId, candidateId) {
     if (type === 'lateApplicationRequest') {
       await addLateApplicationRequest(exerciseId, candidateId);
     }
-    else if (type === 'lateApplicationApproval') {
+    else if (type === 'lateApplicationResponse') {
       await removeLateApplicationRequest(exerciseId, candidateId);
     }
   }
