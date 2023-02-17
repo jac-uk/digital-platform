25b58011713e2c015eecb5e00ac68280d0a1fa6e
drieJac <jac.drie@digitalteam.uk>
2023-02-17 16:28:58 +0000

    Fixed bug whereby emails weren't getting sent as the notification expected a string but instead was getting an array of objects. Fixed so each individual email address is associated with a notification. Added nodescript and new command to help testing this functionality.

--- functions/actions/exercises/sendExerciseReadyForApproval.js
+++ functions/actions/exercises/sendExerciseReadyForApproval.js
@@ -16,14 +16,20 @@ module.exports = (config, firebase, db, auth) => {
   async function sendExerciseReadyForApproval(params) {
     const exerciseId = params.exerciseId;
     const exercise = params.exercise;
+    const toEmails = exercise.seniorSelectionExerciseManager;
 
     // create database commands
     const commands = [];
-    // create notification
-    commands.push({
-      command: 'set',
-      ref: db.collection('notifications').doc(),
-      data: newNotificationExerciseApprovalSubmit(firebase, exerciseId, exercise),
+
+    toEmails.forEach(emailObj => {
+      const email = emailObj.name;  // @todo: This should use an appropriate key instead of 'name' as it's misleading
+
+      // create notification
+      commands.push({
+        command: 'set',
+        ref: db.collection('notifications').doc(),
+        data: newNotificationExerciseApprovalSubmit(firebase, exerciseId, exercise, email),
+      });
     });
 
     // write to db
