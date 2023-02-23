0545e3f1e55f334dded18141cb25b52668a6332f
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2023-02-23 09:06:20 +0000

    Script to enable Justice.gov.uk users from Admin authentication (#843)
    * add node script
    
    * update script

---
+++ nodeScripts/tidyAuthUsers.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { app } = require('./shared/admin');
+const { listAllUsers, updateUser, log } = require('./shared/helpers');
+
+// whether to make changes in authentication database
+const isAction = false;
+
+const main = async () => {
+  log('Get all users from authentication database...');
+  let users = await listAllUsers();
+  log(`Total users: ${users.length}`);
+
+  log('Filter disabled users with email *@justice.gov.uk...');
+  const filteredUsers = users.filter(item =>
+    item.email.indexOf('@justice.gov.uk') > 0 &&
+    item.disabled &&
+    item.providerData.length &&
+    item.providerData.some(provider => provider.providerId === 'password')
+  );
+  log(`Total filtered users: ${filteredUsers.length}`);
+
+  log('Tidy up users...');
+  const result = [];
+  let count = 0;
+  for (let i = 0; i < filteredUsers.length; i++) {
+    const user = filteredUsers[i];
+    if (isAction) {
+      // enable account
+      await updateUser(user.uid, { disabled: false });
+    }
+    count++;
+    result.push(`${count}. ${user.uid}, ${user.email}`);
+  }
+  log('Tidy up users...done');
+
+  return result;
+};
+
+main()
+  .then((result) => {
+    console.log(result.join('\n'));
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit();
+  });
