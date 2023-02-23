e9d9d310e4adf9847b7730c30635e242a8d3645a
halcyonjac <halcyon@judicialappointments.digital>
2023-02-23 11:51:13 +0000

    udpate reasonable adjustments report

--- functions/actions/exercises/generateReasonableAdjustmentsReport.js
+++ functions/actions/exercises/generateReasonableAdjustmentsReport.js
@@ -17,7 +17,7 @@ module.exports = (firebase, db) => {
     const headers = reportHeaders();
 
     // get report rows
-    const rows = reportData(db, applications);
+    const rows = await reportData(firebase, db, applications);
 
     // construct the report document
     const report = {
@@ -46,23 +46,52 @@ const reportHeaders = () => {
     { title: 'Email', ref: 'email' },
     { title: 'Phone number', ref: 'phone' },
     { title: 'Details', ref: 'details' },
+    { title: 'Most recent exercise', ref: 'mostRecentExercise' },
+    { title: 'Most recent details', ref: 'mostRecentDetails' },
   ];
 };
 
 /**
  * Get the report data
  *
+ * @param {firebase} firebase
  * @param {db} db
  * @param {array} applications
  * @returns {array}
  */
-const reportData = (db, applications) => {
-  return applications.map((application) => {
-    return {
+const reportData = async (firebase, db, applications) => {
+  const data = [];
+  for (const application of applications) {
+    // get most recent reasonable adjustments
+    const otherApplications = await getDocuments(db.collection('applications')
+      .where('userId', '==', application.userId)
+      .where('referenceNumber', '!=', application.referenceNumber)
+      .where('personalDetails.reasonableAdjustments', '==', true)
+      .orderBy('referenceNumber', 'desc')
+      .orderBy('appliedAt', 'desc')
+      .limit(1)
+    );
+
+    let mostRecentApplicationData = {
+      mostRecentExercise: '',
+      mostRecentDetails: '',
+    };
+    if (otherApplications && otherApplications.length) {
+      const mostRecentApplication = otherApplications[0];
+      mostRecentApplicationData = {
+        mostRecentExercise: `${mostRecentApplication.exerciseRef} ${mostRecentApplication.exerciseName}`,
+        mostRecentDetails: mostRecentApplication.personalDetails.reasonableAdjustmentsDetails,
+      };
+    }
+    
+    data.push({
       name: application.personalDetails.fullName || null,
       email: application.personalDetails.email || null,
       phone: application.personalDetails.phone || null,
       details: application.personalDetails.reasonableAdjustmentsDetails || null,
-    };
-  });
+      ...mostRecentApplicationData,
+    });
+  }
+
+  return data;
 };
