570f2e1af3939d98566b9a983f01533c8fa03a17
drieJac <jac.drie@digitalteam.uk>
2023-07-27 18:15:09 +0100

    Added default back in to switch statement in ethnicityStats to pick up all of the asian ethnicities. Moved tests into separate files. Changed jest comparator to use toBeCloseTo for floating point comparisons.

---
+++ test/actions/exercises/diversityReport/empStats.spec.js
@@ -0,0 +1,45 @@
+const firebase = require('firebase-admin');
+const db = jest.fn();
+
+const {
+  empStats,
+} = require('../../../../functions/actions/exercises/generateDiversityReport.js')(firebase, db);
+
+// List of all possible emp groups
+const empGroups = [true, false, 'gender', 'ethnicity'];
+
+// Input data structure
+const applicationsRecords = empGroups.map(element => {
+  return {
+    flags: {
+      empApplied: element,
+    },
+  };
+});
+
+// Expected Results
+const results = {
+  applied: { total: 1, percent: 25 },
+  gender: { total: 1, percent: 25 },
+  ethnicity: { total: 1, percent: 25 },
+  noAnswer: { total: 1, percent: 0 },
+  declaration: { total: 4 },
+};
+
+describe('empStats', () => {
+
+  it('returns emp stats', async () => {
+
+    const resultData1 = empStats(applicationsRecords);
+    expect(resultData1.total).toEqual(results.total);
+    expect(resultData1.applied.total).toEqual(results.applied.total);
+    expect(resultData1.applied.percent).toBeCloseTo(results.applied.percent);
+    expect(resultData1.gender.total).toEqual(results.gender.total);
+    expect(resultData1.gender.percent).toBeCloseTo(results.gender.percent);
+    expect(resultData1.ethnicity.total).toEqual(results.ethnicity.total);
+    expect(resultData1.ethnicity.percent).toEqual(results.ethnicity.percent);
+    expect(resultData1.noAnswer.total).toEqual(results.noAnswer.total);
+    expect(resultData1.declaration.total).toEqual(results.declaration.total);
+  });
+
+});
