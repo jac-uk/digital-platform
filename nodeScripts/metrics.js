342a6043c768b023e863ef6e981b6cb8b5381571
warrensearle <warren@precise-minds.co.uk>
2023-07-31 17:58:51 +0100

    Update metrics nodescript

--- nodeScripts/metrics.js
+++ nodeScripts/metrics.js
@@ -5,12 +5,17 @@ const { app, db } = require('./shared/admin.js');
 
 const main = async () => {
   const stats = {};
-  const applications = await db.collection('applications').select().get();
+  const startDate = new Date(2022,7,1);
+  const applications = await db.collection('applications').where('appliedAt', '>', startDate).select().get();
   stats.applications = applications.docs.length;
-  const candidates = await db.collection('candidates').select().get();
+  const candidates = await db.collection('candidates').where('created', '>', startDate).select().get();
   stats.candidates = candidates.docs.length;
-  const exercises = await db.collection('exercises').select().get();
+  const exercises = await db.collection('exercises').where('createdAt', '>', startDate).select().get();
   stats.exercises = exercises.docs.length;
+  const qualifyingTests = await db.collection('qualifyingTests').where('startDate', '>=', startDate).where('status', '==', 'completed').select().get();
+  stats.qualifyingTests = qualifyingTests.docs.length;
+  const qualifyingTestResponses = await db.collection('qualifyingTestResponses').where('qualifyingTest.startDate', '>=', startDate).where('status', '==', 'completed').select().get();
+  stats.qualifyingTestResponses = qualifyingTestResponses.docs.length;
   return stats;
 };
 
