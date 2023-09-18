ecfe3d0f553051d286b5738018d0484e408f0505
tomlovesgithub <44227249+tomlovesgithub@users.noreply.github.com>
2023-09-18 11:15:35 +0100

    2091/self assessment document extraction (#938)
    * doc-extraction-function
    
    * wip
    
    * working proto
    
    * Update package
    
    * Fix nodescript
    
    ---------
    
    Co-authored-by: warrensearle <warren@precise-minds.co.uk>

---
+++ nodeScripts/extractDocumentContent.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const config = require('./shared/config');
+const { firebase, app } = require('./shared/admin.js');
+const { extractDocumentContent } = require('../functions/shared/file-extraction/extractDocumentContent')(config, firebase);
+
+const main = async () => {
+  return extractDocumentContent('exercise/0CgDWjU5Iap9wxBqECZD/candidate-assessement-forms_0.docx','exercise/0CgDWjU5Iap9wxBqECZD/candidate-assessement-forms_filled.docx');
+};
+
+main()
+  .then((result) => {
+    console.log(result);
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit();
+  });
