267442a422303b00403668bdbefeb2b9384a79b2
halcyonjac <halcyon@judicialappointments.digital>
2023-11-10 12:04:33 +0000

    Add _applicationVersion to mockExercise for legal exercises

--- test/shared/applicationConverter.spec.js
+++ test/shared/applicationConverter.spec.js
@@ -85,6 +85,7 @@ describe('applicationConverter', () => {
       describe('legal', () => {
         beforeEach(()=>{
           mockExercise = {
+            _applicationVersion: 2,
             typeOfExercise: 'legal',
           };
           mockApplication = {
@@ -182,6 +183,7 @@ describe('applicationConverter', () => {
               feePaidOrSalariedJudge: false,
             };
             mockExercise = {
+              _applicationVersion: 2,
               typeOfExercise: 'legal',
               previousJudicialExperienceApply: true,
             };
@@ -229,6 +231,7 @@ describe('applicationConverter', () => {
                 feePaidOrSalariedJudge: true,
               };
               mockExercise = {
+                _applicationVersion: 2,
                 typeOfExercise: 'legal',
                 previousJudicialExperienceApply: true,
               };
@@ -278,6 +281,7 @@ describe('applicationConverter', () => {
                 feePaidOrSalariedSatForThirtyDays: true,
               };
               mockExercise = {
+                _applicationVersion: 2,
                 typeOfExercise: 'legal',
                 previousJudicialExperienceApply: true,
                 pjeDays: 100,
@@ -328,6 +332,7 @@ describe('applicationConverter', () => {
                 feePaidOrSalariedSittingDaysDetails: 'fee paid details',
               };
               mockExercise = {
+                _applicationVersion: 2,
                 typeOfExercise: 'legal',
                 previousJudicialExperienceApply: true,
                 pjeDays: 100,
@@ -369,6 +374,7 @@ describe('applicationConverter', () => {
           ],
         };
         mockExercise = {
+          _applicationVersion: 2,
           typeOfExercise: 'leadership',
         };
         // set up
