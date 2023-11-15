f1adc1c4329bd98d31387660cd28666182049f70
HalcyonJAC <79906532+HalcyonJAC@users.noreply.github.com>
2023-11-15 09:44:38 +0000

    Feature/Export commissioner conflicts (#957)
    * add commissioners to vacancy model
    
    * export to doc
    
    * add style
    
    * rename variable

--- functions/index.js
+++ functions/index.js
@@ -82,6 +82,7 @@ exports.checkEnabledUserByEmail = require('./callableFunctions/checkEnabledUserB
 exports.extractDocumentContent = require('./callableFunctions/extractDocumentContent');
 exports.updateUserCustomClaims = require('./callableFunctions/updateUserCustomClaims');
 exports.createZenhubIssue = require('./callableFunctions/createZenhubIssue');
+exports.exportApplicationCommissionerConflicts = require('./callableFunctions/exportApplicationCommissionerConflicts');
 
 // Callable - QTs v2
 exports.listQualifyingTests = require('./callableFunctions/qualifyingTests/v2/listQualifyingTests');
