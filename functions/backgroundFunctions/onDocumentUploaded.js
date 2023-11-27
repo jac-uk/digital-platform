183fbf786b080875e2d930b052a6248e9ed59d4d
Tom Russell <TR115251@hotmail.co.uk>
2023-11-27 18:35:31 +0000

    tidy

--- functions/backgroundFunctions/onDocumentUploaded.js
+++ functions/backgroundFunctions/onDocumentUploaded.js
@@ -6,6 +6,6 @@ const { scanFile } = require('../actions/malware-scanning/scanFile')(config, fir
 module.exports = functions.region('europe-west2').storage
   .object()
   .onFinalize(async (object) => {
-    // console.log('object', JSON.stringify(object));
+    console.log('object', JSON.stringify(object));
     return scanFile(object.name); // file path in the bucket
   });
