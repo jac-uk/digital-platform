f2b37daa6e18293818457c57d37c0dc56bf4b647
warrensearle <warren@precise-minds.co.uk>
2023-11-29 14:16:57 +0000

    Fix lint error

--- functions/actions/tasks/updateTask.js
+++ functions/actions/tasks/updateTask.js
@@ -491,25 +491,24 @@ module.exports = (config, firebase, db) => {
     return result;
   }
 
-/**
- * Get dates between two dates
- * Optionally specify a step size (in days)
- * @param {*} startDate
- * @param {*} endDate
- * @param {*} steps
- * @returns
- */
-function dateRange(startDate, endDate, steps = 1) {
-  const dateArray = [];
-  const currentDate = new Date(startDate);
-  while (currentDate <= new Date(endDate)) {
-    dateArray.push(new Date(currentDate));
-    // Use UTC date to prevent problems with time zones and DST
-    currentDate.setUTCDate(currentDate.getUTCDate() + steps);
+  /**
+   * Get dates between two dates
+   * Optionally specify a step size (in days)
+   * @param {*} startDate
+   * @param {*} endDate
+   * @param {*} steps
+   * @returns
+   */
+  function dateRange(startDate, endDate, steps = 1) {
+    const dateArray = [];
+    const currentDate = new Date(startDate);
+    while (currentDate <= new Date(endDate)) {
+      dateArray.push(new Date(currentDate));
+      // Use UTC date to prevent problems with time zones and DST
+      currentDate.setUTCDate(currentDate.getUTCDate() + steps);
+    }
+    return dateArray;
   }
-  return dateArray;
-};
-
 
   /**
    * initialiseDataTask
