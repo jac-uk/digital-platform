ee5e08af6d2cfd32ff01ed446d2081ac06bb2ce4
tomlovesgithub <44227249+tomlovesgithub@users.noreply.github.com>
2023-11-29 15:42:26 +0000

    Restore document function (#878)
    * restore document function
    
    * move file

---
+++ nodeScripts/restoreDocument.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const config = require('../../functions/shared/config.js');
+const { auth, firebase, app, db } = require('../../functions/shared/admin.js');
+
+const main = async () => {
+  const documentId = 'JypoxJYQCvV5MEU3T5u3';
+  const collectionName = 'exercises';
+
+  const snap = await db.collection(`${collectionName}_deleted`).doc(documentId).get();
+  if (snap.exists) {
+    const docData = snap.data();
+    delete docData.deletedAt;
+    await db.collection(collectionName).doc(documentId).set(docData);
+    await db.collection(`${collectionName}_deleted`).doc(documentId).delete();
+    return true;
+  } else {
+
+    return false;
+  }
+};
+
+main()
+  .then((result) => {
+    console.log(result);
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit();
+  });
