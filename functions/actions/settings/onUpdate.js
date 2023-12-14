ddb62cd68d34f72ba004a27b6897e14d8e0e6f8c
warrensearle <warren@precise-minds.co.uk>
2023-12-14 17:09:29 +0000

    Change name from candidateData to candidateSettings

--- functions/actions/settings/onUpdate.js
+++ functions/actions/settings/onUpdate.js
@@ -4,13 +4,13 @@ module.exports = (config, firebase, db) => {
 
   /**
    * Settings/Services event handler for Update
-   * - if commissioners has changed then copy the latest names to `settings/candidateData`
+   * - if commissioners has changed then copy the latest names to `settings/candidateSettings`
    */
   async function onUpdate(dataBefore, dataAfter) {
     if (dataBefore.commissioners !== dataAfter.commissioners) {
       const saveData = {};
       saveData['commissioners'] = dataAfter.commissioners.map(item => ({ name: item.name }));
-      await db.doc('settings/candidateData').set(saveData, { merge: true });
+      await db.doc('settings/candidateSettings').set(saveData, { merge: true });
     }
     return true;
   }
