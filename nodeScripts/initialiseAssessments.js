25d316a17bdfb4f803c21f11c85ccf4fffcad8bf
drieJac <jac.drie@digitalteam.uk>
2023-12-19 11:49:28 +0000

    Added a nodescript to initialise assessments to help with a fix on production

---
+++ nodeScripts/initialiseAssessments.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Nodescript for initialising independent assessment (IA)
+ */
+
+const config = require('./shared/config.js');
+const { auth, firebase, app, db } = require('./shared/admin.js');
+const { initialiseAssessments } = require('../functions/actions/assessments.js')(config, firebase, db, auth);
+
+const main = async () => {
+  return initialiseAssessments({
+    exerciseId: 'BpEL3HgHEyHCvDMI5E9s',
+    applicationId: 's5xUIUNY7imVdNUmM6v0',
+  });
+};
+
+main()
+  .then((result) => {
+    console.log(result);
+    app.delete();
+    return process.exit();
+  })
+  .catch((error) => {
+    console.log('Error encounted while processing');
+    console.error(error);
+    process.exit();
+  });
