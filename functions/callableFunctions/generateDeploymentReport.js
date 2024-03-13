487ef4a0ec876ae0da0721642211a432b240369d
halcyonjac <halcyon@judicialappointments.digital>
2024-03-13 12:25:49 +0000

    add cloud function to generate deployment report

---
+++ functions/callableFunctions/generateDeploymentReport.js
@@ -0,0 +1,48 @@
+const functions = require('firebase-functions');
+const { firebase, db, auth } = require('../shared/admin.js');
+const { generateDeploymentReport } = require('../actions/exercises/generateDeploymentReport.js')(firebase, db);
+const { getDocument } = require('../shared/helpers.js');
+const { logEvent } = require('../actions/logs/logEvent.js')(firebase, db, auth);
+const { checkFunctionEnabled } = require('../shared/serviceSettings.js')(db);
+const { PERMISSIONS, hasPermissions } = require('../shared/permissions.js');
+
+module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
+  await checkFunctionEnabled();
+
+  // authenticate the request
+  if (!context.auth) {
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called while authenticated.');
+  }
+
+  hasPermissions(context.auth.token.rp, [
+    PERMISSIONS.exercises.permissions.canReadExercises.value,
+    PERMISSIONS.applicationRecords.permissions.canReadApplicationRecords.value,
+    PERMISSIONS.applications.permissions.canReadApplications.value,
+  ]);
+
+  // validate input parameters
+  if (!(typeof data.exerciseId === 'string') || data.exerciseId.length === 0) {
+    throw new functions.https.HttpsError('invalid-argument', 'Please specify an "exerciseId"');
+  }
+
+  // generate the report
+  const result = await generateDeploymentReport(data.exerciseId);
+
+  // log an event
+  const exercise = await getDocument(db.collection('exercises').doc(data.exerciseId));
+  let details = {
+    exerciseId: exercise.id,
+    exerciseRef: exercise.referenceNumber,
+  };
+  let user = {
+    id: context.auth.token.user_id,
+    name: context.auth.token.name,
+  };
+  await logEvent('info', 'Deployment report generated', details, user);
+
+  // return the report to the caller
+  return {
+    result: result,
+  };
+
+});
