d0d95cf748b7836817d72db07ab3c89816ee7243
Ryan Hsieh <79906532+HalcyonJAC@users.noreply.github.com>
2024-03-14 16:22:39 +0000

    Feature/admin 2275 Update reports which include stage status (#1047)
    * add exerciseHelper
    
    * update diversity report
    
    * update outreach report

---
+++ functions/shared/exerciseHelper.js
@@ -0,0 +1,23 @@
+const { EXERCISE_STAGE } = require('./config');
+
+module.exports = {
+  availableStages,
+};
+
+function availableStages(exercise) {
+  const stages = [];
+  if (!exercise) return stages;
+  if (exercise._processingVersion >= 2) {
+    stages.push(EXERCISE_STAGE.SHORTLISTING);
+    stages.push(EXERCISE_STAGE.SELECTION);
+    stages.push(EXERCISE_STAGE.SCC);
+    stages.push(EXERCISE_STAGE.RECOMMENDATION);
+  } else {
+    stages.push(EXERCISE_STAGE.REVIEW);
+    stages.push(EXERCISE_STAGE.SHORTLISTED);
+    stages.push(EXERCISE_STAGE.SELECTED);
+    stages.push(EXERCISE_STAGE.RECOMMENDED);
+    stages.push(EXERCISE_STAGE.HANDOVER);
+  }
+  return stages;
+}
