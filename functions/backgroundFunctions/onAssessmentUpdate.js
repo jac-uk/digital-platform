80ea8e736dfbe6e4b66f2b4dcb3291db20b35c99
tomlovesgithub <44227249+tomlovesgithub@users.noreply.github.com>
2024-05-29 13:39:01 +0100

    2399/issues with IAs (#1095)
    * edit assessors
    
    * edit assessors
    
    * bnefghb

--- functions/backgroundFunctions/onAssessmentUpdate.js
+++ functions/backgroundFunctions/onAssessmentUpdate.js
@@ -4,31 +4,44 @@ const { firebase, db } = require('../shared/admin.js');
 const { onAssessmentCompleted } = require('../actions/assessments')(config, firebase, db);
 const { getSearchMap } = require('../shared/search');
 
+const has = require('lodash/has');
+const { isDifferentPropsByPath } = require('../shared/helpers.js');
+
 module.exports = functions.region('europe-west2').firestore
   .document('assessments/{assessmentId}')
   .onUpdate(async (change, context) => {
     const after = change.after.data();
     const before = change.before.data();
 
-    const updateSearchMap = (before.application.referenceNumber !== after.application.referenceNumber) || 
-      (before.assessor.email !== after.assessor.email) ||
-      (before.assessor.fullName !== after.assessor.fullName) || 
-      (before.candidate.fullName !== after.candidate.fullName);
+    const updateSearchMap = isDifferentPropsByPath(before, after, 'application.referenceNumber') ||
+      isDifferentPropsByPath(before, after, 'assessor.email') ||
+      isDifferentPropsByPath(before, after, 'assessor.fullName') ||
+      isDifferentPropsByPath(before, after, 'candidate.fullName');
 
     if (updateSearchMap) {
+      const updateArray = [];
+      if (has(after, 'assessor.fullName')) {
+        updateArray.push(after.assessor.fullName);
+      }
+      if (has(after, 'assessor.email')) {
+        updateArray.push(after.assessor.email);
+      }
+      if (has(after, 'application.referenceNumber')) {
+        updateArray.push(after.application.referenceNumber);
+      }
+      if (has(after, 'candidate.fullName')) {
+        updateArray.push(after.candidate.fullName);
+      }
+
       // add search map
       await db.doc(`assessments/${context.params.assessmentId}`).update({
-        _search: getSearchMap([
-          after.assessor.fullName,
-          after.assessor.email,
-          after.application.referenceNumber,
-          after.candidate.fullName,
-        ]),
+        _search: getSearchMap(updateArray),
       });
     }
 
     if (after.status !== before.status && after.status === 'completed') {
       onAssessmentCompleted(context.params.assessmentId, after);
     }
+
     return true;
   });
