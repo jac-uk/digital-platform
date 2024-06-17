0f9f94818f136bade4448700829fc4b544bff852
halcyonjac <halcyon@judicialappointments.digital>
2024-06-17 14:35:21 +0100

    update import

--- functions/shared/admin.js
+++ functions/shared/admin.js
@@ -1,11 +1,17 @@
 /**
  * Initialises Admin SDK and exports firestore database connection
  */
-const admin = require('firebase-admin');
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore, FieldPath, FieldValue, Timestamp } = require('firebase-admin/firestore');
+const { getAuth } = require('firebase-admin/auth');
+const { getStorage } = require('firebase-admin/storage');
 
-admin.initializeApp();
+const app = initializeApp();
 
-exports.firebase = admin;
-exports.db = admin.firestore();
-exports.app = admin.app();
-exports.auth = admin.auth();
+exports.firebase = {
+  firestore: { FieldPath, FieldValue, Timestamp },
+};
+exports.db = getFirestore(app);
+exports.app = app;
+exports.auth = getAuth(app);
+exports.storage = getStorage(app);
