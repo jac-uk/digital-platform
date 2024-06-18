07a4d2b0b5301f50c3080c9946c8157ad8769db6
halcyonjac <halcyon@judicialappointments.digital>
2024-06-18 12:16:09 +0100

    export storage to make it compatible with current functions

--- nodeScripts/shared/admin.js
+++ nodeScripts/shared/admin.js
@@ -10,6 +10,9 @@ const app = initializeApp();
 
 exports.firebase = {
   firestore: { FieldPath, FieldValue, Timestamp },
+  storage: function() {
+    return getStorage(app);
+  },
 };
 exports.db = getFirestore(app);
 exports.app = app;
