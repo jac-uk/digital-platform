24c33b0f5a4572769432b3e78554b3156559b947
Tom Russell <TR115251@hotmail.co.uk>
2024-06-20 15:22:00 +0100

    fix

--- functions/actions/exercises/generateStatutoryConsultationReport.js
+++ functions/actions/exercises/generateStatutoryConsultationReport.js
@@ -14,7 +14,7 @@ module.exports = (firebase, db) => {
     // get submitted application with invited to selection day
     const applicationRecords = await getDocuments(db.collection('applicationRecords')
       .where('exercise.id', '==', exerciseId)
-      .where('status', '==', 'invitedToSelectionDay')
+      .where('status', 'in', ['invitedToSelectionDay', 'shortlistingOutcomePassed'])
     );
     const applicationIds = applicationRecords.map(item => item.id);
     const applicationRefs = applicationIds.map(id => db.collection('applications').doc(id));
