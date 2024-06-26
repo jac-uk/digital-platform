14e8e11d47ad1e3f588258c7270bbe57f83a408a
halcyonjac <halcyon@judicialappointments.digital>
2024-06-26 10:30:29 +0100

    add permission

--- functions/shared/permissions.js
+++ functions/shared/permissions.js
@@ -81,6 +81,10 @@ const PERMISSIONS = {
         label: 'Can configure exercise',
         value: 'e11',
       },
+      canResetCharacterIssuesReport: {
+        label: 'Can reset character issues report',
+        value: 'e12',
+      },
     },
   },
   candidates: {
