6e478792e71a7dd9ae89d4ef329dc2d116693b55
Tom Russell <TR115251@hotmail.co.uk>
2024-07-19 16:38:23 +0100

    validate checksums and reconfigure scanallfiles

--- functions/callableFunctions/scanAllFiles.js
+++ functions/callableFunctions/scanAllFiles.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const config = require('../shared/config');
 const { firebase, db } = require('../shared/admin.js');
-const { scanAllFiles } = require('../actions/malware-scanning/scanAllFiles')(config, firebase);
+const { scanAllFiles } = require('../actions/malware-scanning/scanAllFiles')(config, firebase, db);
 const { checkFunctionEnabled } = require('../shared/serviceSettings.js')(db);
 
 module.exports = functions.region('europe-west2').https.onCall(async (data, context) => {
