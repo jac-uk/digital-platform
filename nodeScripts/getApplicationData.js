a9f77e7d79f8603cc17a3d48fe454fb1c2121a61
drieJac <jac.drie@digitalteam.uk>
2024-08-29 11:34:47 +0100

    Reverted nodescript

--- nodeScripts/getApplicationData.js
+++ nodeScripts/getApplicationData.js
@@ -6,45 +6,27 @@ import initGetApplicationData from '../functions/actions/exercises/getApplicatio
 
 const getApplicationData = initGetApplicationData(config, firebase, db, auth);
 
-// const params = {
-//   // columns: ['personalDetails.title'],
-//   columns: ['additionalWorkingPreferences 5'],
-//   exerciseId: 'zIpZ7DWHfk0b6uLUes4O',
-//   type: 'showdata',
-//   whereClauses: [],
-// };  
-
-// const params = {
-//   exerciseId: 'zIpZ7DWHfk0b6uLUes4O',
-//   type: 'showdata',
-//   whereClauses: [],
-// };  
-
 const params = {
-  exerciseId: 'Z2r71rLLFI2m8zOXV3Ev',
-  columns: [
-    'referenceNumber',
-    'personalDetails.dateOfBirth'
-  ],
-  stage: null,
-  stageStatus: null,
-  type: "showData",
+  // columns: ['personalDetails.title'],
+  columns: ['additionalWorkingPreferences 5'],
+  exerciseId: 'zIpZ7DWHfk0b6uLUes4O',
+  type: 'showdata',
   whereClauses: [],
-  statuses: [],
-};
+};  
+
+// const params = {
+//   exerciseId: 'zIpZ7DWHfk0b6uLUes4O',
+//   type: 'showdata',
+//   whereClauses: [],
+// };  
 
 const main = async () => {
-  //return getApplicationData(params);
-
-  const result = await getApplicationData(params);
-  console.log('getApplicationData:');
-  console.log(result);
-
+  return getApplicationData(params);
 };
 
 main()
   .then((result) => {
-    //console.log(result.length);
+    console.log(result.length);
     app.delete();
     return process.exit();
   })
