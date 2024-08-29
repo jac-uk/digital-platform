3e8be9aa8688f825d18845844e7546290ac1eabb
KoWeiJAC <kowei.hung@judicialappointments.digital>
2024-08-29 11:03:17 +0100

    batch clean collection

--- nodeScripts/temp/emptyCollections.js
+++ nodeScripts/temp/emptyCollections.js
@@ -12,10 +12,13 @@
 // Import necessary modules
 import readline from 'readline';
 import chalk from 'chalk';
-import { app, projectId, environment, isProduction } from '../shared/admin.js';
+import { projectId, environment, isProduction, db } from '../shared/admin.js';
+
+const batchSize = 500
 
 const collections = [
-
+  'tests',
+  'tests_2'
 ];
 
 // Create an interface for reading from the command line
@@ -33,16 +36,42 @@ const askConfirmation = (question) => {
   });
 };
 
+/**
+ * Clean documents of specified collection
+ * 
+ * @param {string} collectionPath 
+ * @param {integer} batchSize 
+ * @returns {integer} number of deleted documents
+ */
+async function cleanCollection(collectionPath, batchSize) {
+  let count = 0;
+  const collectionRef = db.collection(collectionPath);
+  const query = collectionRef.orderBy('__name__').limit(batchSize);
+  let snapshot;
+  do {
+    snapshot = await query.get();
+      // Delete documents in a batch
+      const batch = db.batch();
+      snapshot.docs.forEach((doc) => {
+        batch.delete(doc.ref);
+      });
+      await batch.commit();
+      count += snapshot.size
+
+  } while (snapshot.size >= batchSize);
+
+  return count
+}
 const main = async () => {
-  console.log('ProjectId', projectId)
-  console.log('Environment:', environment)
-  console.log('isProduction', isProduction)
+  console.log(chalk.green(`ProjectId: ${projectId}`))
+  console.log(chalk.green(`Environment: ${environment}`))
+  console.log(chalk.green(`isProduction: ${isProduction}`))
   if (isProduction) {
     console.log(chalk.red('You are trying to modify data on production environment, the execution cancelled to prevent from unexpected changes.'));
     process.exit(0); // Exit the script
   }
 
-  const question = `You are cleaning the data of the ${environment} (Project id: ${projectId}), Are you sure you want to continue? (yes/no): `
+  const question = `You are cleaning the data on the ${environment}), Are you sure you want to continue? (yes/no): `
   // Ask for confirmation
   const confirmed = await askConfirmation(question);
   rl.close();
@@ -51,12 +80,13 @@ const main = async () => {
     console.log('Script execution cancelled.');
     process.exit(0); // Exit the script
   }
-
-  // Continue with the rest of your script here
-  console.log('Running the script...');
+ 
   // Your script logic goes here
-
-
+  for (const collection of collections) {
+    console.log(`Cleaning collection: "${collection}" ...`);
+    const count = await cleanCollection(collection, batchSize);
+    console.log(`Deleted ${count} documents.`);
+  }
  
 };
 
