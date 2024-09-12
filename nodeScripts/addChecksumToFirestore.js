5056fe33a3da3ab80de758fe2d12f51abfb55d3f
Tom Russell <TR115251@hotmail.co.uk>
2024-09-12 17:18:58 +0100

    fixes

--- nodeScripts/addChecksumToFirestore.js
+++ nodeScripts/addChecksumToFirestore.js
@@ -1,11 +1,11 @@
 'use strict';
 import { app, db, firebase } from './shared/admin.js';
-import { addChecksumToFirestore } from '../functions/actions/malware-scanning/addChecksumToFirestore.js';
+import addChecksumToFirestoreInit from '../functions/actions/malware-scanning/addChecksumToFirestore.js';
 
-const addChecksum = addChecksumToFirestore(firebase, db);
+const { addChecksumToFirestore } = addChecksumToFirestoreInit(firebase, db);
 
 const main = async () => {
-  return addChecksum('blank.docx', 'ed2dfa9b93adbdd11e3a6c3b77bb22d6d8a6dea8c66b90ce54ec8cfc6cdb1425');
+  return addChecksumToFirestore('test/nothing.txt', 'ed2dfa9b93adbdd11e3a6c3b77bb22d6d8a6dea8c66b90ce54ec8cfc6cdb1425');
 };
 
 main()
