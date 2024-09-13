32c21781b18cdff33bbd6fea247c2587643b9f2a
Tom Russell <TR115251@hotmail.co.uk>
2024-09-13 15:15:41 +0100

    merge issues

--- functions/scheduledFunctions/runScannerTest.js
+++ functions/scheduledFunctions/runScannerTest.js
@@ -1,9 +1,9 @@
 import config from '../shared/config.js';
 import functions from 'firebase-functions';
-import { firebase } from '../shared/admin.js';
+import { firebase, db } from '../shared/admin.js';
 import runScannerTestInit from '../actions/malware-scanning/runScannerTest.js';
 
-const runScannerTest = runScannerTestInit(config, firebase);
+const runScannerTest = runScannerTestInit(config, firebase, db);
 
 const SCHEDULE = config.SCANNER_TEST_SCHEDULE ? config.SCANNER_TEST_SCHEDULE : 'every 10 minutes';
 
