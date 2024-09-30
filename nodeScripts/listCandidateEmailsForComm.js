77f41e48622a3cc8ff8929401809ad901133f204
warrensearle <warren@precise-minds.co.uk>
2024-09-30 13:44:19 +0100

    Fix lint error (instead of updating lint rules)

--- nodeScripts/listCandidateEmailsForComm.js
+++ nodeScripts/listCandidateEmailsForComm.js
@@ -13,110 +13,110 @@
 'use strict';
 
 import { db } from './shared/admin.js';
-import _ from 'lodash'
+import _ from 'lodash';
 import { createObjectCsvWriter as createCsvWriter } from 'csv-writer';
 
-const appliedAfter = new Date('09-01-2023')
+const appliedAfter = new Date('09-01-2023');
 
 async function listEmailsAppliedAfter(date) {
-  const batchSize = 100
-  let allEmails = []
-  let lastApplicationDoc = null
-  let snapshot = null
-  let query = null
+  const batchSize = 100;
+  let allEmails = [];
+  let lastApplicationDoc = null;
+  let snapshot = null;
+  let query = null;
 
   do {
       query =  db.collection('applications')
       .where('appliedAt', '>=', date)
-      .orderBy('appliedAt')
+      .orderBy('appliedAt');
 
       if (lastApplicationDoc) {
-        query = query.startAfter(lastApplicationDoc)
+        query = query.startAfter(lastApplicationDoc);
       }
-      query = query.limit(batchSize)
+      query = query.limit(batchSize);
 
-      snapshot = await query.get()
+      snapshot = await query.get();
       if (snapshot.docs.length < 1) {
-        break
+        break;
       } 
 
-      lastApplicationDoc = snapshot.docs[snapshot.docs.length -1]
+      lastApplicationDoc = snapshot.docs[snapshot.docs.length -1];
       snapshot.forEach((doc) => {
         const document = doc.data();
-        if (document?.personalDetails?.email) {
-          allEmails.push(document?.personalDetails?.email);
+        if (document && document.personalDetails && document.personalDetails.email) {
+          allEmails.push(document.personalDetails.email);
         }
       });
 
   } while (snapshot.docs.length >= batchSize);
 
-  allEmails = _.uniq(allEmails)
-  console.log('total', allEmails.length)
-  console.log(allEmails)
+  allEmails = _.uniq(allEmails);
+  console.log('total', allEmails.length);
+  console.log(allEmails);
 
   return allEmails;
 }
 
 async function listCandidateEmailsRegisteredInLastSixMonths() {
-  const batchSize = 100
-  let allEmails = []
-  let lastDoc = null
-  let snapshot = null
-  let query = null
+  const batchSize = 100;
+  let allEmails = [];
+  let lastDoc = null;
+  let snapshot = null;
+  let query = null;
 
   do {
-      const sixMonthsAgo = new Date()
+      const sixMonthsAgo = new Date();
       sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
-      sixMonthsAgo.setHours(0, 0, 0, 0)
+      sixMonthsAgo.setHours(0, 0, 0, 0);
 
       query =  db.collection('candidates')
       .where('created', '>=', sixMonthsAgo)
-      .orderBy('created')
+      .orderBy('created');
 
       if (lastDoc) {
-        query = query.startAfter(lastDoc)
+        query = query.startAfter(lastDoc);
       }
-      query = query.limit(batchSize)
+      query = query.limit(batchSize);
 
-      snapshot = await query.get()
+      snapshot = await query.get();
       if (snapshot.docs.length < 1) {
-        break
+        break;
       } 
 
-      lastDoc = snapshot.docs[snapshot.docs.length -1]
+      lastDoc = snapshot.docs[snapshot.docs.length -1];
       snapshot.forEach((doc) => {
         const document = doc.data();
-        if (document?.email) {
-          allEmails.push(document?.email);
+        if (document && document.email) {
+          allEmails.push(document.email);
         }
       });
 
   } while (snapshot.docs.length >= batchSize);
 
-  allEmails = _.uniq(allEmails)
-  console.log('total', allEmails.length)
-  console.log(allEmails)
+  allEmails = _.uniq(allEmails);
+  console.log('total', allEmails.length);
+  console.log(allEmails);
 
   return allEmails;
 }
 
 const main = async () => {
 
-  const emails1 = await listEmailsAppliedAfter(appliedAfter)
-  const emails2 = await listCandidateEmailsRegisteredInLastSixMonths()
-  const allEmails = _.union(emails1, emails2)
+  const emails1 = await listEmailsAppliedAfter(appliedAfter);
+  const emails2 = await listCandidateEmailsRegisteredInLastSixMonths();
+  const allEmails = _.union(emails1, emails2);
  
  const headers = [
    { id: 'email', title: 'email' },
  ];
  const output = createCsvWriter({
-   path: `candidate_emails_for_comm.csv`,
+   path: 'candidate_emails_for_comm.csv',
    header: headers,
  });
  await output.writeRecords(allEmails.map((email) => ({ email })));
  
- console.log('total', allEmails.length)
- return allEmails
+ console.log('total', allEmails.length);
+ return allEmails;
 };
 
 
