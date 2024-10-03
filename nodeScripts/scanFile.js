97714b1fc4c1ae9d77b79e44eef83ee35855ba2d
Tom Russell <TR115251@hotmail.co.uk>
2024-10-03 15:18:25 +0100

    wip

--- nodeScripts/scanFile.js
+++ nodeScripts/scanFile.js
@@ -6,7 +6,7 @@ import scanFileInit from '../functions/actions/malware-scanning/scanFile.js';
 const scan = scanFileInit(config, firebase);
 
 const main = async () => {
-  return scan('blank.docx');
+  return scan('exercise/1j25Hmhx4y1M1tF523FH/candidate-assessement-forms - 20249199547.docx');
 };
 
 main()
