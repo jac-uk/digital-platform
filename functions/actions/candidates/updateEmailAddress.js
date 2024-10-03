5ba8db2b636df33d48f97d3eec35566c084c4494
drieJac <jac.drie@digitalteam.uk>
2024-10-03 14:15:55 +0100

    Revert modifications to email change functionality as this was only put on develop for testing purposes initially and not yet intended for production.

--- functions/actions/candidates/updateEmailAddress.js
+++ functions/actions/candidates/updateEmailAddress.js
@@ -9,78 +9,17 @@ export default (auth) => {
     const currentEmailAddress = data.currentEmailAddress;
     const newEmailAddress = data.newEmailAddress;
 
-    console.log('In updateEmailAddress ...');
-
     try {
       const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-      console.log('Validating');
-
       if (emailRegEx.test(currentEmailAddress) === true && (emailRegEx.test(newEmailAddress) === true)) {
-
-        console.log('Valid email');
-
-        const currentUser = await auth.getUserByEmail(currentEmailAddress);
-
-        // Check if the new email already exists (throws an error if the user is NOT found, which we need to catch)
-        let newEmailUser = null;
-        try {
-          newEmailUser = await auth.getUserByEmail(newEmailAddress);
-        } catch (e1) {
-            // eslint-disable-next-line no-empty
-        }
-
-        console.log(`Check for email: ${newEmailAddress}`);
-        console.log('newEmailUser:');
-        console.log(newEmailUser);
-
-        if (newEmailUser) {
-
-          console.log('RETURNING ERROR AS EMAIL ALREADY EXISTS!');
-
-          return {
-            status: 'error',
-            data: {
-              code: 'auth/email-already-exists',
-              message: 'Unable to update email at this time. Please contact the admin if the problem persists.',
-            },
-          };
-        }
-        
-        console.log('EMAIL UNIQUE SO UPDATING USER');
-
-        // Update user
-        const updatedUser = await auth.updateUser(
-          currentUser.uid,
-          {
-            email: newEmailAddress,
-            emailVerified: false,   // Ensure the user must verify the email
-          }
-        );
-
-
-        console.log(`Email updated successfully for UID: ${currentUser.uid}`);
-
-        // Generate email verification link
-        const verificationLink = await auth.generateEmailVerificationLink(newEmailAddress);
-        console.log(`Verification email link generated: ${verificationLink}`);
-    
-
-
-        // @TODO: USE A TEMPLATE TO SEND THE EMAIL!!
-
-
-        
-
-
+        const user = await auth.getUserByEmail(currentEmailAddress);
+        const updatedUser = await auth.updateUser(user.uid, {email: newEmailAddress});
         return {
           status: 'success',
           data: updatedUser,
         };
       } else {
-
-        console.log('Invalid email');
-
         return {
           status: 'error',
           data: {
