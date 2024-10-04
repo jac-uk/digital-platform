36a3d36e3cfe97539978d1b09ffb0ce0dd047bac
drieJAC <jac.drie@digitalteam.uk>
2024-10-04 01:25:31 +0100

    Added sending of email using notify, though its commented out here until we're ready for UT (#1221)

--- functions/actions/candidates/updateEmailAddress.js
+++ functions/actions/candidates/updateEmailAddress.js
@@ -1,4 +1,11 @@
-export default (auth) => {
+import initFactories from '../../shared/factories.js';
+import { applyUpdates } from '../../shared/helpers.js';
+
+export default (config, auth, firebase, db) => {
+
+  const {
+    newNotificationEmailVerificationLink,
+  } = initFactories(config);
 
   return updateEmailAddress;
 
@@ -6,20 +13,115 @@ export default (auth) => {
    * Update candidate login email address
    */
   async function updateEmailAddress(data) {
+
+    console.log('Send email ...');
+
     const currentEmailAddress = data.currentEmailAddress;
     const newEmailAddress = data.newEmailAddress;
+
+    console.log('In updateEmailAddress ...');
 
     try {
       const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+      console.log('Validating');
+
       if (emailRegEx.test(currentEmailAddress) === true && (emailRegEx.test(newEmailAddress) === true)) {
-        const user = await auth.getUserByEmail(currentEmailAddress);
-        const updatedUser = await auth.updateUser(user.uid, {email: newEmailAddress});
+
+        console.log('Valid email');
+
+        const currentUser = await auth.getUserByEmail(currentEmailAddress);
+
+        // Check if the new email already exists (throws an error if the user is NOT found, which we need to catch)
+        let newEmailUser = null;
+        try {
+          newEmailUser = await auth.getUserByEmail(newEmailAddress);
+        } catch (e1) {
+            // eslint-disable-next-line no-empty
+        }
+
+        console.log(`Check for email: ${newEmailAddress}`);
+        console.log('newEmailUser:');
+        console.log(newEmailUser);
+
+        if (newEmailUser) {
+
+          console.log('RETURNING ERROR AS EMAIL ALREADY EXISTS!');
+
+          return {
+            status: 'error',
+            data: {
+              code: 'auth/email-already-exists',
+              message: 'Unable to update email at this time. Please contact the admin if the problem persists.',
+            },
+          };
+        }
+        
+        console.log('EMAIL UNIQUE SO UPDATING USER');
+
+        // Update user
+        const updatedUser = await auth.updateUser(
+          currentUser.uid,
+          {
+            email: newEmailAddress,
+            emailVerified: false,   // Ensure the user must verify the email
+          }
+        );
+
+        console.log(`Email updated successfully for UID: ${currentUser.uid}`);
+
+        // Generate email verification link
+        const verificationLink = await auth.generateEmailVerificationLink(newEmailAddress);
+        console.log(`Verification email link generated: ${verificationLink}`);
+
+
+
+        // @TODO: Uncomment below and make notify service woerk on develop!
+
+        // Send the verification email
+        // const commands = [];
+        // commands.push({
+        //   command: 'set',
+        //   ref: db.collection('notifications').doc(),
+        //   data: newNotificationEmailVerificationLink(firebase, newEmailAddress, verificationLink),
+        // });
+        // const result = await applyUpdates(db, commands);
+        
+        // if (result) {
+
+        //   console.log('The email verification link was sent ok.');
+
+        //   return {
+        //     status: 'success',
+        //     data: updatedUser,
+        //   };
+        // }
+        // else {
+
+        //   console.log('The email verification link could not be sent.');
+
+        //   return {
+        //     status: 'error',
+        //     data: {
+        //       code: 'auth/verification-link-send-failure',
+        //       message: 'The email verification link could not be sent.',
+        //     },
+        //   };
+        // }
+
+
+        // @TODO: REMOVE
         return {
           status: 'success',
           data: updatedUser,
         };
+
+        
+
       } else {
+
+        console.log('Invalid email');
+
         return {
           status: 'error',
           data: {
