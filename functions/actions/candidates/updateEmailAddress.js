755f017e8b9f44d38f84a71ae4cd261ce7856641
drieJac <jac.drie@digitalteam.uk>
2024-10-09 01:41:10 +0100

    Revert the change email modifications as they havent been tested yet.

--- functions/actions/candidates/updateEmailAddress.js
+++ functions/actions/candidates/updateEmailAddress.js
@@ -1,11 +1,4 @@
-import initFactories from '../../shared/factories.js';
-import { applyUpdates } from '../../shared/helpers.js';
-
-export default (config, auth, firebase, db) => {
-
-  const {
-    newNotificationEmailVerificationLink,
-  } = initFactories(config);
+export default (auth) => {
 
   return updateEmailAddress;
 
@@ -13,115 +6,20 @@ export default (config, auth, firebase, db) => {
    * Update candidate login email address
    */
   async function updateEmailAddress(data) {
-
-    console.log('Send email ...');
-
     const currentEmailAddress = data.currentEmailAddress;
     const newEmailAddress = data.newEmailAddress;
-
-    console.log('In updateEmailAddress ...');
 
     try {
       const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-      console.log('Validating');
-
       if (emailRegEx.test(currentEmailAddress) === true && (emailRegEx.test(newEmailAddress) === true)) {
-
-        console.log('Valid email');
-
-        const currentUser = await auth.getUserByEmail(currentEmailAddress);
-
-        // Check if the new email already exists (throws an error if the user is NOT found, which we need to catch)
-        let newEmailUser = null;
-        try {
-          newEmailUser = await auth.getUserByEmail(newEmailAddress);
-        } catch (e1) {
-            // eslint-disable-next-line no-empty
-        }
-
-        console.log(`Check for email: ${newEmailAddress}`);
-        console.log('newEmailUser:');
-        console.log(newEmailUser);
-
-        if (newEmailUser) {
-
-          console.log('RETURNING ERROR AS EMAIL ALREADY EXISTS!');
-
-          return {
-            status: 'error',
-            data: {
-              code: 'auth/email-already-exists',
-              message: 'Unable to update email at this time. Please contact the admin if the problem persists.',
-            },
-          };
-        }
-        
-        console.log('EMAIL UNIQUE SO UPDATING USER');
-
-        // Update user
-        const updatedUser = await auth.updateUser(
-          currentUser.uid,
-          {
-            email: newEmailAddress,
-            emailVerified: false,   // Ensure the user must verify the email
-          }
-        );
-
-        console.log(`Email updated successfully for UID: ${currentUser.uid}`);
-
-        // Generate email verification link
-        const verificationLink = await auth.generateEmailVerificationLink(newEmailAddress);
-        console.log(`Verification email link generated: ${verificationLink}`);
-
-
-
-        // @TODO: Uncomment below and make notify service woerk on develop!
-
-        // Send the verification email
-        // const commands = [];
-        // commands.push({
-        //   command: 'set',
-        //   ref: db.collection('notifications').doc(),
-        //   data: newNotificationEmailVerificationLink(firebase, newEmailAddress, verificationLink),
-        // });
-        // const result = await applyUpdates(db, commands);
-        
-        // if (result) {
-
-        //   console.log('The email verification link was sent ok.');
-
-        //   return {
-        //     status: 'success',
-        //     data: updatedUser,
-        //   };
-        // }
-        // else {
-
-        //   console.log('The email verification link could not be sent.');
-
-        //   return {
-        //     status: 'error',
-        //     data: {
-        //       code: 'auth/verification-link-send-failure',
-        //       message: 'The email verification link could not be sent.',
-        //     },
-        //   };
-        // }
-
-
-        // @TODO: REMOVE
+        const user = await auth.getUserByEmail(currentEmailAddress);
+        const updatedUser = await auth.updateUser(user.uid, {email: newEmailAddress});
         return {
           status: 'success',
           data: updatedUser,
         };
-
-        
-
       } else {
-
-        console.log('Invalid email');
-
         return {
           status: 'error',
           data: {
--- functions/callableFunctions/updateEmailAddress.js
+++ functions/callableFunctions/updateEmailAddress.js
@@ -1,10 +1,10 @@
 import functions from 'firebase-functions';
-import { auth, firebase, db } from '../shared/admin.js';
+import { auth, db } from '../shared/admin.js';
 import { checkArguments } from '../shared/helpers.js';
 import initUpdateEmailAddress from '../actions/candidates/updateEmailAddress.js';
 import initServiceSettings from '../shared/serviceSettings.js';
 
-const updateEmailAddress = initUpdateEmailAddress(auth, firebase);
+const updateEmailAddress = initUpdateEmailAddress(auth);
 const { checkFunctionEnabled } = initServiceSettings(db);
 
 export default functions.region('europe-west2').https.onCall(async (data, context) => {
