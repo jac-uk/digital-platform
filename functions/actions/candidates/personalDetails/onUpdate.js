026f904278c2eaae02037387bdf60f50c44774e2
drieJac <jac.drie@digitalteam.uk>
2024-10-30 11:06:49 +0000

    Reset mobileVerifiedAt field when candidate mobile number changes to force re-verification

--- functions/actions/candidates/personalDetails/onUpdate.js
+++ functions/actions/candidates/personalDetails/onUpdate.js
@@ -1,4 +1,5 @@
 import initSearch from '../search.js';
+import { applyUpdates } from '../../../shared/helpers.js';
 
 export default (config, firebase, db) => {
   const { updateCandidate } = initSearch(firebase, db);
@@ -9,11 +10,30 @@ export default (config, firebase, db) => {
    * Candidate Personal Details event handler for Update
    */
   async function onUpdate(candidateId, dataBefore, dataAfter) {
-
-    // update candidate document
-    const result = await updateCandidate(candidateId);
-
-    return result;
+    try {
+        // If mobile number has changed then reset the mobileVerifiedAt so it forces re-verification
+        if (dataBefore.mobile !== dataAfter.mobile) {
+          const personalDetails = db.collection('candidates').doc(candidateId).collection('documents').doc('personalDetails');
+          const commands = [];
+          commands.push({
+            command: 'update',
+            ref: personalDetails,
+            data: {
+              mobileVerifiedAt: null,
+            },
+          });
+          await applyUpdates(db, commands);
+        }
+        
+        // Update candidate document
+        const result = await updateCandidate(candidateId);
+        return result;
+    }
+    catch (err) {
+      console.log('ERROR:');
+      console.log(err);
+      return false;
+    }
   }
 
 };
