284f0953beaf1877bd53d4197eb1fbd95b7dc03f
drieJac <jac.drie@digitalteam.uk>
2024-10-31 16:13:59 +0000

    Remove changes as they conflict with Halcyons code

--- functions/actions/candidates/personalDetails/onUpdate.js
+++ functions/actions/candidates/personalDetails/onUpdate.js
@@ -1,5 +1,4 @@
 import initSearch from '../search.js';
-import { applyUpdates } from '../../../shared/helpers.js';
 
 export default (config, firebase, db) => {
   const { updateCandidate } = initSearch(firebase, db);
@@ -10,30 +9,11 @@ export default (config, firebase, db) => {
    * Candidate Personal Details event handler for Update
    */
   async function onUpdate(candidateId, dataBefore, dataAfter) {
-    try {
-        // If mobile number exists and has changed then reset the mobileVerifiedAt so it forces re-verification
-        if (dataBefore.mobile && dataBefore.mobile !== dataAfter.mobile) {
-          const personalDetails = db.collection('candidates').doc(candidateId).collection('documents').doc('personalDetails');
-          const commands = [];
-          commands.push({
-            command: 'update',
-            ref: personalDetails,
-            data: {
-              mobileVerifiedAt: null,
-            },
-          });
-          await applyUpdates(db, commands);
-        }
-        
-        // Update candidate document
-        const result = await updateCandidate(candidateId);
-        return result;
-    }
-    catch (err) {
-      console.log('ERROR:');
-      console.log(err);
-      return false;
-    }
+
+    // update candidate document
+    const result = await updateCandidate(candidateId);
+
+    return result;
   }
 
 };
