74f7d897abcf4e294a4d87263f8480772c6a9cf3
KoWeiJAC <kowei.hung@judicialappointments.digital>
2024-12-07 22:21:59 +0000

    html for scc summary report

--- functions/actions/exercises/sccSummaryReport.js
+++ functions/actions/exercises/sccSummaryReport.js
@@ -123,120 +123,71 @@ export default (firebase, db) => {
         return false;
   }
 
-  /**
- * Exports eligibility issues to a Google Docs file
- *
- * @param {*} applicationRecords
- * @returns
- */
-  async function exportSccAnnexReport(exercise, applicationRecords) {
-
-    // get drive service
-    await drive.login();
-
-    // get settings and apply them
-    const settings = await getDocument(db.collection('settings').doc('services'));
-    drive.setDriveId(settings.google.driveId);
-
-    // generate a filename for the document we are going to create ex. JAC00787_SCC Eligibility Annexes
-    const now = new Date();
-    // const timestamp = new Date(now.getTime() - (now.getTimezoneOffset() * 60000)).toISOString();
-    const filename = `${exercise.referenceNumber}_SCC Eligibility Annexes` ;
-
-    // make sure a destination folder exists to create the file in
-    const folderName = 'Eligibility Export';
-    const folders = await drive.listFolders();
-    let folderId = 0;
-    folders.forEach((v, i) => {
-      if (v.name === folderName) {
-        folderId = v.id;
-      }
-    });
-    if (folderId === 0) { // folder doesn't exist so create it
-      folderId = await drive.createFolder(folderName);
-    }
-
-    // Create eligibility issues document
-    const fileId = await drive.createFile(filename, {
-      folderId: folderId,
-      sourceType: drive.MIME_TYPE.HTML,
-      sourceContent: getHtmlSccSummaryReport(sccSummaryReport),
-      destinationType: drive.MIME_TYPE.DOCUMENT,
-    });
-
-    if (fileId) {
-      return await drive.exportFile(fileId, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
-    }
-
-    return false;
-  }
-
   function getHtmlSccSummaryReport(sccSummaryReport) {
+    const {
+      numberOfVacancies,
+      locationDetails,
+      launch,
+      closed,
+      numberOfApplications,
+      numberOfWithdrawals,
+      numberOfRemovedOnEligibilityOrASC,
+      numberOfShortlisted,
+      methodOfShortlisting,
+      selectionDayTools,
+      datesOfSelectionDays,
+    } = sccSummaryReport;
+
+    const rows = [
+      { header: 'Number of vacancies', content: numberOfVacancies.join(' ,') },
+      { header: 'Number of candidates proposed for Recommendation', content: '' },
+      { header: 'Vacancies by jurisdiction/chamber', content: '' },
+      { header: 'Location details', content: locationDetails },
+      { header: 'VR (includes details of SPTW)', content: 'Annex <span style="color:red" >X</span>' },
+      { header: '', content: '' },
+      { header: 'Launch', content: launch },
+      { header: 'Closed', content: closed },
+      { header: 'Number of applications', content: numberOfApplications },
+      { header: 'Number of withdrawals', content: numberOfWithdrawals },
+      { header: 'Number removed on eligibility/ASC', content: numberOfRemovedOnEligibilityOrASC },
+      { header: '', content: '' },
+      { header: 'Number shortlisted', content: numberOfShortlisted },
+      { header: 'Method of shortlisting', content: methodOfShortlisting.join(' ,') },
+      { header: 'Dates of shortlisting', content: '' },
+      { header: '', content: '' },
+      { header: 'Statutory consultee(s)', content: '<span style="color:red"><pre>insert name/s or &#39;consultation waived&#39;</pre> </span>' },
+      { header: '', content: '' },
+      { header: 'Selection day tools', content: selectionDayTools.join(' ,') },
+      { header: 'Number of A-C candidates', content: '' },
+      { header: 'Dates of selection days', content: datesOfSelectionDays },
+      { header: '', content: '' },
+      { header: '<if applicable> Date s.94 list Created', content: '' },
+      { header: '<if applicable> Candidates Remaining on s.94 list', content: '' },
+      { header: '', content: '' },
+      { header: 'Character Checks undertaken', content: '<b style="font-style:italic">E.g. HMRC, ACRO, BSB, etc</b>' },
+      { header: 'Character issues', content: 'No issues identified or &lt;All issues were considered when s.94 list was created and no further issues identified&gt; or &lt;The following matters require a decision&gt; &lt;Amend as needed&gt; &lt;This exercise uses the revised Good Character Guidance launched on 15 October&gt; or &lt;This exercise uses the older Good Character Guidance before its launch on 15 October&gt;:' },
+      { header: 'Matters requiring a decision', content: 'Annex <span style="font-red">X</span>' },
+      { header: 'Previously declared, within guidance', content: 'Annex <span style="font-red">X</span>' },
+      { header: 'High scoring D candidates', content: 'Annex <span style="font-red">X</span>' },
+    ];
 
     let writer = new htmlWriter();
-
+    writer.addHeading('Summary', 'center');
     writer.addRaw(`
-      <p style="text-align: right;"><a name="annex-a"><b>ANNEX A</b></a></p>
-          `);
-          writer.addHeading('JUDICIAL APPOINTMENTS ELIGIBILITY STATEMENT AND ASC', 'center');
-          writer.addRaw(`
-      <p class="red" style="text-align: right;">Ref: 00077</p>
       <table>
-        <tbody>
-          <tr>
-            <td width="250"><b>Title:</b></td>
-            <td colspan="4">${exercise.name}</td>
-          </tr>
-          <tr>
-            <td width="250"><b>Statutory eligibility requirement:</b></td>
-            <td colspan="4"></td>
-          </tr>
-          <tr>
-            <td width="250"><b>Relevant qualification:</b></td>
-            <td width="75" style="text-align: center;"><b>Solicitor</b><br>${exercise.qualifications.includes('solicitor') ? 'Yes' : 'No'}</td>
-            <td width="80" style="text-align: center;"><b>Barrister</b><br>${exercise.qualifications.includes('barrister') ? 'Yes' : 'No'}</td>
-            <td style="text-align: center;"><b>Fellow CILEX</b><br>${exercise.qualifications.includes('cilex') ? 'Yes' : 'No'}</td>
-            <td style="text-align: center;"><b>Other (e.g. Patent Agent, Medical etc)</b><br>${exercise.qualifications.includes('other') ? 'Yes' : 'No'}</td>
-          </tr>
-          <tr>
-            <td width="250"><b>Non-statutory eligibility requirement<br>Approved by Lord Chancellor (N/A)</b></td>
-            <td colspan="4"></td>
-          </tr>
-          <tr>
-            <td width="250"><b>Additional Information:</b></td>
-            <td colspan="4"></td>
-          </tr>
-          <tr>
-            <td width="250">
-              <b>Reasonable length of service (and retirement age if not 70*):</b>
-              <br>
-              <b>*As set out in the government response to the Judicial Mandatory Retirement Age consultation published on 8 March, it is the intention to raise the mandatory retirement age (MRA) for judicial holders to 75.</b>
-            </td>
-            <td colspan="4">${exercise.reasonableLengthService} ${exercise.reasonableLengthService > 1 ? 'years' : 'year'}</td>
-          </tr>
-          <tr>
-            <td width="250">
-              <b>Salaried posts only:</b>
-              <br>
-              <b>Previous service in judicial office</b>
-            </td>
-            <td colspan="4"></td>
-          </tr>
-          <tr>
-            <td width="250"><b>House of Commons Disqualification Act 1975 applies:</b></td>
-            <td colspan="4"></td>
-          </tr>
-          <tr>
-            <td width="250">
-              <b>Salaried posts only:</b>
-              <br>
-              <b>Medical Examination:</b>
-            </td>
-            <td colspan="4"></td>
-          </tr>
-        </tbody>
-      </table>
-          `);
+        <tbody>`
+    );
+    for (const row of rows) {
+      writer.addRaw(
+        `<tr>
+          <td><b>${row.header}</b></td>
+          <td>${row.content}</td>
+        </tr>`
+      );
+    }
+    writer.addRaw(
+       `</tbody>
+      </table>`);
 
     return writer.toString();
   }
