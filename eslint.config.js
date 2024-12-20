b19255ece9407fa1fc0a3003ef2852b2d208a8fb
Tom Russell <TR115251@hotmail.co.uk>
2024-12-20 13:11:03 +0000

    fix lint

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import eslintPluginPromise from 'eslint-plugin-promise';
+import eslintPluginImport from 'eslint-plugin-import';
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      globals: {
+        NodeJS: true,
+      },
+    },
+    plugins: {
+      promise: eslintPluginPromise,
+      import: eslintPluginImport,
+    },
+    rules: {
+      'comma-dangle': ['error', { arrays: 'always-multiline', objects: 'always-multiline' }],
+      'eol-last': ['error', 'always'],
+      'prefer-arrow-callback': 'error',
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'semi': 'error',
+      'no-console': 'off',
+      'no-unused-vars': 'warn',
+      'import/no-unresolved': [2, { ignore: ['^firebase-admin/.+'] }],
+      'import/no-commonjs': 2,
+      'import/extensions': [2, 'ignorePackages'],
+      ...eslintPluginPromise.configs.recommended.rules,
+    },
+  },
+];
