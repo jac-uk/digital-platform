4126ac56ccd146c6382d092c36d8f399ef208fef
halcyonjac <halcyon@judicialappointments.digital>
2025-01-31 06:46:46 +0000

    change panel packs location

--- functions/actions/panels/initialisePanelExport.js
+++ functions/actions/panels/initialisePanelExport.js
@@ -1,6 +1,7 @@
 import { getDocument, getDocuments, applyUpdates, getAllDocuments, formatDate } from '../../shared/helpers.js';
 import initDrive from '../../shared/google-drive.js';
 import { exportGradingSheet } from './exportGradingSheet.js';
+import lookup from '../../shared/converters/lookup.js';
 
 const drive = initDrive();
 
@@ -59,27 +60,62 @@ export default (config, firebase, db) => {
     const exerciseId = panel.exercise ? panel.exercise.id : panel.exerciseId;
     const exercise = await getDocument(db.collection('exercises').doc(exerciseId));
 
-    // get exercise ref number and make a folder name
-    const folderName =
-      (exercise.referenceNumber).slice(3) + ' ' + panel.name;
+    // get google drive name
+    const driveName = exercise.referenceNumber;
+
+    // get panel and type folder name
+    const panelFolderName = panel.name;
+    const panelTypeFolderName = lookup(panel.type) || panel.type;
 
     // login to google drive and create panel folder
     await drive.login();
 
-    drive.setDriveId(settings.google.driveId);
-    const panelFolderId = await drive.createFolder(folderName, {
-      parentId: settings.google.rootFolderId,
-    });
+    // check if drive exists
+    const drives = await drive.listSharedDrives(driveName);
+    if (!drives.length) {
+      console.error('Folder not found:', driveName);
+      const data = {
+        error: 'Folder not found - please rename root exercise folder according to the reference number of the exercise, i.e. JAC00XXX',
+      };
+      await panel.ref.update(data);
+      return false;
+    }
+    const driveId = drives[0].id;
+    drive.setDriveId(driveId);
+    
+    const panelFolders = await drive.listFolders('', false, panelFolderName);
+    console.log('panelFolders', panelFolders);
+    let panelFolderId = null;
+    if (panelFolders && panelFolders.length) {
+      panelFolderId = panelFolders[0].id;
+    } else {
+      panelFolderId = await drive.createFolder(panelFolderName, {
+        parentId: driveId,
+      });
+    }
+    console.log('panelFolderId', panelFolderId);
+
+    const panelTypeFolders = await drive.listFolders('', false, panelTypeFolderName);
+    console.log('panelTypeFolders', panelTypeFolders);
+    let panelTypeFolderId = null;
+    if (panelTypeFolders && panelTypeFolders.length) {
+      panelTypeFolderId = panelTypeFolders[0].id;
+    } else {
+      panelTypeFolderId = await drive.createFolder(panelTypeFolderName, {
+        parentId: panelFolderId,
+      });
+    }
+    console.log('panelTypeFolderId', panelTypeFolderId);
 
     // Create grading spreadsheet
-    await exportGradingSheet(drive, panelFolderId, folderName, panel).catch(e => 'Error: Grading Sheet');
+    await exportGradingSheet(drive, panelTypeFolderId, panelTypeFolderName, panel).catch(e => 'Error: Grading Sheet');
 
     // update panel and start processing
     const applicationIds = Object.keys(applicationsMap);
     const data = {
       drive: {
-        driveId: settings.google.driveId,
-        folderId: panelFolderId,
+        driveId: driveId,
+        folderId: panelTypeFolderId,
       },
       applicationsMap: applicationsMap,
       status: 'processing',
